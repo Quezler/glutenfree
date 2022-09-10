b7d947827a6e3bf66cb3e7879009088076cf3f30
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2022-09-10 17:30:12 +0200

    Create the init command

--- app.php
+++ app.php
@@ -1,12 +1,13 @@
 #!/usr/bin/env php
 <?php
 
+define('__GLUTENFREE__', __DIR__);
 require __DIR__.'/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
-//
+$application->add(new \App\Command\InitCommand());
 
 $application->run();
---
+++ app/Command/InitCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitCommand extends Command
+{
+    protected static $defaultName = 'init';
+
+    protected function configure(): void
+    {
+        $this->addArgument('name', InputArgument::REQUIRED, 'Name');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        mkdir($directory = __GLUTENFREE__ . '/mods/' . $input->getArgument('name'));
+
+        $info = [
+            "name" => $input->getArgument('name'),
+            "title" => ucfirst(str_replace('-', ' ', $input->getArgument('name'))),
+            "description" => "Nobody expects the spanish inquisition.",
+            "version" => "1.0.0",
+            "author" => "Quezler",
+            "factorio_version" => "1.1",
+            "dependencies" => [
+                "? base",
+            ]
+        ];
+
+        var_dump($info);
+
+        file_put_contents("{$directory}/info.json", json_encode($info, JSON_PRETTY_PRINT));
+
+        return Command::SUCCESS;
+    }
+}
