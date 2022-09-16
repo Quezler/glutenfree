0143938bc7c787061ad6db54eb7cd688ba345a46
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2022-09-16 16:04:59 +0200

    Initial cleanup

--- mods/glutenfree-ltn-announcer/data.lua
+++ mods/glutenfree-ltn-announcer/data.lua
@@ -2,28 +2,12 @@ flib = require('__flib__.data-util')
 
 local speaker = data.raw['programmable-speaker']['programmable-speaker']
 
--- local ltn_stop_announcer = table.deepcopy(data.raw['constant-combinator']['constant-combinator'])
--- ltn_stop_announcer.name = 'logistic-train-stop-announcer'
--- ltn_stop_announcer.icon = speaker.icon
--- ltn_stop_announcer.icon_size = 64
--- ltn_stop_announcer.icon_mipmaps = nil
--- ltn_stop_announcer.next_upgrade = nil
--- ltn_stop_announcer.minable = nil
--- ltn_stop_announcer.selection_box = {{-0.5, -0.5}, {0.5, 0.5}}
--- ltn_stop_announcer.selection_priority = (ltn_stop_announcer.selection_priority or 50) + 10
--- ltn_stop_announcer.collision_mask = {{-0.15, -0.15}, {0.15, 0.15}}
--- ltn_stop_announcer.collision_mask = {'rail-layer'}
--- ltn_stop_announcer.item_slot_count = 100
--- ltn_stop_announcer.sprites = speaker.sprite
+--
 
 local ltn_stop_announcer = {
   name = 'logistic-train-stop-announcer',
   type = 'electric-pole',
 }
-
--- ltn_stop_announcer.pictures = make_4way_animation_from_spritesheet(speaker.sprite)
--- print(serpent.block(ltn_stop_announcer.pictures))
--- print('owo!')
 
 ltn_stop_announcer.pictures = speaker.sprite
 ltn_stop_announcer.pictures.layers[1].direction_count = 1
@@ -44,13 +28,6 @@ ltn_stop_announcer.connection_points = {{
   wire = connection_points,
 }}
 
--- ltn_stop_announcer.connection_points = {
---   connection_points,
---   -- connection_points,
---   -- connection_points,
---   -- connection_points,
--- }
-
 ltn_stop_announcer.selection_box = {{-0.5, -0.5}, {0.5, 0.5}}
 ltn_stop_announcer.selection_priority = (ltn_stop_announcer.selection_priority or 50) + 10
 ltn_stop_announcer.collision_box = {{-0.5, -0.5}, {0.5, 0.5}}
@@ -62,27 +39,12 @@ ltn_stop_announcer.draw_copper_wires = false
 ltn_stop_announcer.flags = {'player-creation'}
 ltn_stop_announcer.placeable_by = {item = 'programmable-speaker', count = 1}
 
--- local ltn_stop_announcer = flib.copy_prototype(data.raw["constant-combinator"]["constant-combinator"], "logistic-train-stop-announcer")
--- ltn_stop_out.icon = "__LogisticTrainNetwork__/graphics/icons/output.png"
--- ltn_stop_out.icon = data.raw["constant-combinator"]"__LogisticTrainNetwork__/graphics/icons/output.png"
--- ltn_stop_out.icon_size = 64
--- ltn_stop_out.icon_mipmaps = nil
--- ltn_stop_out.next_upgrade = nil
--- ltn_stop_out.minable = nil
--- ltn_stop_out.selection_box = {{-0.5, -0.5}, {0.5, 0.5}}
--- ltn_stop_out.selection_priority = (ltn_stop_out.selection_priority or 50) + 10 -- increase priority to default + 10
--- ltn_stop_out.collision_box = {{-0.15, -0.15}, {0.15, 0.15}}
--- ltn_stop_out.collision_mask = {"rail-layer"} -- collide only with rail entities
--- ltn_stop_out.item_slot_count = 50
-
-
--- local ltn_stop_announcer_red = table.deepcopy(data.raw['constant-combinator']['logistic-train-stop-lamp-control'], 'logistic-train-stop-announcer-red-signal')
--- ltn_stop_announcer_red.name = 'logistic-train-stop-announcer-red-signal'
-
 local red_signal = flib.copy_prototype(data.raw['constant-combinator']['logistic-train-stop-lamp-control'], 'logistic-train-stop-announcer-red-signal')
 local green_signal = flib.copy_prototype(data.raw['constant-combinator']['logistic-train-stop-lamp-control'], 'logistic-train-stop-announcer-green-signal')
 
 red_signal.draw_circuit_wires = false
 green_signal.draw_circuit_wires = false
+
+--
 
 data:extend({ltn_stop_announcer, red_signal, green_signal})
--- mods/glutenfree-ltn-announcer/scripts/speaker.lua
+++ mods/glutenfree-ltn-announcer/scripts/speaker.lua
@@ -13,30 +13,15 @@ function speaker.init()
   global.deliveries = global.deliveries or {}
   global.logistic_train_stops = global.logistic_train_stops or {}
 
-  global.train_stops = {}
   for _, surface in pairs(game.surfaces) do
-    for _, entity in pairs(surface.find_entities_filtered{type = "train-stop"}) do
-      global.train_stops[entity.unit_number] = entity
-
-      if entity.name == 'logistic-train-stop' then
-        speaker.add_speaker_to_ltn_stop(entity)
-      end
+    for _, entity in pairs(surface.find_entities_filtered{type = 'train-stop', name = 'logistic-train-stop'}) do
+      speaker.add_speaker_to_ltn_stop(entity)
     end
   end
 end
 
 function speaker.on_created_entity(event)
   local entity = event.created_entity or event.entity or event.destination
-
-  -- todo: figure out how to remove copper wires from ghosts
-  if entity.name == "entity-ghost" and entity.ghost_name == "logistic-train-stop-announcer" then
-    -- entity.disconnect_neighbour()
-
-    -- for _, neighbour in ipairs(entity.neighbours) do
-    --   game.print(neighbour.name)
-    -- end
-  end
-
   if entity.name ~= 'logistic-train-stop' then return end
 
   speaker.add_speaker_to_ltn_stop(entity)
@@ -124,29 +109,20 @@ function speaker.on_train_schedule_changed(event)
   if not global.deliveries[event.train.id] then return end
   local delivery = global.deliveries[event.train.id]
 
-  -- print('schedule + delivery:')
-  -- print(serpent.block( event.train.schedule ))
-  -- print(serpent.block( delivery ))
+  -- get the provider & requester station id from LTN
+  -- so we can be sure for which stop the tmp one is.
 
   local provider = global.logistic_train_stops[delivery.from_id]
-  if provider then
-    provider.entity.surface.create_entity{name = "flying-text", position = provider.entity.position, text = serpent.block( delivery.shipment )}
-    speaker.announce(provider.entity)
-  end
+  if provider then speaker.announce(provider.entity) end
 
   local requester = global.logistic_train_stops[delivery.to_id]
-  if requester then
-    requester.entity.surface.create_entity{name = "flying-text", position = requester.entity.position, text = serpent.block( delivery.shipment )}
-    speaker.announce(requester.entity)
-  end
+  if requester then speaker.announce(requester.entity) end
 end
 
 -- update the speakerpole signals
 function speaker.announce(entity)
   local entry = global.entries[entity.unit_number]
   if not entry then return end
-
-  -- string.gmatch("item,something", "[^,]+")
 
   local red = {}
   local green = {}
@@ -158,12 +134,6 @@ function speaker.announce(entity)
 
       -- is the train still [underway] to here?
       if trains.is_inbound(train, entity) then
-
-        -- -- is this the botmall storage provider
-        -- if (entity.unit_number == 3365039 and train.id == 1236 ) then
-        --   game.print('is_inbound: ' .. serpent.block( trains.is_inbound(train, entity) ))
-        --   print(serpent.block( train.schedule ))
-        -- end
 
         -- sum any/all the items due for pickup
         if delivery.from_id == entity.unit_number then
@@ -180,7 +150,6 @@ function speaker.announce(entity)
         end
       end
 
-      -- print(serpent.block( delivery ))
     end
   end
 
