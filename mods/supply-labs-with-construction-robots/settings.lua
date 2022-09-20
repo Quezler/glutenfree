39bb950bfea609b537096da76c54827a089aca45
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2022-09-20 16:01:28 +0200

    This changelog entry should be hidden/unreleased

--- mods/supply-labs-with-construction-robots/settings.lua
+++ mods/supply-labs-with-construction-robots/settings.lua
@@ -7,12 +7,12 @@ data:extend({
     maximum_value = 3600,
     default_value = 3600,
   },
-    {
-      type = "int-setting",
-      name = "lab-resupply-amount",
-      setting_type = "startup",
-      minimum_value = 1,
-      maximum_value = 1000,
-      default_value = 200,
-    }
+  {
+    type = "int-setting",
+    name = "lab-resupply-amount",
+    setting_type = "startup",
+    minimum_value = 1,
+    maximum_value = 1000,
+    default_value = 200,
+  }
 })
