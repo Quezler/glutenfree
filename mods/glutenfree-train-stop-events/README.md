f80a4092628345c39ff6422808af866a2a6a9ab6
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2022-09-22 14:01:42 +0200

    Autosave goes brrr

---
+++ mods/glutenfree-train-stop-events/README.md
@@ -0,0 +1,4 @@
+events provided by this mod:
+
+- `script.on_event(remote.call("glutenfree-train-stop-events", "on_train_stop_trains_changed"), ...)`
+
--- mods/glutenfree-train-stop-events/scripts/handler.lua
+++ mods/glutenfree-train-stop-events/scripts/handler.lua
@@ -1,6 +1,7 @@
 local handler = {}
 
 local on_train_stop_trains_changed = script.generate_event_name()
+local on_train_removed_from_train_stop = script.generate_event_name()
 
 remote.add_interface("glutenfree-train-stop-events", {
   on_train_stop_trains_changed = function() return on_train_stop_trains_changed end
@@ -14,7 +15,34 @@ function handler.init()
   global.train_stops = {}
 
   -- keyed by .id
-  global.trains = {} 
+  global.trains = {}
+
+  global.
+
+  for _, surface in pairs(game.surfaces) do
+    for _, entity in pairs(surface.find_entities_filtered{type = 'train-stop'}) do
+      handler.register_train_stop(entity)
+    end
+
+    for _, train in ipairs(surface.get_trains()) do
+      handler.register_train(train)
+    end
+  end
+end
+
+function handler.on_created_entity(event)
+  local entity = event.created_entity or event.entity or event.destination
+  if entity.type ~= 'train-stop' then return end
+
+  handler.register_train_stop(entity)
+end
+
+function handler.register_train_stop(entity)
+  global.train_stops[entity.unit_number] = entity
+end
+
+function handler.register_train(train)
+  global.trains[train.id] = train
 end
 
 function handler.on_train_schedule_changed(event)
