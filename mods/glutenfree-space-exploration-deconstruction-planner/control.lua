2086e89d12ff9665b25e76816cb06ee6de9e08b4
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2022-10-09 12:56:41 +0200

    Stash entryless prototype

---
+++ mods/glutenfree-space-exploration-deconstruction-planner/control.lua
@@ -0,0 +1,34 @@
+local Planner = require('scripts.planner')
+
+--
+
+local function init()
+  Planner.init()
+end
+
+local function load()
+  --
+end
+
+script.on_init(function()
+  init()
+  load()
+end)
+
+script.on_load(function()
+  load()
+end)
+
+--
+
+script.on_event(defines.events.on_player_selected_area, function(event)
+  game.print('on_player_selected_area')
+  Planner.on_player_selected_area(event)
+end)
+
+script.on_event(defines.events.on_player_alt_selected_area, function(event)
+  game.print('on_player_alt_selected_area')
+end)
+
+script.on_event(defines.events.on_robot_mined_entity, Planner.on_robot_mined_entity)
+script.on_event(defines.events.on_cancelled_deconstruction, Planner.on_cancelled_deconstruction)
---
+++ mods/glutenfree-space-exploration-deconstruction-planner/data.lua
@@ -0,0 +1,49 @@
+local mod_prefix = 'glutenfree-space-exploration-deconstruction-planner-'
+
+local shortcut = {
+  type = "shortcut",
+  name = mod_prefix .. 'shortcut',
+
+  action = "spawn-item",
+  item_to_spawn = mod_prefix .. 'item',
+  technology_to_unlock = 'se-space-platform-scaffold',
+
+  style = "default",
+  icon = data.raw['shortcut']['give-deconstruction-planner'].icon,
+  small_icon = data.raw['shortcut']['give-deconstruction-planner'].small_icon,
+  disabled_small_icon = data.raw['shortcut']['give-deconstruction-planner'].disabled_small_icon,
+}
+
+local item = {
+  type = "selection-tool",
+  name = mod_prefix .. 'item',
+  icon = "__glutenfree-space-exploration-deconstruction-planner__/graphics/items/greyscale-deconstruction-planner.png",
+  icon_size = 64,
+  icon_mipmaps = 4,
+  flags = {"hidden", "not-stackable", "spawnable", "only-in-cursor"},
+  stack_size = 1,
+
+  selection_color = {r = 0.25, g = 0.25, b = 0.25},
+  alt_selection_color = {r = 0.25, g = 0.25, b = 0.25},
+
+  selection_mode = {'deconstruct'},
+  alt_selection_mode = {'cancel-deconstruct'},
+
+  selection_cursor_box_type = 'not-allowed',
+  alt_selection_cursor_box_type = 'not-allowed',
+
+  always_include_tiles = true,
+  tile_filters = {'se-space-platform-scaffold', 'se-space-platform-plating', 'se-spaceship-floor'},
+  alt_tile_filters = {'se-space-platform-scaffold', 'se-space-platform-plating', 'se-spaceship-floor'},
+},
+
+-- data:extend({item, shortcut}) -- doesn't work
+-- data:extend({shortcut, item}) -- complains that the item doesn't exist
+
+-- invalid prototype array {}
+-- data:extend({item})
+-- data:extend({shortcut})
+
+-- works
+data:extend({shortcut})
+data:extend({item})
