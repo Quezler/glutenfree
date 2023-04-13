f62af9031bdd9a56f0fecc40c17550f4ff360b98
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-04-13 17:09:54 +0200

    Birth lossless-reactor

---
+++ mods/lossless-reactor/data.lua
@@ -0,0 +1,4 @@
+-- When this is true, the reactor will stop consuming fuel/energy when the temperature has reached the maximum.
+data.raw['reactor']['nuclear-reactor'].scale_energy_usage = true
+
+-- Hi Mylon, yea i didn't for-in data.raw['reactor'] since i'm still brainstorming, also why the mod's name is singular :)
