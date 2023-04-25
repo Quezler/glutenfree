6eb6770c6d6b5c8547f5ba3707af3963eb40381d
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-04-25 20:53:19 +0200

    Publish se-instant-plague-rocket

---
+++ mods/se-instant-plague-rocket/README.md
@@ -0,0 +1,1 @@
+Waiting 30 minutes / forced to wait at least 5 minutes / dealing with a 1.2 GB desync report? no thank you.
---
+++ mods/se-instant-plague-rocket/settings.lua
@@ -0,0 +1,11 @@
+data.raw["int-setting"]["se-plague-max-runtime-2"] = nil
+
+data:extend({
+  {
+    type = "double-setting",
+    name = "se-plague-max-runtime-2",
+    setting_type = "runtime-global",
+    default_value = 1 / 60,
+    hidden = true
+  }
+})
