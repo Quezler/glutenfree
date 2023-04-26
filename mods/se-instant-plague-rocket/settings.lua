6352112f28e8f4415bd96b8384a22c7e02847bff
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-04-26 10:13:09 +0200

    Fix it running for a second and not just a tick

--- mods/se-instant-plague-rocket/settings.lua
+++ mods/se-instant-plague-rocket/settings.lua
@@ -5,7 +5,7 @@ data:extend({
     type = "double-setting",
     name = "se-plague-max-runtime-2",
     setting_type = "runtime-global",
-    default_value = 1 / 60,
+    default_value = 1 / 60 / 60,
     hidden = true
   }
 })
