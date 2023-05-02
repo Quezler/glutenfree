2fd60a172304a1e720650f39081da76a591552c5
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-02 09:48:14 +0200

    Move the core miner helper mod to this repository

---
+++ mods/se-core-miner-output-helper/README.md
@@ -0,0 +1,2 @@
+Figuring out exactly how many core fragments per second a surface is making can be annoying,
+this helper mod tells you the total amount you're after, as well as the rough formula used for it.
---
+++ mods/se-core-miner-output-helper/control.lua
@@ -0,0 +1,33 @@
+
+local function round(number, decimals)
+  local multiplier = 10 ^ (decimals or 0)
+  return math.floor(number * multiplier + 0.5) / multiplier
+end
+
+--
+
+script.on_event(defines.events.on_selected_entity_changed, function(event)
+  local player = game.get_player(event.player_index)
+  local entity = player.selected
+
+  if entity and entity.name == "se-core-miner-drill" and entity.mining_target then
+
+    local core_miners_on_this_surface = #entity.surface.find_entities_filtered({name = "se-core-miner-drill"})
+    local fragment_name = entity.mining_target.prototype.mineable_properties.products[1].name
+    local fragments_per_second = (entity.prototype.mining_speed / entity.mining_target.prototype.mineable_properties.mining_time) * entity.mining_target.amount / entity.mining_target.prototype.normal_resource_amount
+
+    local text = {}
+    text[1] = "[item="..fragment_name.."]"
+    text[2] = math.floor(fragments_per_second * 10) / 10 .. "/s"
+    text[3] = "+"
+    text[4] = entity.force.mining_drill_productivity_bonus * 10 .. "0%"
+    text[5] = "x"
+    text[6] = core_miners_on_this_surface
+    text[7] = "="
+    text[8] = "[item="..fragment_name.."]"
+    text[9] = round(fragments_per_second * (1 + entity.force.mining_drill_productivity_bonus) * core_miners_on_this_surface, 2) .. "/s"
+
+    player.create_local_flying_text({text = table.concat(text, " "), position = entity.position})
+  end
+
+end)
