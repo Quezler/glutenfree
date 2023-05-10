d01984d54a0b46a46cbaf827025e25519f7f4824
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-10 11:03:35 +0200

    Did you put your commit in the goblet of github?

---
+++ mods/informatron-didu-pt-yer-name-in-da-goblet-of-fiya/README.lua
@@ -0,0 +1,1 @@
+Who thought opening it after 20 seconds (even on a 50x50 and/or /cheat world) was a good idea?
---
+++ mods/informatron-didu-pt-yer-name-in-da-goblet-of-fiya/settings.lua
@@ -0,0 +1,2 @@
+data.raw['bool-setting']['informatron-show-at-start'].hidden = true
+data.raw['bool-setting']['informatron-show-at-start'].forced_value = false
