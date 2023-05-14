f04203b75240d6443008172262a742f7efc7aa8d
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-14 16:23:18 +0200

    Publish the there-are-still-hostiles-detected mod

---
+++ mods/se-there-are-still-hostiles-detected/README.md
@@ -0,0 +1,3 @@
+When you press the "confirm hostile extinction" button and there are still hostiles left it'll show text at your cursor.
+
+Obviously counting all the entities has some performance impact, there is currently no caching or sensible limit in place.
---
+++ mods/se-there-are-still-hostiles-detected/control.lua
@@ -0,0 +1,20 @@
+script.on_event(defines.events.on_gui_click, function(event)
+  if not event.element.valid then return end
+  if event.element.tags.action ~= 'confirm-extinction' then return end
+
+  local zone = remote.call("space-exploration", "get_zone_from_zone_index", {zone_index = event.element.tags.zone_index})
+  local surface = game.get_surface(zone.surface_index) -- don't check for a nil zone first since how could it even be one
+
+  local enemies = surface.find_entities_filtered{
+    force = "enemy", -- currently no support for SE's enemy_forces(force) for pvp scenarios and god knows which other mods
+    type = {"unit-spawner", "turret"},
+  }
+
+  if #enemies == 0 then return end
+
+  local player = game.get_player(event.player_index)
+  player.create_local_flying_text{
+    text = tostring(#enemies) .. ' nests/worms left', -- don't care about singular since it'll only be 1 for like a second
+    create_at_cursor = true,
+  }
+end)
