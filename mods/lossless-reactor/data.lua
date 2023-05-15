ef54f94f6bd182ce9155aaa11c3f335c58a5368a
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-15 18:23:42 +0200

    Support the space exploration antimatter reactor
    Yes yes, the mod’s description still says vanilla, but i didn’t want to make a secondary mod yet, maybe i’ll turn it into a startup setting someday but for now its just a hidden extra purpose.

--- mods/lossless-reactor/data.lua
+++ mods/lossless-reactor/data.lua
@@ -1,4 +1,6 @@
 -- When this is true, the reactor will stop consuming fuel/energy when the temperature has reached the maximum.
 data.raw['reactor']['nuclear-reactor'].scale_energy_usage = true
 
--- Hi Mylon, yea i didn't for-in data.raw['reactor'] since i'm still brainstorming, also why the mod's name is singular :)
+if mods['space-exploration'] then
+  data.raw['reactor']['se-antimatter-reactor'].scale_energy_usage = true
+end
