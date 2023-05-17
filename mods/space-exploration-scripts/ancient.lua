d00ea4c65fede67adac63b3db6fc339514645e8b
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-17 22:34:03 +0200

    Support ancient

---
+++ mods/space-exploration-scripts/ancient.lua
@@ -0,0 +1,15 @@
+-- step 1
+mod_prefix = 'se-'
+core_util = require('__core__/lualib/util.lua')
+Event = {addListener = function() end}
+
+-- step 2
+local Ancient = require('__space-exploration__.scripts.ancient')
+
+-- step 3
+Event = nil
+core_util = nil
+mod_prefix = nil
+
+-- step 4
+return Ancient
