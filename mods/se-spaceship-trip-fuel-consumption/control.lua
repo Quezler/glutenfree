8eca677156d1b736da658b1b3130e03b35e73320
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-18 13:39:52 +0200

    Remember last known fuel consumption after anchoring

--- mods/se-spaceship-trip-fuel-consumption/control.lua
+++ mods/se-spaceship-trip-fuel-consumption/control.lua
@@ -15,11 +15,11 @@ handler.on_init = function(event)
       global.spaceship_console_outputs[spaceship_id] = entity
     end
   end
+
+  global.spaceship_sum = {}
 end
 
 handler.on_entity_cloned = function(event)
-  -- event.destination.products_finished = 0
-
   if event.destination.name == "se-spaceship-console-output" then
     local spaceship_id = event.destination.get_or_create_control_behavior().get_signal(1).count
     global.spaceship_console_outputs[spaceship_id] = event.destination
@@ -37,6 +37,10 @@ handler.on_tick = function(event)
     for _, player in pairs(game.connected_players) do
       handler.gui_update(player, event.tick)
     end
+  end
+
+  if event.tick % (60 * 60) == 0 then -- every minute
+    handler.gc()
   end
 end
 
@@ -56,31 +60,30 @@ handler.gui_update = function(player, tick)
           name = {'se-spaceship-rocket-engine', 'se-spaceship-ion-engine', 'se-spaceship-antimatter-engine'}
         }
 
-        if root["flow_speed"] and root["flow_speed"]["panel_speed"] then
-          local panel = root["flow_speed"]["panel_speed"]
+        global.spaceship_sum[spaceship_id] = handler.sum_fuel_usage_for_engines(engines)
+      end
 
-          -- this doesn't show in the 1-4 ticks between opening the dropdown, but its too unnoticable for now
-          if not panel['fuel_usage_label'] then
-            panel.add{
-              type = "label",
-              name = "fuel_usage_label"
-            }
-          end
+      if root["flow_speed"] and root["flow_speed"]["panel_speed"] then
+        local panel = root["flow_speed"]["panel_speed"]
 
-          local sum = handler.sum_fuel_usage_for_engines(engines)
-          -- game.print(serpent.block( sum ))
-
-          local caption = {}
-          for fuel_name, fuel_amount in pairs(sum) do
-            if fuel_amount > -1 then
-              table.insert(caption, "[fluid=" .. fuel_name .. "] " .. fuel_amount)
-            end
-          end
-
-          panel['fuel_usage_label'].caption = table.concat(caption, " ")
+        -- this doesn't show in the 1-4 ticks between opening the dropdown, but its too unnoticable for now
+        if not panel['fuel_usage_label'] then
+          panel.add{
+            type = "label",
+            name = "fuel_usage_label"
+          }
         end
 
+        local caption = {}
+        for fuel_name, fuel_amount in pairs(global.spaceship_sum[spaceship_id] or {}) do
+          if fuel_amount > -1 then
+            table.insert(caption, "[fluid=" .. fuel_name .. "] " .. fuel_amount)
+          end
+        end
+
+        panel['fuel_usage_label'].caption = table.concat(caption, " ")
       end
+
     end
   end
 end
@@ -103,17 +106,6 @@ handler.liquid_used_per_fuel_craft = {
 }
 
 handler.sum_fuel_usage_for_engines = function(engines)
-  -- local sum = 0
-
-  -- for _, engine in ipairs(engines) do
-  --   local struct = global.engines[engine.unit_number]
-  --   if struct and struct.entity.valid then
-  --     sum = sum + (engine.products_finished - struct.products_finished)
-  --   end
-  -- end
-
-  -- return sum
-
   -- -1 means the engine type is missing, if the engine is present but unused math.max below will set it to 0
   local sum = {
     ['se-liquid-rocket-fuel'] = -1,
@@ -135,7 +127,10 @@ end
 handler.gc = function()
   -- garbage created when a spaceship gets decomissioned
   for spaceship_id, entity in pairs(global.spaceship_console_outputs) do
-    if not entity.valid then global.spaceship_console_outputs[spaceship_id] = nil end
+    if not entity.valid then
+      global.spaceship_console_outputs[spaceship_id] = nil
+      global.spaceship_sum[spaceship_id] = nil
+    end
   end
 
   -- garbage generally left behind a spaceship launching
