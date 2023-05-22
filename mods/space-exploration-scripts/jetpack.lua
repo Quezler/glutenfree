01b2b6a7525e19333cb15a4c2007d5cbcde0a955
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-22 20:23:50 +0200

    Fix jetpack crashing if the player did self harm

--- mods/space-exploration-scripts/jetpack.lua
+++ mods/space-exploration-scripts/jetpack.lua
@@ -2,7 +2,9 @@
 Event = {addListener = function() end}
 
 -- step 2
+local oed = script.get_event_handler(defines.events.on_entity_damaged)
 local Jetpack = require('__jetpack__.scripts.jetpack')
+script.on_event(defines.events.on_entity_damaged, oed)
 
 -- step 3
 Event = nil
