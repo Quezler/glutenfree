9a75010f620be95eff84b9d9fecdde878d6d5bd1
Patrick ‘Quezler’ Mounier <patrick@queztech.net>
2023-05-22 20:38:14 +0200

    Fix another jetpack event registered without Event

--- mods/space-exploration-scripts/jetpack.lua
+++ mods/space-exploration-scripts/jetpack.lua
@@ -3,7 +3,9 @@ Event = {addListener = function() end}
 
 -- step 2
 local oed = script.get_event_handler(defines.events.on_entity_damaged)
+local odst = script.get_event_handler(defines.events.on_player_driving_changed_state)
 local Jetpack = require('__jetpack__.scripts.jetpack')
+script.on_event(defines.events.on_player_driving_changed_state, odst)
 script.on_event(defines.events.on_entity_damaged, oed)
 
 -- step 3
