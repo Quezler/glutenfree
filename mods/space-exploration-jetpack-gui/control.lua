b11c93f7f0e6f4f2b15a7e1f1493859b826aded5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-05-31 19:52:45 +0200

    Fix crash for new worlds?

--- mods/space-exploration-jetpack-gui/control.lua
+++ mods/space-exploration-jetpack-gui/control.lua
@@ -14,12 +14,12 @@ function JetpackGUI.on_configuration_changed(event)
   local compatible_fuels = remote.call("jetpack", "get_fuels")
   global.compatible_fuels = {}
 
-  if not compatible_fuels["solid-fuel"] then
+  if compatible_fuels and compatible_fuels["solid-fuel"] then
+    global.compatible_fuels = compatible_fuels
+  else
     for _, compatible_fuel in pairs(compatible_fuels) do
       global.compatible_fuels[compatible_fuel.fuel_name] = {thrust = compatible_fuel.thrust}
     end
-  else
-    global.compatible_fuels = compatible_fuels
   end
 
   -- {
