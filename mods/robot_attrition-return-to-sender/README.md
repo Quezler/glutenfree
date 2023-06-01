78dcb3d1ce3f2197523b1af4964491177338384e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-06-01 20:22:14 +0200

    Utilize the trash slots instead of abandoning
    Suggestion by Mernom @ https://discord.com/channels/139677590393716737/231518135231053825/1113878909101031434

--- mods/robot_attrition-return-to-sender/README.md
+++ mods/robot_attrition-return-to-sender/README.md
@@ -1,6 +1,5 @@
-if a personal construction robot picks up dropped cargo, it'll abandon you and take its cargo to the closest logistic network.
-
-you are responsible for ensuring your inventory has enough construction robots, the bots do not return to you after dropping off.
+~~if a personal construction robot picks up dropped cargo, it'll abandon you and take its cargo to the closest logistic network.~~
+if a personal construction robot picks up dropped cargo, it'll put it in your trash slots instead of into your main inventory.
 
 considered names:
 - return to sender
--- mods/robot_attrition-return-to-sender/control.lua
+++ mods/robot_attrition-return-to-sender/control.lua
@@ -11,64 +11,47 @@ function Handler.on_init()
 end
 
 function Handler.on_load()
-  if global.construction_robots and #global.construction_robots > 0 then
+  if Handler.should_on_tick() then
     script.on_event(defines.events.on_tick, Handler.on_tick)
   end
+end
+
+function Handler.should_on_tick()
+  return global.construction_robots and #global.construction_robots > 0
 end
 
 function Handler.on_tick(event)
   for _, construction_robot in ipairs(global.construction_robots) do
     if construction_robot.valid then
-      Handler.fitting_name(construction_robot)
+      Handler.after_robot_visited_cargo(construction_robot)
     end
   end
 
   global.construction_robots = {}
+  -- if Handler.should_on_tick() then return end
   script.on_event(defines.events.on_tick, nil)
 end
 
 script.on_event(defines.events.on_robot_pre_mined, function(event)
+  if not logistic_network_is_personal_roboport(event.robot.logistic_network) then return end
+
   table.insert(global.construction_robots, event.robot)
   script.on_event(defines.events.on_tick, Handler.on_tick)
 end, {{filter = 'name', name = 'logistic-robot-dropped-cargo'}})
 
-function Handler.fitting_name(robot)
+function Handler.after_robot_visited_cargo(robot)
   local cargo = robot.get_inventory(defines.inventory.robot_cargo)
   if cargo.is_empty() then return end -- picked up an empty parcel
 
-  if not logistic_network_is_personal_roboport(robot.logistic_network) then return end
+  -- what if a player died in the tick a robot picked up dropped cargo? i guess we'll await the reports
+  local trash = robot.logistic_network.cells[1].owner.get_inventory(defines.inventory.character_trash)
 
-  -- by construction area
-  for _, logistic_network in ipairs(robot.surface.find_logistic_networks_by_construction_area(robot.position, robot.force)) do
-    if not logistic_network_is_personal_roboport(logistic_network) then
-      robot.logistic_network = logistic_network
-      return
-    end
-  end
+  -- just teleport the items to the player instantly so we don't have to check each tick if the bot has arrived home yet
+  local count = cargo[1].count
+  local inserted = trash.insert(cargo[1])
 
-  -- by surface
-  local bot = robot.surface.create_entity{
-    name = robot.name,
-    force = robot.force,
-    position = robot.position,
-  }
-
-  local logistic_network = bot.logistic_network
-  bot.destroy()
-
-  if logistic_network then
-    robot.logistic_network = logistic_network
-    return
-  end
-
-  -- will i ever forgive myself for allowing several dropped cargo's to stack for this edge case?
-  local drop = robot.surface.create_entity{
-    name = "logistic-robot-dropped-cargo",
-    position = robot.position,
-    force = robot.force
-  }
-  local item_stack = cargo[1]
-  drop.get_inventory(defines.inventory.chest)[1].transfer_stack(item_stack)
+  -- if this crash occurs often we might have to temporarily increase `character_trash_slot_count_bonus` or something
+  if count ~= inserted then error('trash slots full?') end
 end
 
 --
