4e018d4495ccb5fae49863b0a8257170bc92ddf7
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-11-27 14:14:36 +0100

    Settle on final prototypes.lua format

--- mods/configurable-blueprints-v1/tools/prototypes.js
+++ mods/configurable-blueprints-v1/tools/prototypes.js
@@ -1,19 +1,16 @@
 const prototypes_in = Bun.file("./tools/prototypes.lua");
 const prototypes_out = Bun.file("./scripts/prototypes.lua");
 
-// console.log(import.meta.url);
-
 let lines = (await prototypes_in.text()).split('\n');
 lines.pop();
 
 lines.forEach((line, i) => {
   const parts = line.split(' - ');
   parts[1] = parts[1].padEnd(27, ' ');
-  lines[i] = `  ` + parts.reverse().join(', - ');
+  lines[i] = `  ` + parts.reverse().join(', -- ');
 });
 
 lines.unshift('return {');
-lines.unshift('local abstract = "abstract" -- eh this just felt easier than coming up with a different name or comment strategy :)');
 lines.push('}');
 
 await Bun.write(prototypes_out, lines.join('\n'));
