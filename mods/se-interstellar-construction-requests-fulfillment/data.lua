2132283f46b72225cee973104aa631f9a765f5e7
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-07 17:19:31 +0100

    Switch to an ammo turret since it has the right properties

--- mods/se-interstellar-construction-requests-fulfillment/data.lua
+++ mods/se-interstellar-construction-requests-fulfillment/data.lua
@@ -1,16 +1,16 @@
-local turret = table.deepcopy(data.raw['electric-turret']['se-meteor-point-defence-charger'])
-turret.name = 'se-interstellar-construction-requests-fulfillment--turret'
-turret.flags = {"placeable-player", "player-creation"}
-turret.selectable_in_game = true
+local turret = table.deepcopy(data.raw['ammo-turret']['se-meteor-point-defence-container'])
+turret.name = 'se-interstellar-construction-requests-fulfillment-turret'
+-- turret.flags = {"placeable-player", "player-creation"}
+-- turret.selectable_in_game = true
 
 turret.base_picture.layers[2].tint = {r=244, g=209, b=6}
 turret.base_picture.layers[2].hr_version.tint = turret.base_picture.layers[2].tint
 
-turret.collision_mask = {
-  "water-tile",
-  "item-layer",
-  "object-layer",
-  "player-layer",
-},
+-- turret.collision_mask = {
+--   "water-tile",
+--   "item-layer",
+--   "object-layer",
+--   "player-layer",
+-- },
 
 data:extend({turret})
