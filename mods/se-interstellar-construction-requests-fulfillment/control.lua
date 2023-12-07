1794c2b317d24b132c763f4de53fd6e43a38e477
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-07 21:11:59 +0100

    Register interstellar construction turrets in global

--- mods/se-interstellar-construction-requests-fulfillment/control.lua
+++ mods/se-interstellar-construction-requests-fulfillment/control.lua
@@ -1,3 +1,32 @@
+script.on_init(function(event)
+  global.entities = {}
+end)
+
+function on_created_entity(event)
+  local entity = event.created_entity or event.entity or event.destination
+
+  global.entities[entity.unit_number] = entity
+end
+
+for _, event in ipairs({
+  defines.events.on_built_entity,
+  defines.events.on_robot_built_entity,
+  defines.events.script_raised_built,
+  defines.events.script_raised_revive,
+  defines.events.on_entity_cloned,
+}) do
+  script.on_event(event, on_created_entity, {
+    {filter = 'name', name = 'se-interstellar-construction-requests-fulfillment--turret'},
+  })
+end
+
+script.on_nth_tick(60, function(event)
+  for unit_number, entity in pairs(global.entities) do
+    game.print(entity.energy)
+    game.print(game.entity_prototypes[entity.name].electric_energy_source_prototype.buffer_capacity)
+  end
+end)
+
 script.on_nth_tick(600, function(event)
 
   for _, player in ipairs(game.connected_players) do
