b6dd8abc32d8db52a0a3bb122d071aa0ac1b441c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-07 17:43:43 +0100

    Switch to electric turret whilst preserving the ammo turret source

--- mods/se-interstellar-construction-requests-fulfillment/data.lua
+++ mods/se-interstellar-construction-requests-fulfillment/data.lua
@@ -1,5 +1,6 @@
 local turret = table.deepcopy(data.raw['ammo-turret']['se-meteor-point-defence-container'])
-turret.name = 'se-interstellar-construction-requests-fulfillment-container'
+turret.type = 'electric-turret'
+turret.name = 'se-interstellar-construction-requests-fulfillment-turret'
 
 local tint = {r=244, g=209, b=6}
 turret.base_picture.layers[2].tint = tint
@@ -10,5 +11,11 @@ turret.icons = {
   {icon = '__space-exploration-graphics__/graphics/icons/meteor-point-defence-base.png', icon_size = 64},
   {icon = '__space-exploration-graphics__/graphics/icons/meteor-point-defence-mask.png', icon_size = 64, tint = tint}
 }
+
+turret.energy_source = table.deepcopy(data.raw['electric-turret']['se-meteor-point-defence-charger']).energy_source
+turret.attack_parameters = table.deepcopy(data.raw['electric-turret']['se-meteor-point-defence-charger']).attack_parameters
+
+turret.localised_name = nil
+turret.localised_description = nil
 
 data:extend({turret})
