22827f7633526b694e91c882091c07da7640fb2f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-08 15:31:45 +0100

    Simplify attack parameters

--- mods/se-interstellar-construction-requests-fulfillment/data.lua
+++ mods/se-interstellar-construction-requests-fulfillment/data.lua
@@ -19,40 +19,15 @@ turret.energy_source = {
   usage_priority = "primary-input"
 }
 
-turret.attack_parameters = table.deepcopy(data.raw['electric-turret']['laser-turret']).attack_parameters
-turret.attack_parameters.ammo_type.energy_consumption = "1GJ"
-
-log('search for this!')
-log(serpent.block( turret.attack_parameters ))
-
--- 1.909 Script @__se-interstellar-construction-requests-fulfillment__/data.lua:26: {
---   ammo_type = {
---     action = {
---       action_delivery = {
---         beam = "laser-beam",
---         duration = 40,
---         max_length = 24,
---         source_offset = {
---           0,
---           -1.3143899999999999
---         },
---         type = "beam"
---       },
---       type = "direct"
---     },
---     category = "laser",
---     energy_consumption = "1GJ"
---   },
---   cooldown = 40,
---   damage_modifier = 2,
---   range = 24,
---   source_direction_count = 64,
---   source_offset = {
---     0,
---     -0.85587225
---   },
---   type = "beam"
--- }
+turret.attack_parameters = {
+  ammo_type = {
+    category = "se-meteor-defence",
+    energy_consumption = "1GJ",
+  },
+  range = 1,
+  cooldown = 1,
+  type = "beam",
+}
 
 turret.localised_name = nil
 turret.localised_description = nil
