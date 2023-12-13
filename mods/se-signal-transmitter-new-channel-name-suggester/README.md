e11b2b6c715487054ee8dccbb111c1905e362f1d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-13 14:39:54 +0100

    Publish se-signal-transmitter-new-channel-name-suggester

---
+++ mods/se-signal-transmitter-new-channel-name-suggester/README.md
@@ -0,0 +1,1 @@
+Click the edit button next to the default channel name and the text area should be prefilled with the rich text icon and the zone's name.
---
+++ mods/se-signal-transmitter-new-channel-name-suggester/control.lua
@@ -0,0 +1,31 @@
+local Zone = require('__space-exploration-scripts__.zone')
+
+local mod_prefix = "" -- should be `aai-` for versions after v0.4.9
+local default_channel = "Default"
+
+script.on_event(defines.events.on_gui_click, function(event)
+  if event.element and event.element.name == mod_prefix .. "change_channel" then
+    if event.element.parent.children[1].caption == default_channel then
+      local player = game.get_player(event.player_index)
+      local coin = {name = "coin", count = 1}
+      if player.get_main_inventory().insert(coin) then
+        player.get_main_inventory().remove(coin)
+      end
+    end
+  end
+end)
+
+script.on_event(defines.events.on_player_main_inventory_changed, function(event)
+  local player = game.get_player(event.player_index)
+
+  if player.opened and player.opened.type == "frame" and player.opened.name == "aai-signal-sender" then
+    local write_channel = player.opened.children[2].children[2].children[1].children[1]
+    if write_channel.text ~= default_channel then return end
+
+    local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = player.surface.index})
+    if not zone then return end
+
+    write_channel.text = Zone._get_rich_text_name(zone)
+    write_channel.select_all()
+  end
+end)
