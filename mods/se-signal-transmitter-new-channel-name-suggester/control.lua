276218da1610685ce3a4be28e158cab6828845e3
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-19 14:41:06 +0100

    Clearing the textfield (rightclick or backspace when everything is selected) now triggers the mod the same way `Default` does

--- mods/se-signal-transmitter-new-channel-name-suggester/control.lua
+++ mods/se-signal-transmitter-new-channel-name-suggester/control.lua
@@ -3,8 +3,17 @@ local Zone = require('__space-exploration-scripts__.zone')
 local mod_prefix = "" -- should be `aai-` for versions after v0.4.9
 local default_channel = "Default"
 
+script.on_init(function(event)
+  global.player_keystrokes_since_editing_channel = {}
+end)
+
+script.on_configuration_changed(function(event)
+  global.player_keystrokes_since_editing_channel = global.player_keystrokes_since_editing_channel or {}
+end)
+
 script.on_event(defines.events.on_gui_click, function(event)
   if event.element and event.element.name == mod_prefix .. "change_channel" then
+    global.player_keystrokes_since_editing_channel[event.player_index] = 0
     if event.element.parent.children[1].caption == default_channel then
       local player = game.get_player(event.player_index)
       local coin = {name = "coin", count = 1}
@@ -29,5 +38,21 @@ script.on_event(defines.events.on_player_main_inventory_changed, function(event)
 
     write_channel.text = Zone._get_rich_text_name(zone)
     write_channel.select_all()
+  end
+end)
+
+script.on_event(defines.events.on_gui_text_changed, function(event)
+  if event.element and event.element.name == mod_prefix .. "write-channel" then
+    global.player_keystrokes_since_editing_channel[event.player_index] = global.player_keystrokes_since_editing_channel[event.player_index] + 1
+
+    if event.element.text == "" and global.player_keystrokes_since_editing_channel[event.player_index] == 1 then
+      local player = game.get_player(event.player_index)
+      
+      local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = player.surface.index})
+      if not zone then return end
+  
+      event.element.text = Zone._get_rich_text_name(zone)
+      event.element.select_all()
+    end
   end
 end)
