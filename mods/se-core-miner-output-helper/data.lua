55b4335eaedeee69cf1871e9198e8efdc58f48d4
Patrick 'Quezler' Mounier <patrick@queztech.net>
2023-12-19 13:23:34 +0100

    Fix missing style & updated the description

---
+++ mods/se-core-miner-output-helper/data.lua
@@ -0,0 +1,8 @@
+local style = data.raw["gui-style"]["default"]
+
+style.se_zonelist_zone_data_label_link = {
+  type = "label_style",
+  parent = "se_zonelist_zone_data_label",
+  font = "default-semibold",
+  font_color = {1, 1, 1},
+}
