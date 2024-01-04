e5a7815b9d76ac6f9da04ef498583a2ffd4ae8f2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-04 11:00:27 +0100

    Publish no--inventory-is-full--corpse-pickup-troubles

---
+++ mods/no--inventory-is-full--corpse-pickup-troubles/README.md
@@ -0,0 +1,7 @@
+When you loot a corpse via mining it inserts the items in order, which is a problem if you have more items than your body can hold without armor.
+
+And since worn armor is typically last in the corpse it tries to pick that up last, which often resorts to many "inventory is full" messages.
+
+This mod inserts the best* armor from the corpse into your armor slot (if its empty) before letting the game insert all the other items.
+
+Thumbnail is a reference to Wolfenstein ET, where the first thing you do to a corpse is take its uniform.
---
+++ mods/no--inventory-is-full--corpse-pickup-troubles/control.lua
@@ -0,0 +1,37 @@
+local function is_better_armor(old_stack, new_stack)
+  if old_stack == nil then return true end
+  if new_stack.prototype.inventory_size_bonus > old_stack.prototype.inventory_size_bonus then return true end
+
+  -- todo: if both the inventory stack size bonuses match and space exploration is installed, pick normal or spacesuit?
+  -- todo: if both the inventory stack size bonuses match, determine which suit has better resistances/equipment in it.
+
+  return false
+end
+
+script.on_event(defines.events.on_pre_player_mined_item, function(event)
+  local player = game.get_player(event.player_index)
+  local armor = player.get_inventory(defines.inventory.character_armor)
+
+  if not armor.is_empty() then return end
+  -- game.print(event.entity.name)
+
+  local best_armor = nil
+
+  local corpse = event.entity.get_inventory(defines.inventory.character_corpse)
+  for i = 1, #corpse do
+    local stack = corpse[i]
+    if stack.valid_for_read and stack.is_armor then
+      best_armor = is_better_armor(best_armor, stack) and stack or best_armor
+    end
+  end
+
+  if best_armor then
+    -- game.print(best_armor.name)
+    best_armor.swap_stack(armor[1])
+  else
+    -- game.print('nil')
+    -- corpse had no armor
+  end
+end, {
+  {filter = "type", type = "character-corpse"},
+})
