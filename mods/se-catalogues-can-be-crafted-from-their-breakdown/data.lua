e20b404dbdb38f2d03b0184a08c51022cc7aca80
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-10 22:38:04 +0100

    Find out which recipe creates what
    There will probably need to be some finetuning by hand about which recipe is unlocked when and sciences that could allow for better paths to something, and perhaps some data cards will remain at play.

--- mods/se-catalogues-can-be-crafted-from-their-breakdown/data.lua
+++ mods/se-catalogues-can-be-crafted-from-their-breakdown/data.lua
@@ -1,8 +1,55 @@
+local function get_recipe_results(recipe)
+  if recipe.results then return recipe.results end
+
+  if recipe.normal then return get_recipe_results(recipe.normal) end
+
+  return {{type = "item", name = recipe.result, amount = recipe.result_count or 1}}
+end
+
+local function get_full_recipe_results(recipe)
+  local results = get_recipe_results(recipe)
+  for i, result in pairs(results) do
+    if result[1] and result[2] then
+      results[i] = {type = "item", name = result[1], amount = result[2]}
+    end
+    result.type = result.type or "item"
+  end
+
+  return results
+end
+
+-- log(serpent.block(data.raw['recipe']))
+-- error()
+
+local recipes_for = {}
+for _, recipe in pairs(data.raw['recipe']) do
+  -- log(recipe.name)
+  for _, result in pairs(get_full_recipe_results(recipe)) do
+    if result.type == "item" or result.type == nil then
+      -- log(recipe.name .. serpent.line(result))
+      -- log(serpent.block(recipe))
+      -- log(serpent.block(result))
+      recipes_for[result.name] = recipes_for[result.name] or {}
+      recipes_for[result.name][recipe.name] = true
+    end
+  end
+end
+
+log(serpent.block(recipes_for))
+error()
+
+local function handle_catalogue(item_name)
+  log(serpent.block(data.raw['recipe']['item-name']))
+  error()
+end
+
 for _, item in pairs(data.raw['item']) do
   if string.match(item.name, "-catalogue-") then
     log(item.name)
   end
 end
+
+handle_catalogue('se-astronomic-catalogue-1')
 
 -- se-astronomic-catalogue-1
 -- se-astronomic-catalogue-2
@@ -27,4 +74,4 @@ end
 -- se-kr-matter-catalogue-1
 -- se-kr-matter-catalogue-2
 
-error()
+-- error()
