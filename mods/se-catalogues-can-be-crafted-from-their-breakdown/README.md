0d0a646fd1f5f519d83d4c2e60044bb7e1f64a4c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-10 21:29:26 +0100

    Stash se-catalogues-can-be-crafted-from-their-breakdown

---
+++ mods/se-catalogues-can-be-crafted-from-their-breakdown/README.md
@@ -0,0 +1,5 @@
+My playthroughs often stagnate due to no-one participating in the run bothering/caring to setup new science tiers since the ratios are mind-boggling.
+
+This mod adds an alternative catalogue recipe in where you would just input the ~~raw~~ intermediate ingredients to get the same result.
+
+Now i'm able to focus on growing my infrastructure and playing with techs i could already afford but without large setups.
---
+++ mods/se-catalogues-can-be-crafted-from-their-breakdown/data.lua
@@ -0,0 +1,30 @@
+for _, item in pairs(data.raw['item']) do
+  if string.match(item.name, "-catalogue-") then
+    log(item.name)
+  end
+end
+
+-- se-astronomic-catalogue-1
+-- se-astronomic-catalogue-2
+-- se-astronomic-catalogue-3
+-- se-astronomic-catalogue-4
+-- se-biological-catalogue-1
+-- se-biological-catalogue-2
+-- se-biological-catalogue-3
+-- se-biological-catalogue-4
+-- se-energy-catalogue-1
+-- se-energy-catalogue-2
+-- se-energy-catalogue-3
+-- se-energy-catalogue-4
+-- se-material-catalogue-1
+-- se-material-catalogue-2
+-- se-material-catalogue-3
+-- se-material-catalogue-4
+-- se-deep-catalogue-1
+-- se-deep-catalogue-2
+-- se-deep-catalogue-3
+-- se-deep-catalogue-4
+-- se-kr-matter-catalogue-1
+-- se-kr-matter-catalogue-2
+
+error()
