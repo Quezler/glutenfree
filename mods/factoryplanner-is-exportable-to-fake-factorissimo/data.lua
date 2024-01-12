0e08f8a11f9a8c6cb15b71ea684efd975af1f20c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-12 12:45:31 +0100

    Stash failed attempt using just a template

--- mods/factoryplanner-is-exportable-to-fake-factorissimo/data.lua
+++ mods/factoryplanner-is-exportable-to-fake-factorissimo/data.lua
@@ -1,47 +1,45 @@
 local mod_prefix = 'fietff-'
 local mod_path = '__factoryplanner-is-exportable-to-fake-factorissimo__'
 
-local function create_container(config)
-  return {
-    type = 'container',
-    name = mod_prefix .. 'container-' .. config.i,
+local container_template = {
+  type = 'container',
+
+  icon = string.format(mod_path .. '/graphics/icon/factory-%d.png', config.i),
+
+  enable_inventory_bar = false,
+
+  flags = {
+    'placeable-player',
+    'player-creation',
+    'hide-alt-info',
+  },
   
-    icon = string.format(mod_path .. '/graphics/icon/factory-%d.png', config.i),
-    icon_size = 64,
-  
-    collision_box = config.collision_box,
-    selection_box = config.selection_box,
-  
-    inventory_size = 10 * config.i,
-    enable_inventory_bar = false,
-  
-    flags = {
-      'placeable-player',
-      'player-creation',
-      'hide-alt-info',
-    },
-  
-    minable = {mining_time = 0.2 * config.i},
-    
-    picture = {
-      layers = {
-        {
-          filename = string.format(mod_path .. '/graphics/factory/factory-%d-shadow.png', config.i),
-          width = 416,
-          height = 320,
-          shift = config.shift,
-          draw_as_shadow = true
-        },
-        {
-          filename = string.format(mod_path .. '/graphics/factory/factory-1-alt.png', config.i),
-          width = 416,
-          height = 320,
-          shift = config.shift,
-        }
+  picture = {
+    layers = {
+      {
+        filename = string.format(mod_path .. '/graphics/factory/factory-%d-shadow.png', config.i),
+        width = 416,
+        height = 320,
+        shift = config.shift,
+        draw_as_shadow = true
+      },
+      {
+        filename = string.format(mod_path .. '/graphics/factory/factory-1-alt.png', config.i),
+        width = 416,
+        height = 320,
+        shift = config.shift,
       }
     }
   }
-end
+}
+
+local container_1 = table.deepcopy(container_template)
+container_1.name = mod_prefix .. 'container-1'
+container_1.icon = mod_path .. '/graphics/icon/factory-1.png'
+container_1.collision_box = {{-3.8, -3.8}, {3.8, 3.8}}
+container_1.selection_box = {{-3.8, -3.8}, {3.8, 3.8}}
+container_1.inventory_size = 10 * 1
+container_1.minable = {mining_time = 0.5 * 1,
 
 local container_1 = create_container({
   i = 1,
