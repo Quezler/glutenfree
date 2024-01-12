f3818104df5577d6d076897aab3ded672d8fac8e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-12 15:34:00 +0100

    Cleanup item box extraction

--- mods/factoryplanner-is-exportable-to-fake-factorissimo/control.lua
+++ mods/factoryplanner-is-exportable-to-fake-factorissimo/control.lua
@@ -1,8 +1,12 @@
 local print_gui = require('scripts.print_gui')
 
-local function products(root)
+local item_box_products = 1
+local item_box_byproducts = 2
+local item_box_ingredients = 3
+
+local function get_item_box_contents(root, item_box_index)
   local products = {}
-  for _, sprite_button in ipairs(root.children[2].children[2].children[1].children[1].children[2].children[1].children[1].children) do
+  for _, sprite_button in ipairs(root.children[2].children[2].children[1].children[item_box_index].children[2].children[1].children[1].children) do
     if sprite_button.sprite ~= "utility/add" then
       local left, right = sprite_button.sprite:match('([^/]+)/([^/]+)')
       table.insert(products, {type = left, name = right, amount = sprite_button.number})
@@ -11,43 +15,18 @@ local function products(root)
   return products
 end
 
-local function byproducts(root)
-  local byproducts = {}
-  for _, sprite_button in ipairs(root.children[2].children[2].children[1].children[2].children[2].children[1].children[1].children) do
-    local left, right = sprite_button.sprite:match('([^/]+)/([^/]+)')
-    table.insert(byproducts, {type = left, name = right, amount = sprite_button.number})
-  end
-  return byproducts
-end
-
-
-local function ingredients(root)
-  local ingredients = {}
-  for _, sprite_button in ipairs(root.children[2].children[2].children[1].children[3].children[2].children[1].children[1].children) do
-    local left, right = sprite_button.sprite:match('([^/]+)/([^/]+)')
-    table.insert(ingredients, {type = left, name = right, amount = sprite_button.number})
-  end
-  return ingredients
-end
-
 script.on_event(defines.events.on_gui_opened, function(event)
   if event.gui_type ~= defines.gui_type.custom then return end
   if event.element.name ~= "fp_frame_main_dialog" then return end
   game.print(event.element.name)
 
-  -- log(print_gui.serpent( event.element ))
+  -- log(print_gui.path_to_caption(event.element, 'fp.pu_product'     , 'event.element')) -- event.element[2][2][1][1][1][1]
+  -- log(print_gui.path_to_caption(event.element, 'fp.pu_byingredient', 'event.element')) -- event.element[2][2][1][2][1][1]
+  -- log(print_gui.path_to_caption(event.element, 'fp.pu_ingredient'  , 'event.element')) -- event.element[2][2][1][3][1][1]
 
-  -- log(print_gui.path_to_caption(event.element, 'fp.pu_product', 'event.element')) -- event.element[2][2][1][1][1][1]
-
-  -- log(print_gui.serpent( event.element.children[2].children[2].children[1].children[1] ))
-  -- log(print_gui.serpent( event.element.children[2].children[2].children[1].children[1].children[2].children[1].children[1] ))
-
-  log(serpent.line( products(event.element) ))
-
-  -- log(print_gui.path_to_caption(event.element, 'fp.pu_ingredient', 'event.element')) -- event.element[2][2][1][1][1][1]
-
-  -- log(print_gui.serpent( event.element.children[2].children[2].children[1].children[3] ))
-
-  log(serpent.line( byproducts(event.element) ))
-  log(serpent.line( ingredients(event.element) ))
+  log(serpent.block({
+    products = get_item_box_contents(event.element, item_box_products),
+    byproducts = get_item_box_contents(event.element, item_box_byproducts),
+    ingredients = get_item_box_contents(event.element, item_box_ingredients),
+  }))
 end)
