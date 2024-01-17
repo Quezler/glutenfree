9292bb454cc9a3be64a7b7e36e8e192a5d53f67d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-17 23:16:35 +0100

    Support the radar construction pylon too (albeit the comment math is off)

--- mods/se-construction-pylon-show-orange-n-green-in-hand/data-final-fixes.lua
+++ mods/se-construction-pylon-show-orange-n-green-in-hand/data-final-fixes.lua
@@ -85,3 +85,56 @@ table.insert(rvp.layers, {
   priority = "extra-high-no-scale",
   width = 8,
 })
+
+local rvp2 = data.raw['electric-pole']['se-pylon-construction-radar'].radius_visualisation_picture
+rvp2.layers = {}
+
+-- construction radius of 256 / 64 = 4
+-- 64 is the size of the power area sprite, so in blocks of 4 we need to fill up the area
+-- todo: grab the values from the prototype and handle steps that aren't a multiple of 64
+
+for w = 1, 2 do
+  for h = 1, 2 do
+    table.insert(rvp2.layers, {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {(-1 + w * 2) / 8, (-1 + h * 2) / 8},
+      blend_mode = "additive-soft", -- looks the best out of all the blend modes
+    })
+    table.insert(rvp2.layers, {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {(-1 + w * 2) / 8, -(-1 + h * 2) / 8},
+      blend_mode = "additive-soft",
+    })
+  end
+  for h = 1, 2 do
+    table.insert(rvp2.layers, {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {-(-1 + w * 2) / 8, (-1 + h * 2) / 8},
+      blend_mode = "additive-soft",
+    })
+    table.insert(rvp2.layers, {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {-(-1 + w * 2) / 8, -(-1 + h * 2) / 8},
+      blend_mode = "additive-soft",
+    })
+  end
+end
+
+table.insert(rvp2.layers, {
+  filename = "__base__/graphics/entity/small-electric-pole/electric-pole-radius-visualization.png",
+  height = 8,
+  priority = "extra-high-no-scale",
+  width = 8,
+})
