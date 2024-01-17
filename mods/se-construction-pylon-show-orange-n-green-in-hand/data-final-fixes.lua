46cc4581fda3c44428621f5c02ee17628ec7e060
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-17 22:35:15 +0100

    Stash se-construction-pylon-show-orange-n-green-in-hand proof of concept

---
+++ mods/se-construction-pylon-show-orange-n-green-in-hand/data-final-fixes.lua
@@ -0,0 +1,34 @@
+-- i'd say i'm allowed to require postprocess since i want that description, and that mod generates it :)
+
+data.raw['electric-pole']['se-pylon-construction'].radius_visualisation_picture = {
+  layers = {
+    {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {1 / 8, 1 / 8},
+    },
+    {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {1 / 8, 3 / 8},
+    },
+    {
+      filename = "__core__/graphics/visualization-construction-radius.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      shift = {1 / 8, 5 / 8},
+    },
+    {
+      filename = "__base__/graphics/entity/small-electric-pole/electric-pole-radius-visualization.png",
+      height = 8,
+      priority = "extra-high-no-scale",
+      width = 8,
+      -- blend_mode = "overwrite",
+    },
+  }
+}
