599e40167fd6f56655f3eeccbbf08cb345b100ea
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-22 22:44:36 +0100

    Setup the recipe

--- mods/se-core-miner-drilling-mud/README.md
+++ mods/se-core-miner-drilling-mud/README.md
@@ -3,3 +3,7 @@ Based on reactions of the [Core Miner Refactor](https://discord.com/channels/419
 This mod boosts core miner output by 2.5x when you give a core mining drill some drilling mud.
 
 Due to technical limitations you'll need to move the miner down by 1 tile in order to opt-in for drilling mud.
+
+Multiplication factor, drilling fluid ingredients, crafting time/tints & basically everything is up for balance suggestions.
+
+(note that this mod causes the numbers of my other mod (se-core-miner-output-helper) to not be accurate anymore, not marked incompatible)
---
+++ mods/se-core-miner-drilling-mud/data-final-fixes.lua
@@ -0,0 +1,33 @@
+data:extend{{
+  type = 'recipe',
+  name = 'se-core-miner-drill-drilling-mud',
+  category = 'chemistry',
+
+  ingredients = {
+    {
+      type = "fluid",
+      name = "water",
+      amount = 50,
+    },
+    {
+      type = "fluid",
+      name = "lubricant",
+      amount = 10,
+    },
+    {
+      type = "item",
+      name = "landfill",
+      amount = 1,
+    },
+  },
+
+  energy_required = 50,
+  
+  results = {{
+    type = "fluid",
+    name = 'se-core-miner-drill-drilling-mud',
+    amount = 200,
+  }},
+}}
+
+table.insert(data.raw['technology']['se-core-miner'].effects, {type = "unlock-recipe", recipe = 'se-core-miner-drill-drilling-mud'})
