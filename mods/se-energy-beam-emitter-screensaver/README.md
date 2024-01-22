89526c971b54a4fc0a7558727f2e39abf304b265
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-22 15:30:14 +0100

    Publish se-energy-beam-emitter-screensaver

---
+++ mods/se-energy-beam-emitter-screensaver/README.md
@@ -0,0 +1,7 @@
+Doubleclick on the laser preview window to ride it. (no you cannot actually steer it)
+
+You'll also need to zoom in a bit by yourself in order to trigger the chunk to chart.
+
+Thumbnail originates from the https://t.me/addstickers/Skavenzepto sticker pack on telegram.
+
+Future version: perhaps just teleporting the remote view around each tick for each charting and no need for double clicking.
---
+++ mods/se-energy-beam-emitter-screensaver/control.lua
@@ -0,0 +1,21 @@
+script.on_event(defines.events.on_gui_click, function(event)
+  if event.element.type ~= 'camera' then end
+
+  local entity = event.element.entity
+  if entity == nil then end -- untargeted beam?
+  if entity.name ~= 'energy-glaive-beam' then end
+
+  local player = game.get_player(event.player_index)
+  local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = entity.surface.index})
+
+  -- opening the map right after remote view start does not cause the game to enter following map mode,
+  -- so because i am lazy atm i will not delay it by one tick, but just prompt the user to double click.
+  local second_click = player.surface == entity.surface
+
+  remote.call("space-exploration", "remote_view_start", {player=player, zone_name = zone.name, position = entity.position, location_name="Doomwheel", freeze_history=true})
+  player.open_map(entity.position, 1, entity)
+
+  if second_click then
+    player.opened = nil
+  end
+end)
