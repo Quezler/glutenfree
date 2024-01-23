259b42cc739fae0692722bc4ec65a87418550db7
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-23 15:23:34 +0100

    Attempt to create infinity pipes capable of 1 direction

--- mods/se-core-miner-drilling-mud/data.lua
+++ mods/se-core-miner-drilling-mud/data.lua
@@ -43,3 +43,20 @@ end
 coreminer.energy_usage = 1000/6*2.5 .. "KJ"
 coreminer.mining_speed = 200 -- twice the default
 coreminer.animations.layers[2].animation_speed = 2
+
+local up1 = table.deepcopy(data.raw['infinity-pipe']['infinity-pipe'])
+local up2 = table.deepcopy(data.raw['infinity-pipe']['infinity-pipe'])
+local up3 = table.deepcopy(data.raw['infinity-pipe']['infinity-pipe'])
+local up4 = table.deepcopy(data.raw['infinity-pipe']['infinity-pipe'])
+
+up1.name = 'infinity-pipe-drilling-mud-1'
+up2.name = 'infinity-pipe-drilling-mud-2'
+up3.name = 'infinity-pipe-drilling-mud-3'
+up4.name = 'infinity-pipe-drilling-mud-4'
+
+up1.fluid_box.pipe_connections = {up1.fluid_box.pipe_connections[1]}
+up2.fluid_box.pipe_connections = {up1.fluid_box.pipe_connections[2]}
+up3.fluid_box.pipe_connections = {up1.fluid_box.pipe_connections[3]}
+up4.fluid_box.pipe_connections = {up1.fluid_box.pipe_connections[4]}
+
+data:extend{up1, up2, up3, up4}
