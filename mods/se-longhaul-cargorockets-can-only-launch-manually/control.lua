15da512b95cbb78f4054e7441e0eec3a2d53e7ae
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-25 19:27:43 +0100

    Extract the surface name from the dropdown text

--- mods/se-longhaul-cargorockets-can-only-launch-manually/control.lua
+++ mods/se-longhaul-cargorockets-can-only-launch-manually/control.lua
@@ -5,6 +5,21 @@ local LaunchpadGUI = {name_rocket_launch_pad_gui_root = 'se-rocket-launch-pad-gu
 
 local print_gui = require('scripts.print_gui')
 
+function remove_rich_text(s) -- includes [color] blocks
+  local words = {}
+
+  -- split at each space
+  for word in s:gmatch("%S+") do
+    if word:sub(1, 1) == '[' or word:sub(-1, -1) == ']' then
+      -- ignore
+    else
+      table.insert(words, word)
+    end
+  end
+
+  return table.concat(words, ' ')
+end
+
 local function reset_launch_trigger_to_manual(entity, player)
   local position = entity.surface.find_non_colliding_position(entity.name, entity.position, 0, 1, true)
   local ghost = entity.surface.create_entity{
@@ -48,6 +63,7 @@ commands.add_command('reset-launch-trigger-to-manual', nil, function(command)
       local destination_dropdown = root.children[2].children[2]['launchpad-list-zones']
       local destination_selected = destination_dropdown.items[destination_dropdown.selected_index]
       log(serpent.line(destination_selected)) -- "    [img=virtual-signal/se-planet] Nauvis"
+      log(serpent.line(remove_rich_text(destination_selected))) -- Nauvis
 
       local position_dropdown = root.children[2].children[2]['launchpad-list-landing-pad-names']
       local position_selected = position_dropdown.items[position_dropdown.selected_index]
