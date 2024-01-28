b464e4ff709b293525da02484f814627ee6c6103
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-28 19:01:56 +0100

    Disable on_tick whilst inactive

--- mods/flushable-fluid-wagons/control.lua
+++ mods/flushable-fluid-wagons/control.lua
@@ -9,11 +9,50 @@ end
 
 script.on_init(function(event)
   global.structs = {}
+  global.structs_count = 0
   global.tank_number_to_wagon = {}
   on_configuration_changed()
 end)
 
 script.on_configuration_changed(on_configuration_changed)
+
+local function tick_struct(struct)
+  if struct.tank.valid == false then return false end
+  if struct.wagon.valid == false then return false end
+
+  if struct.player.opened == struct.wagon then return true end
+  if struct.player.opened ~= struct.tank then return false end
+  assert(struct.player.connected) -- can opened be non-nil if a player is offline?
+
+  local fluid = struct.wagon.fluidbox[1]
+  if fluid then
+    struct.tank.clear_fluid_inside()
+    struct.tank.insert_fluid(fluid)
+  end
+end
+
+local function on_tick(event)
+  for unit_number, struct in pairs(global.structs) do
+    local keep, why = tick_struct(struct)
+    if keep == false then
+      struct.tank.destroy()
+      global.tank_number_to_wagon[struct.tank_unit_number] = nil
+
+      global.structs[unit_number] = nil
+      global.structs_count = global.structs_count - 1
+    end
+  end
+
+  if global.structs_count == 0 then
+    script.on_event(defines.events.on_tick, nil)
+  end
+end
+
+script.on_load(function(event)
+  if global.structs_count > 0 then
+    script.on_event(defines.events.on_tick, on_tick)
+  end
+end)
 
 script.on_event('open-fluid-wagon', function(event)
   local player = game.get_player(event.player_index)
@@ -49,6 +88,9 @@ script.on_event('open-fluid-wagon', function(event)
     player = player,
   }
 
+  global.structs_count = global.structs_count + 1
+  script.on_event(defines.events.on_tick, on_tick)
+
   local coin = {name = "coin", count = 1}
   if player.get_main_inventory().insert(coin) then
     player.get_main_inventory().remove(coin)
@@ -63,31 +105,6 @@ script.on_event(defines.events.on_player_main_inventory_changed, function(event)
 
   _global.player_should_open[player.index] = nil
   player.opened = tank
-end)
-
-local function tick_struct(struct)
-  if struct.tank.valid == false then return false end
-  if struct.wagon.valid == false then return false end
-
-  if struct.player.opened == struct.wagon then return true end
-  if struct.player.opened ~= struct.tank then return false end
-  assert(struct.player.connected) -- can opened be non-nil if a player is offline?
-
-  local fluid = struct.wagon.fluidbox[1]
-  if fluid then
-    struct.tank.clear_fluid_inside()
-    struct.tank.insert_fluid(fluid)
-  end
-end
-
-script.on_event(defines.events.on_tick, function(event)
-  for unit_number, struct in pairs(global.structs) do
-    local keep, why = tick_struct(struct)
-    if keep == false then
-      struct.tank.destroy()
-      global.tank_number_to_wagon[struct.tank_unit_number] = nil
-    end
-  end
 end)
 
 script.on_event(defines.events.on_player_flushed_fluid, function(event)
