f364751af74113f743a9fb1b921f96132e2c1b8d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-28 16:15:35 +0100

    Open the tank when opening the fluid wagon

---
+++ mods/flushable-fluid-wagons/README.md
@@ -0,0 +1,1 @@
+Could i have instead of gui Z layer trickery used a relative gui? yes i could have, but where's the fun in that :3
--- mods/flushable-fluid-wagons/control.lua
+++ mods/flushable-fluid-wagons/control.lua
@@ -1,9 +1,48 @@
+local _global = {player_should_open = {}}
+
 script.on_event('open-fluid-wagon', function(event)
   local player = game.get_player(event.player_index)
 
-  local selected = player.selected
-  if selected == nil then return end
-  if selected.type ~= 'fluid-wagon' then return end
+  local entity = player.selected
+  if entity == nil then return end
+  if entity.type ~= 'fluid-wagon' then return end
 
-  game.print(selected.name)
+  local fluid = entity.fluidbox[1]
+  -- if fluid == nil then return end -- empty fluid wagon
+
+  game.print(entity.name)
+
+  local tank = entity.surface.create_entity{
+    name = entity.name .. '-flushable',
+    force = entity.force,
+    position = entity.position,
+  }
+
+  tank.destructible = false
+  if fluid then
+    tank.insert_fluid(fluid)
+  end
+
+  _global.player_should_open[player.index] = tank
+
+  local coin = {name = "coin", count = 1}
+  if player.get_main_inventory().insert(coin) then
+    player.get_main_inventory().remove(coin)
+  end
+end)
+
+-- script.on_event(defines.events.on_gui_opened, function(event)
+--   log(event.gui_type)
+--   -- log(event.entity)
+--   -- log(event.equipment)
+--   if event.entity then log(event.entity.name) end
+-- end)
+
+script.on_event(defines.events.on_player_main_inventory_changed, function(event)
+  local player = game.get_player(event.player_index)
+
+  local tank = _global.player_should_open[player.index]
+  if tank == nil then return end
+  _global.player_should_open[player.index] = nil
+  player.opened = tank
 end)
