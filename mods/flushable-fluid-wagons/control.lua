4927d3ccba0aa58a6a9544a7417e4bbe358ba410
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-28 18:45:30 +0100

    Support all fluid wagons in the control stage

--- mods/flushable-fluid-wagons/control.lua
+++ mods/flushable-fluid-wagons/control.lua
@@ -1,11 +1,19 @@
 local _global = {player_should_open = {}}
 
+local function on_configuration_changed()
+  global.is_flushable = {}
+  for entity_name, prototype in pairs(game.get_filtered_entity_prototypes({{filter = 'type', type = 'fluid-wagon'}})) do
+    global.is_flushable[entity_name .. '-flushable'] = true
+  end
+end
+
 script.on_init(function(event)
   global.structs = {}
-
-  -- global.wagon_number_to_tank = {}
   global.tank_number_to_wagon = {}
+  on_configuration_changed()
 end)
+
+script.on_configuration_changed(on_configuration_changed)
 
 script.on_event('open-fluid-wagon', function(event)
   local player = game.get_player(event.player_index)
@@ -15,9 +23,7 @@ script.on_event('open-fluid-wagon', function(event)
   if entity.type ~= 'fluid-wagon' then return end
 
   local fluid = entity.fluidbox[1]
-  -- if fluid == nil then return end -- empty fluid wagon
-
-  game.print(entity.name)
+  if fluid == nil then return end -- empty fluid wagon (to allow access to equipment)
 
   local tank = entity.surface.create_entity{
     name = entity.name .. '-flushable',
@@ -35,7 +41,7 @@ script.on_event('open-fluid-wagon', function(event)
   -- global.wagon_number_to_tank[entity.unit_number] = tank
   global.tank_number_to_wagon[tank.unit_number] = entity
 
-  global.structs[tank.unit_number] = { -- tank unit number so multiple per wagon can exist
+  global.structs[tank.unit_number] = { -- tank's unit number so multiple per wagon can exist
     wagon_unit_number = entity.unit_number,
     wagon = entity,
 
@@ -56,16 +62,17 @@ script.on_event(defines.events.on_player_main_inventory_changed, function(event)
 
   local tank = _global.player_should_open[player.index]
   if tank == nil then return end
+
   _global.player_should_open[player.index] = nil
   player.opened = tank
 end)
 
 local function tick_struct(struct)
-  if struct.tank.valid == false then return false, debug.getinfo(1).currentline end
-  if struct.wagon.valid == false then return false, debug.getinfo(1).currentline end
+  if struct.tank.valid == false then return false end
+  if struct.wagon.valid == false then return false end
 
-  if struct.player.opened == struct.wagon then return true, "we'll wait for the next tick" end
-  if struct.player.opened ~= struct.tank then return false, debug.getinfo(1).currentline end
+  if struct.player.opened == struct.wagon then return true end
+  if struct.player.opened ~= struct.tank then return false end
   assert(struct.player.connected) -- can opened be non-nil if a player is offline?
 
   local fluid = struct.wagon.fluidbox[1]
@@ -73,27 +80,20 @@ local function tick_struct(struct)
     struct.tank.clear_fluid_inside()
     struct.tank.insert_fluid(fluid)
   end
-
-  return true
 end
 
 script.on_event(defines.events.on_tick, function(event)
   for unit_number, struct in pairs(global.structs) do
     local keep, why = tick_struct(struct)
     if keep == false then
-      -- log(why)
-      -- log(struct.player.opened.name)
       struct.tank.destroy()
-      -- struct.wagon.destroy()
-
-      -- global.wagon_number_to_tank[struct.wagon_unit_number] = nil
       global.tank_number_to_wagon[struct.tank_unit_number] = nil
     end
   end
 end)
 
 script.on_event(defines.events.on_player_flushed_fluid, function(event)
-  if event.entity.name == 'fluid-wagon-flushable' then
+  if global.is_flushable[event.entity.name] then
     local wagon = global.tank_number_to_wagon[event.entity.unit_number]
     wagon.clear_fluid_inside()
   end
