d7c83dd9771fb9d2ff6d9d8d522026c0a60d1192
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-01-30 19:07:28 +0100

    Support discovering zones by satellite

--- mods/se-see-info-about-the-newly-unlocked-zone-in-chat/control.lua
+++ mods/se-see-info-about-the-newly-unlocked-zone-in-chat/control.lua
@@ -14,39 +14,49 @@ local function get_threat(zone)
   return 0
 end
 
+local function tick_force(force)
+  local old_known_zones = global.known_zones[force.name]
+  local new_known_zones = remote.call("space-exploration", "get_known_zones", {force_name = force.name})
+
+  -- this should prevent newly created forces from being spammed with everything already unlocked i suppose?
+  if old_known_zones == nil then
+    global.known_zones[force.name] = new_known_zones
+    return
+  end
+
+  local to_print = nil
+
+  for zone_index, _ in pairs(new_known_zones) do
+    if old_known_zones[zone_index] == nil then
+      local zone = remote.call("space-exploration", "get_zone_from_zone_index", {zone_index = tonumber(zone_index)})
+
+      if to_print then goto skip end
+      to_print = {"", string.format("[img=entity/%s] ", zone.primary_resource), {"entity-name." .. zone.primary_resource}, " is the primary resource, and the threat level is ", string.format('%d%%', get_threat(zone) * 100), "."}
+    end
+  end
+
+  force.print(to_print)
+
+  ::skip::
+  global.known_zones[force.name] = new_known_zones
+end
+
 local research_can_trigger_zone_unlock = {
   ['se-zone-discovery-random'] = true,
   ['se-zone-discovery-targeted'] = true,
   ['se-zone-discovery-deep'] = true,
 }
+
 script.on_event(defines.events.on_research_finished, function(event)
-  -- game.print(event.research.name)
   if research_can_trigger_zone_unlock[event.research.name] then
-    
-    local force = event.research.force
-    local old_known_zones = global.known_zones[force.name]
-    local new_known_zones = remote.call("space-exploration", "get_known_zones", {force_name = force.name})
+    tick_force(event.research.force)
+  end
+end)
 
-    -- this should prevent newly created forces from being spammed with everything already unlocked i suppose?
-    if old_known_zones == nil then
-      global.known_zones[force.name] = new_known_zones
-      return
+script.on_event(defines.events.on_rocket_launched, function(event)
+  if event.rocket and event.rocket.valid then
+    if event.rocket.get_item_count('satellite') > 0 then
+      tick_force(event.rocket.force)
     end
-
-    local to_print = nil
-
-    for zone_index, _ in pairs(new_known_zones) do
-      if old_known_zones[zone_index] == nil then
-        local zone = remote.call("space-exploration", "get_zone_from_zone_index", {zone_index = tonumber(zone_index)})
-
-        if to_print then goto skip end
-        to_print = {"", string.format("[img=entity/%s] ", zone.primary_resource), {"entity-name." .. zone.primary_resource}, " is the primary resource, and the threat level is ", string.format('%d%%', get_threat(zone) * 100), "."}
-      end
-    end
-
-    force.print(to_print)
-
-    ::skip::
-    global.known_zones[force.name] = new_known_zones
   end
 end)
