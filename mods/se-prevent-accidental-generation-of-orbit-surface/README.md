7709d4c7cd0e9c0aab9a67231a1fb5c99249a4d8
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-02 17:27:16 +0100

    Mod portal release

---
+++ mods/se-prevent-accidental-generation-of-orbit-surface/README.md
@@ -0,0 +1,1 @@
+If you click on a thing that hasn't generated yet in the bottom left corner of remote view it asks you to shift-click to confirm generating it.
---
+++ mods/se-prevent-accidental-generation-of-orbit-surface/control.lua
@@ -0,0 +1,27 @@
+script.on_event(defines.events.on_gui_click, function(event)
+  -- if event.element.style ~= 'se_remote_view_hierarchy_button' then return end
+  if event.element.parent.name ~= 'hierarchy-flow' then return end
+  if event.element.tags.action ~= 'go-to-zone' then return end -- ignore the interstellar map
+
+  local surface_exists = remote.call("space-exploration", "zone_get_surface", {zone_index = event.element.tags.zone_index}) ~= nil
+  if surface_exists == true then return end
+
+  if event.shift == true then return end
+
+  event.element.parent.add{
+    type = "sprite-button",
+    sprite = event.element.sprite,
+    tooltip = event.element.tooltip,
+    tags = event.element.tags,
+    style = "se_remote_view_hierarchy_button",
+    
+    index = event.element.get_index_in_parent(),
+  }
+
+  event.element.destroy()
+
+  game.get_player(event.player_index).create_local_flying_text{
+    text = "Shift-click to generate surface.",
+    create_at_cursor = true,
+  }
+end)
