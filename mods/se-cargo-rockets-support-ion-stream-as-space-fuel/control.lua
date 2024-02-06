fdb02281df0df1b5ec5ba0261f862f2d415a3058
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-06 11:25:39 +0100

    Polishing

--- mods/se-cargo-rockets-support-ion-stream-as-space-fuel/control.lua
+++ mods/se-cargo-rockets-support-ion-stream-as-space-fuel/control.lua
@@ -1,4 +1,5 @@
 local Zone = require('__space-exploration-scripts__.zone')
+local Util = require('__space-exploration__.scripts.util')
 local Spaceship = require('__space-exploration-scripts__.spaceship')
 local Launchpad = {name_rocket_launch_pad = 'se-rocket-launch-pad'}
 local LaunchpadGUI = {name_rocket_launch_pad_gui_root = 'se-rocket-launch-pad-gui'}
@@ -46,12 +47,8 @@ for _, event in ipairs({
 end
 
 local function on_configuration_changed(event)
-  -- 2'000'000 / 4'000'000 = 0.5
-  -- global.ion_exchange_rate = game.fluid_prototypes['se-liquid-rocket-fuel'].fuel_value / Spaceship.ion_stream_energy
-
-  -- 4'000'000 / 2'000'000 = 2.0
+  -- 4'000'000 / 2'000'000 = 2.0 -- 2x rocket fuel
   global.ion_exchange_rate = Spaceship.ion_stream_energy / game.fluid_prototypes['se-liquid-rocket-fuel'].fuel_value
-
   global.surface_is_space = {}
 end
 
@@ -71,7 +68,7 @@ end)
 script.on_configuration_changed(on_configuration_changed)
 
 local function get_surface_is_space(surface)
-  -- if true then return true end
+  -- if 'debug' then return true end
   if global.surface_is_space[surface.index] == nil then
     local zone = Zone.from_surface_index(surface.index)
 
@@ -95,8 +92,6 @@ local function tick_struct(struct)
     end
   end
 
-  -- game.print(serpent.line(global.surface_is_space))
-
   if fluids['se-ion-stream'] and get_surface_is_space(struct.container.surface) then
     struct.recently_received_ion = true
     local transferred = struct.tank.insert_fluid({name = 'se-liquid-rocket-fuel', amount = fluids['se-ion-stream'] * global.ion_exchange_rate})
@@ -117,37 +112,34 @@ local function tick_player(player)
   local fuel_capacity_progress = root.children[2].children[1]['fuel_capacity_progress']
 
   local struct = global.structs[root.tags.unit_number]
-  if struct == nil then return end
+  if struct == nil then return end -- nil when the silo is freshly placed somehow (possibly my cargo rocket label mod?)
+
   if struct.recently_received_ion == false then
     fuel_capacity_progress.style = 'se_launchpad_progressbar_fuel'
     return
   end
 
   fuel_capacity_progress.style = 'se_launchpad_progressbar_ion'
+
   local caption = fuel_capacity_progress.caption
+  caption[1] = 'space-exploration.label_liquid_ion_stream'
 
   local fuel_k_min_string = caption[2][2]
   local fuel_k_min = tonumber(fuel_k_min_string:sub(1, -2))  -- remove the k, then cast to number
-  caption[2][2] = fuel_k_min / 2 .. 'k'
-  -- game.print(fuel_capacity_progress.caption[2][2])
-  -- game.print(fuel_k_min / 2 .. 'k')
+  caption[2][2] = Util.format_fuel(fuel_k_min / global.ion_exchange_rate * 1000)
 
   local fuel_k_max_string = caption[2][3]
   if fuel_k_max_string ~= '?' then
-    local fuel_max_k = tonumber(fuel_k_max_string:sub(1, -2)) -- remove the k, then cast to number
-    caption[2][3] = fuel_max_k / 2 .. 'k'
+    local fuel_k_max = tonumber(fuel_k_max_string:sub(1, -2)) -- remove the k, then cast to number
+    caption[2][3] = Util.format_fuel(fuel_k_max / global.ion_exchange_rate * 1000)
   end
 
-  caption[1] = "space-exploration.label_liquid_ion_stream"
-  -- game.print(serpent.line(caption))
   fuel_capacity_progress.caption = caption
-
-  -- game.print(serpent.block(fuel_capacity_progress.caption))
 end
 
 script.on_event(defines.events.on_tick, function(event)
   for _, struct in pairs(global.structs) do
-    if (event.tick + struct.unit_number) % 60 == 0 then
+    if (event.tick + struct.unit_number + 1) % 60 == 0 then -- 1 tick before SE ticks the silo
       tick_struct(struct)
     end
   end
@@ -165,30 +157,21 @@ script.on_event(defines.events.on_gui_opened, function(event)
   end
 end)
 
+script.on_event(defines.events.on_gui_selection_state_changed, function(event)
+  if not (event.element and event.element.valid) then return end
+  tick_player(game.get_player(event.player_index))
+  -- local element = event.element
+  -- local player = game.get_player(event.player_index)
+  -- local root = player.gui.relative[LaunchpadGUI.name_rocket_launch_pad_gui_root]
+  -- if not (root and root.tags and root.tags.unit_number) then return end
+
+  -- tick_player(player)
+end)
+
 -- surface indexes can be reused, which means another zone type might take its place
 script.on_event(defines.events.on_surface_deleted, function(event)
   global.surface_is_space[event.surface_index] = nil
 end)
-
--- local function on_entity_removed(event)
---   local entity = event.entity
---   game.print(entity.name)
---   local struct = global.structs[entity.unit_number]
---   if struct then global.structs[entity.unit_number] = nil
---     struct.ion_tank.destroy()
---   end
--- end
-
--- for _, event in ipairs({
---   defines.events.on_player_mined_entity,
---   defines.events.on_robot_mined_entity,
---   defines.events.on_entity_died,
---   defines.events.script_raised_destroy,
--- }) do
---   script.on_event(event, on_entity_removed, {
---     {filter = 'name', name = 'se-rocket-launch-pad'},
---   })
--- end
 
 script.on_event(defines.events.on_entity_destroyed, function(event)
   local deathrattle = global.deathrattles[event.registration_number]
