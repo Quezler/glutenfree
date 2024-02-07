3e226176aad75a43bba22e15ba698df2777b619a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-07 16:00:20 +0100

    Prevent spaceship <=> zone id collisions

--- mods/se-universe-explorer-highlight-zones-new-to-you/control.lua
+++ mods/se-universe-explorer-highlight-zones-new-to-you/control.lua
@@ -1,4 +1,6 @@
 script.on_init(function(event)
+  global.version = 1
+
   global.next_tick_events = {}
 
   global.player_index_to_selected_zones_map = {}
@@ -17,7 +19,10 @@ local function on_post_gui_opened(event)
   if not scroll_pane then return end
 
   for _, row in pairs(scroll_pane.children) do
-    if not global.player_index_to_selected_zones_map[event.player_index] or not global.player_index_to_selected_zones_map[event.player_index][row.tags.zone_index] then
+    local tags = row.tags
+    if not global.player_index_to_selected_zones_map[event.player_index]
+    or not global.player_index_to_selected_zones_map[event.player_index][tags.zone_type]
+    or not global.player_index_to_selected_zones_map[event.player_index][tags.zone_type][tags.zone_index] then
       row.row_flow.children[2].add{ -- Zone icon
         type = "sprite",
         sprite = "utility/notification",
@@ -51,13 +56,74 @@ script.on_event(defines.events.on_gui_opened, function(event)
   script.on_event(defines.events.on_tick, on_tick)
 end)
 
+local function mark_as_seen(player_index, tags)
+  global.player_index_to_selected_zones_map[player_index]                                  = global.player_index_to_selected_zones_map[player_index] or {}
+  global.player_index_to_selected_zones_map[player_index][tags.zone_type]                  = global.player_index_to_selected_zones_map[player_index][tags.zone_type] or {}
+  global.player_index_to_selected_zones_map[player_index][tags.zone_type][tags.zone_index] = true
+end
+
 script.on_event(defines.events.on_gui_click, function(event)
   if event.element and event.element.tags and event.element.tags.action == Zonelist.action_zone_row_button then
   --game.print('selected zone ' .. event.element.tags.zone_index)
-    global.player_index_to_selected_zones_map[event.player_index] = global.player_index_to_selected_zones_map[event.player_index] or {}
-    global.player_index_to_selected_zones_map[event.player_index][event.element.tags.zone_index] = true
+    mark_as_seen(event.player_index, event.element.tags)
 
     local notification = event.element.row_flow.children[2].children[1]
     if notification then notification.destroy() end
+  end
+end)
+
+script.on_configuration_changed(function(event)
+  if (global.version or 0) == 0 then
+    local zones = remote.call("space-exploration", "get_zone_index", {}) -- does not return `zone.type == spaceship`'s
+
+    local zone_by_index = {}
+    for _, zone in ipairs(zones) do
+      zone_by_index[zone.index] = zone
+    end
+
+    local player_index_to_selected_zones_map = global.player_index_to_selected_zones_map
+    global.player_index_to_selected_zones_map = {}
+
+    local spaceship_ids = {}
+
+    -- for player_index, map in pairs(player_index_to_selected_zones_map) do
+    --   for zone_index, _ in pairs(map) do
+    --     local zone = zone_by_index[zone_index]
+    --     log(zone.type)
+    --     if zone == nil then
+    --       spaceship_ids[zone_index] = true
+    --     end
+    --   end
+    -- end
+
+    -- log(serpent.line(spaceship_ids))
+
+    -- for player_index, map in pairs(player_index_to_selected_zones_map) do
+    --   for zone_index, _ in pairs(map) do
+    --     local zone = zone_by_index[zone_index]
+    --     if zone and not spaceship_ids[zone_index] then
+    --       mark_as_seen(player_index, {zone_type = zone.type, zone_index = zone.index})
+    --     end
+    --     -- log(serpent.line(zone_index))
+    --   end
+    -- end
+
+    for player_index, map in pairs(player_index_to_selected_zones_map) do
+      local visible_zones = remote.call("space-exploration", "get_known_zones", {force_name = game.get_player(player_index).force.name})
+
+      local visible_zone_by_index = {}
+      for _, zone in ipairs(zones) do
+        visible_zone_by_index[zone.index] = zone
+      end
+
+      for zone_index, _ in pairs(map) do
+        local zone = zone_by_index[zone_index]
+        if zone and visible_zone_by_index[zone_index] then
+          mark_as_seen(player_index, {zone_type = zone.type, zone_index = zone.index})
+        end
+      end
+    end
+
+    global.version = 1
   end
 end)
