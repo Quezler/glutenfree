97ef04872995549c33145d88ecc3194fd2099cc5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-12 12:50:14 +0100

    Recreate destroyed containers and contents

--- mods/se-spaceship-max-speed-ignore-container-integrity/control.lua
+++ mods/se-spaceship-max-speed-ignore-container-integrity/control.lua
@@ -5,11 +5,30 @@ commands.add_command('se-spaceship-max-speed-ignore-container-integrity', nil, f
 
   if string.find(surface.name, "spaceship-") then
     local containers = surface.find_entities_filtered{type = {"container", "logistic-container"}}
+
+    local ghosts = {} -- not actual ghosts, just a singular/prular combo that makes sense to be used within create_entity
+
     for _, container in ipairs(containers) do
+
+      local inventory = container.get_inventory(defines.inventory.chest)
+      local ghost = {
+        name = container.name,
+        force = container.force,
+        position = container.position,
+
+        inventory = game.create_inventory(#inventory),
+      }
+
+      for slot = 1, #inventory do
+        inventory[slot].swap_stack(ghost.inventory[slot])
+      end
+      
       -- todo: save inventory
       -- todo: preserve wires
       -- todo: preserve signals
+
       container.destroy()
+      table.insert(ghosts, ghost)
     end
 
     -- trigger Spaceship.on_entity_created(event)
@@ -19,5 +38,21 @@ commands.add_command('se-spaceship-max-speed-ignore-container-integrity', nil, f
       text = {'achievement-name.so-long-and-thanks-for-all-the-fish'},
       raise_built = true,
     }
+
+    for _, ghost in ipairs(ghosts) do
+      local container = surface.create_entity{
+        name = ghost.name,
+        force = ghost.force,
+        position = ghost.position,
+      }
+
+      local inventory = container.get_inventory(defines.inventory.chest)
+
+      for slot = 1, #inventory do
+        inventory[slot].swap_stack(ghost.inventory[slot])
+      end
+
+      ghost.inventory.destroy()
+    end
   end
 end)
