30072005d01e732694361e096f13f2acdded0dbe
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-16 14:21:29 +0100

    Stash initial draft

---
+++ mods/se-k2-matter-equivalent-exchange/README.md
@@ -0,0 +1,5 @@
+Space Exploration's compatibility with Krastorio 2 adds basic matter stabilizers, this mod undoes that change.
+
+It also undoes the matter deconversion nerf (imagine paying 2000 matter for a cube worth only 1000 :D)
+
+Obviously this goes against the intended balance of their compatibility, but i'm used to this.
---
+++ mods/se-k2-matter-equivalent-exchange/data-updates.lua
@@ -0,0 +1,84 @@
+local data_util = require("__space-exploration__.data_util")
+
+-- data.raw['recipe']['se-kr-basic-stabilizer'].hidden = true
+-- data.raw['recipe']['se-kr-charge-basic-stabilizer'].hidden = true
+
+-- this apparently doesn't exist upstream? (written @ 0.6.125)
+function data_util.remove_result(recipe, name)
+  if type(recipe) == "string" then recipe = data.raw.recipe[recipe] end
+  if not recipe then return end
+  if recipe.results then
+    data_util.remove_result_sub(recipe, name)
+  end
+  if recipe.normal and recipe.normal.results then
+    data_util.remove_result_sub(recipe.normal, name)
+  end
+  if recipe.expensive and recipe.expensive.results then
+    data_util.remove_result_sub(recipe.expensive, name)
+  end
+end
+
+local function remove_basic_stabilizer(recipe_name)
+  data_util.remove_ingredient(recipe_name, "se-kr-charged-basic-stabilizer")
+  data_util.remove_result(recipe_name, "se-kr-charged-basic-stabilizer")
+  data_util.remove_result(recipe_name, "se-kr-basic-stabilizer")
+end
+
+remove_basic_stabilizer("matter-to-stone")
+remove_basic_stabilizer("matter-to-sand")
+remove_basic_stabilizer("matter-to-coal")
+remove_basic_stabilizer("matter-to-copper-ore")
+remove_basic_stabilizer("matter-to-iron-ore")
+remove_basic_stabilizer("matter-to-crude-oil")
+remove_basic_stabilizer("matter-to-raw-rare-metals")
+remove_basic_stabilizer("matter-to-uranium-ore")
+remove_basic_stabilizer("matter-to-uranium-238")
+remove_basic_stabilizer("matter-to-water")
+remove_basic_stabilizer("matter-to-mineral-water")
+
+for _, recipe in pairs(data.raw.recipe) do
+  if recipe.subgroup == "matter-deconversion" then
+    log(recipe.name)
+    if recipe.ingredients then
+      for _, ingredient in pairs(recipe.ingredients) do
+        if ingredient.name == "matter" then
+          ingredient.amount = ingredient.amount / 2
+          if ingredient.catalyst_amount then
+            ingredient.catalyst_amount = ingredient.catalyst_amount / 2
+          end
+        end
+      end
+    end
+    if recipe.normal then
+      for _, ingredient in pairs(recipe.normal.ingredients) do
+        if ingredient.name == "matter" then
+          ingredient.amount = ingredient.amount / 2
+          if ingredient.catalyst_amount then
+            ingredient.catalyst_amount = ingredient.catalyst_amount / 2
+          end
+        end
+      end      
+    end
+    if recipe.expensive then
+      for _, ingredient in pairs(recipe.expensive.ingredients) do
+        if ingredient.name == "matter" then
+          ingredient.amount = ingredient.amount / 2
+          if ingredient.catalyst_amount then
+            ingredient.catalyst_amount = ingredient.catalyst_amount / 2
+          end
+        end
+      end
+    end
+  end
+end
+
+-- remove the recipe from techs by enabling it fully and then disabeling it completely
+
+data_util.enable_recipe('se-kr-basic-stabilizer')
+data_util.disable_recipe('se-kr-basic-stabilizer')
+
+data_util.enable_recipe('se-kr-charge-basic-stabilizer')
+data_util.disable_recipe('se-kr-charge-basic-stabilizer')
+
+-- makes sense, but it makes the technology items look weird
+-- data_util.recipe_require_tech("kr-stabilizer-charging-station", "se-kr-advanced-matter-processing")
