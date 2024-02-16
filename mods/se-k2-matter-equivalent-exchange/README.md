449dfe34bab858e781c78ff023bebe4f7b7291ab
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-16 15:32:16 +0100

    Stash exhausting attempt to check the numbers programatically

--- mods/se-k2-matter-equivalent-exchange/README.md
+++ mods/se-k2-matter-equivalent-exchange/README.md
@@ -1,5 +1,3 @@
-Space Exploration's compatibility with Krastorio 2 adds basic matter stabilizers, this mod undoes that change.
+2000 matter for a cube worth only 1000? don't be silly.
 
-It also undoes the matter deconversion nerf (imagine paying 2000 matter for a cube worth only 1000 :D)
-
-Obviously this goes against the intended balance of their compatibility, but i'm used to this.
+this mod ensures the input and output matter values match.
--- mods/se-k2-matter-equivalent-exchange/data-updates.lua
+++ mods/se-k2-matter-equivalent-exchange/data-updates.lua
@@ -4,41 +4,41 @@ local data_util = require("__space-exploration__.data_util")
 -- data.raw['recipe']['se-kr-charge-basic-stabilizer'].hidden = true
 
 -- this apparently doesn't exist upstream? (written @ 0.6.125)
-function data_util.remove_result(recipe, name)
-  if type(recipe) == "string" then recipe = data.raw.recipe[recipe] end
-  if not recipe then return end
-  if recipe.results then
-    data_util.remove_result_sub(recipe, name)
-  end
-  if recipe.normal and recipe.normal.results then
-    data_util.remove_result_sub(recipe.normal, name)
-  end
-  if recipe.expensive and recipe.expensive.results then
-    data_util.remove_result_sub(recipe.expensive, name)
-  end
-end
+-- function data_util.remove_result(recipe, name)
+--   if type(recipe) == "string" then recipe = data.raw.recipe[recipe] end
+--   if not recipe then return end
+--   if recipe.results then
+--     data_util.remove_result_sub(recipe, name)
+--   end
+--   if recipe.normal and recipe.normal.results then
+--     data_util.remove_result_sub(recipe.normal, name)
+--   end
+--   if recipe.expensive and recipe.expensive.results then
+--     data_util.remove_result_sub(recipe.expensive, name)
+--   end
+-- end
 
-local function remove_basic_stabilizer(recipe_name)
-  data_util.remove_ingredient(recipe_name, "se-kr-charged-basic-stabilizer")
-  data_util.remove_result(recipe_name, "se-kr-charged-basic-stabilizer")
-  data_util.remove_result(recipe_name, "se-kr-basic-stabilizer")
-end
+-- local function remove_basic_stabilizer(recipe_name)
+--   data_util.remove_ingredient(recipe_name, "se-kr-charged-basic-stabilizer")
+--   data_util.remove_result(recipe_name, "se-kr-charged-basic-stabilizer")
+--   data_util.remove_result(recipe_name, "se-kr-basic-stabilizer")
+-- end
 
-remove_basic_stabilizer("matter-to-stone")
-remove_basic_stabilizer("matter-to-sand")
-remove_basic_stabilizer("matter-to-coal")
-remove_basic_stabilizer("matter-to-copper-ore")
-remove_basic_stabilizer("matter-to-iron-ore")
-remove_basic_stabilizer("matter-to-crude-oil")
-remove_basic_stabilizer("matter-to-raw-rare-metals")
-remove_basic_stabilizer("matter-to-uranium-ore")
-remove_basic_stabilizer("matter-to-uranium-238")
-remove_basic_stabilizer("matter-to-water")
-remove_basic_stabilizer("matter-to-mineral-water")
+-- remove_basic_stabilizer("matter-to-stone")
+-- remove_basic_stabilizer("matter-to-sand")
+-- remove_basic_stabilizer("matter-to-coal")
+-- remove_basic_stabilizer("matter-to-copper-ore")
+-- remove_basic_stabilizer("matter-to-iron-ore")
+-- remove_basic_stabilizer("matter-to-crude-oil")
+-- remove_basic_stabilizer("matter-to-raw-rare-metals")
+-- remove_basic_stabilizer("matter-to-uranium-ore")
+-- remove_basic_stabilizer("matter-to-uranium-238")
+-- remove_basic_stabilizer("matter-to-water")
+-- remove_basic_stabilizer("matter-to-mineral-water")
 
 for _, recipe in pairs(data.raw.recipe) do
   if recipe.subgroup == "matter-deconversion" then
-    log(recipe.name)
+    -- log(recipe.name)
     if recipe.ingredients then
       for _, ingredient in pairs(recipe.ingredients) do
         if ingredient.name == "matter" then
@@ -72,13 +72,34 @@ for _, recipe in pairs(data.raw.recipe) do
   end
 end
 
+local function opposite(recipe_name)
+  local left, right = recipe_name:match("(.+)%-to%-(.+)")
+  return right .. '-to-' .. left
+end
+
+for _, recipe in pairs(data.raw.recipe) do
+  -- if recipe.subgroup == "matter-deconversion" then
+  --   log(recipe.name)
+  --   log(opposite(recipe.name))
+  -- end
+  if recipe.subgroup == "matter-conversion" then
+    log(recipe.name)
+    local opposite_recipe = data.raw['recipe'][opposite(recipe.name)]
+    log(opposite_recipe ~= nil)
+  end
+end
+
+
 -- remove the recipe from techs by enabling it fully and then disabeling it completely
 
-data_util.enable_recipe('se-kr-basic-stabilizer')
-data_util.disable_recipe('se-kr-basic-stabilizer')
+-- data_util.enable_recipe('se-kr-basic-stabilizer')
+-- data_util.disable_recipe('se-kr-basic-stabilizer')
 
-data_util.enable_recipe('se-kr-charge-basic-stabilizer')
-data_util.disable_recipe('se-kr-charge-basic-stabilizer')
+-- data_util.enable_recipe('se-kr-charge-basic-stabilizer')
+-- data_util.disable_recipe('se-kr-charge-basic-stabilizer')
 
 -- makes sense, but it makes the technology items look weird
 -- data_util.recipe_require_tech("kr-stabilizer-charging-station", "se-kr-advanced-matter-processing")
+
+-- se-vulcanite-to-matter
+-- matter-to-se-vulcanute
