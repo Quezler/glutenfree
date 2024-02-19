bb67cbee035b530c09e74ac1327b9062cb80a13f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-19 16:59:13 +0100

    Align furnace alt mode icon & size with the energized ring

--- mods/se-energy-beam-receiver-doubles-as-a-fast-furnace/data.lua
+++ mods/se-energy-beam-receiver-doubles-as-a-fast-furnace/data.lua
@@ -8,11 +8,18 @@ furnace.icon = receiver.icon
 furnace.icon_size = receiver.icon_size
 
 furnace.collision_mask = {}
-furnace.collision_box = receiver.collision_box
-furnace.selection_box = {
-  {receiver.selection_box[1][1] + 0.5, receiver.selection_box[1][2] + 0.5},
-  {receiver.selection_box[2][1] - 0.5, receiver.selection_box[2][2] - 0.5},
+furnace.collision_box = {
+  {receiver.collision_box[1][1], receiver.collision_box[1][2] + 2.5},
+  {receiver.collision_box[2][1], receiver.collision_box[2][2] + 2.5},
 }
+
+furnace.selection_box = {
+  {receiver.selection_box[1][1] + 0.5, receiver.selection_box[1][2] + 0.5 + 2.5},
+  {receiver.selection_box[2][1] - 0.5, receiver.selection_box[2][2] - 0.5 + 2.5},
+}
+
 furnace.selection_priority = (receiver.selection_priority or 50) + 1
+furnace.scale_entity_info_icon = true
+table.insert(furnace.flags, 'placeable-off-grid')
 
 data:extend{furnace}
