73c0322fd8d499de684204ba9fbf60baa01a6020
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-19 16:02:50 +0100

    Make compatible with k2, add icons & localised name

--- mods/se-energy-beam-receiver-doubles-as-a-fast-furnace/data.lua
+++ mods/se-energy-beam-receiver-doubles-as-a-fast-furnace/data.lua
@@ -2,6 +2,10 @@ local receiver = data.raw['reactor']['se-energy-receiver']
 
 local furnace = table.deepcopy(data.raw['furnace']['electric-furnace'])
 furnace.name = 'se-energy-receiver-electric-furnace'
+furnace.localised_name = {'entity-name.' .. receiver.name}
+
+furnace.icon = receiver.icon
+furnace.icon_size = receiver.icon_size
 
 furnace.collision_mask = {}
 furnace.collision_box = receiver.collision_box
@@ -9,6 +13,6 @@ furnace.selection_box = {
   {receiver.selection_box[1][1] + 0.5, receiver.selection_box[1][2] + 0.5},
   {receiver.selection_box[2][1] - 0.5, receiver.selection_box[2][2] - 0.5},
 }
-furnace.selection_priority = (receiver.selection_priority or 50) +1
+furnace.selection_priority = (receiver.selection_priority or 50) + 1
 
 data:extend{furnace}
