2da8c92ccfc3d6d638f153a3030f5bca52bb3d35
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-19 16:59:39 +0100

    〃 & place on new and existing receivers

---
+++ mods/se-energy-beam-receiver-doubles-as-a-fast-furnace/control.lua
@@ -0,0 +1,40 @@
+local function on_created_entity(event)
+  local entity = event.created_entity or event.entity or event.destination
+
+  local furnace = entity.surface.create_entity{
+    name = 'se-energy-receiver-electric-furnace',
+    force = entity.force,
+    position = {entity.position.x, entity.position.y - 2.5},
+  }
+
+  furnace.destructible = false
+
+  global.structs[entity.unit_number] = {
+    unit_number = entity.unit_number,
+
+    reactor = entity,
+    furnace = furnace,
+  }
+end
+
+for _, event in ipairs({
+  defines.events.on_built_entity,
+  defines.events.on_robot_built_entity,
+  defines.events.script_raised_built,
+  defines.events.script_raised_revive,
+  -- defines.events.on_entity_cloned,
+}) do
+  script.on_event(event, on_created_entity, {
+    {filter = 'name', name = 'se-energy-receiver'},
+  })
+end
+
+script.on_init(function(event)
+  global.structs = {}
+
+  for _, surface in pairs(game.surfaces) do
+    for _, entity in pairs(surface.find_entities_filtered({name = {'se-energy-receiver'}})) do
+      on_created_entity({entity = entity})
+    end
+  end
+end)
