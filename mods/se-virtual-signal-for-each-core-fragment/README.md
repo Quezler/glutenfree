0d00c60d4ecccd205d52af557b7ef31b448bb3b3
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-23 22:07:18 +0100

    Mod portal release

---
+++ mods/se-virtual-signal-for-each-core-fragment/README.md
@@ -0,0 +1,1 @@
+Made on request to aid in cybersyn networks.
---
+++ mods/se-virtual-signal-for-each-core-fragment/data-updates.lua
@@ -0,0 +1,35 @@
+local function starts_with(str, start)
+  return str:sub(1, #start) == start
+end
+
+data:extend{{
+  type = "item-subgroup",
+  name = "se-core-fragment-signals",
+  group = "signals",
+  order = "e"
+}}
+
+local signal_background = {
+  icon =  "__base__/graphics/icons/signal/signal_blue.png",
+  icon_size = 64, icon_mipmaps = 4,
+}
+
+for _, item in pairs(data.raw['item']) do
+  if starts_with(item.name, 'se-core-fragment-') then
+    local signal = {
+      type = 'virtual-signal',
+      name = item.name .. '-virtual-signal',
+      icons = item.icons,
+      localised_name = {"", "Signal", " ", item.localised_name or {'item-name.' .. item.name}},
+      subgroup = 'se-core-fragment-signals',
+      order = item.order,
+    }
+    
+    for _, icon in ipairs(signal.icons) do
+      icon.scale = (icon.scale or 1) * 0.7
+    end
+    table.insert(signal.icons, 1, signal_background)
+
+    data:extend{signal}
+  end
+end
