2ffefc205a0118d4de2076f88cc6ccd881dda6fa
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-02-23 12:49:08 +0100

    Refresh the zonelist whenever a button is clicked

--- mods/se-zone-can-be-given-description-like-module-city/control.lua
+++ mods/se-zone-can-be-given-description-like-module-city/control.lua
@@ -14,6 +14,27 @@ local function update_zonelist_for_player(player, root)
 
     name_cell.caption = string.format('Module City [font=default-smaller]%s[/font]', name_cell.caption)
   end
+
+  local zone_index = nil
+  if not zone_index then
+    local parent = util.get_gui_element(root, Zonelist.path_zone_data_flow)
+    if not parent then return end
+
+    local container = parent[Zonelist.name_zone_data_container_frame]
+    local content = container[Zonelist.name_zone_data_content_scroll_pane]
+
+    local button_flow = content.parent.parent[Zonelist.name_zone_data_bottom_button_flow]
+    local view_button = button_flow[Zonelist.name_zone_data_view_surface_button]
+    zone_index = view_button.tags.zone_index
+
+    if view_button.tags.zone_type == "spaceship" then
+      -- coremining_header.visible = false
+      -- coremining.visible = false
+      return
+    end
+  end
+
+  game.print(zone_index)
 end
 
 local function on_zonelist_opened(event)
@@ -31,3 +52,18 @@ end
 
 script.on_init(register_events)
 script.on_load(register_events)
+
+script.on_event(defines.events.on_gui_click, function(event)
+  local player = game.get_player(event.player_index)
+
+  local root = Zonelist.get(player)
+  if not root then return end
+
+  update_zonelist_for_player(player, root)
+
+  -- local parent = Util.get_gui_element(root, Zonelist.path_zone_data_flow)
+  -- if not parent then return end
+
+  -- local container = parent[Zonelist.name_zone_data_container_frame]
+  -- local content = container[Zonelist.name_zone_data_content_scroll_pane]
+end)
