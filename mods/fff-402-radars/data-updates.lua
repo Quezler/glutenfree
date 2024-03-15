3bfb6979c65574e2a667a576b086ffafdd19dfdf
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-03-15 17:04:26 +0100

    Create a spoke entity

--- mods/fff-402-radars/data-updates.lua
+++ mods/fff-402-radars/data-updates.lua
@@ -5,9 +5,32 @@ local circuit_connector_definition = circuit_connector_definitions.create(univer
   {{variation = 24, main_offset = util.by_pixel(-42.5, -10), shadow_offset = util.by_pixel(-42, -7.5), show_shadow = false}}
 )
 
+local w_pole = data.raw['item']['small-electric-pole']
 local r_wire = data.raw['item']['red-wire']
 
 for _, radar in pairs(data.raw['radar']) do
+  data:extend{{
+    type = 'container',
+    name = mod_prefix .. radar.name .. '-spoke',
+    localised_name = {"", {'entity-name.' .. radar.name}, ' (fff 402 spoke)'},
+
+    -- selectable_in_game = false,
+    selection_box = radar.selection_box,
+    collision_box = radar.collision_box,
+    collision_mask = {},
+
+    inventory_size = 0,
+    picture = util.empty_sprite(),
+
+    circuit_wire_max_distance = corner_to_center,
+    draw_circuit_wires = false,
+
+    icons = {
+      {icon = radar.icon, icon_size = radar.icon_size, icon_mipmaps = radar.icon_mipmaps},
+      {icon = w_pole.icon, icon_size = w_pole.icon_size, icon_mipmaps = w_pole.icon_mipmaps, scale = 0.5},
+    }
+  }}
+
   data:extend{{
     type = 'container',
     name = mod_prefix .. radar.name .. '-red-wire',
