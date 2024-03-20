a26ffaf2ec9803163c1243196059b577bf01aca0
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-03-20 06:52:56 +0100

    Give the filtered storage tanks custom icons

--- mods/factoryplanner-is-exportable-to-fake-factorissimo/data-final-fixes.lua
+++ mods/factoryplanner-is-exportable-to-fake-factorissimo/data-final-fixes.lua
@@ -24,25 +24,33 @@ data.raw["container"][mod_prefix .. "container-3"].additional_pastable_entities 
 data:extend{
   {
     type = 'item-subgroup',
-    name = mod_prefix .. 'fluid-port-in',
+    name = mod_prefix .. 'storage-tanks',
 
     group = 'other',
-  },
-  {
-    type = 'item-subgroup',
-    name = mod_prefix .. 'fluid-port-out',
-
-    group = 'other',
-  },
+  }
 }
 
+if data.raw['fluid']['se-decompressing-steam'] then
+  data.raw['fluid']['se-decompressing-steam'].fietff_storage_tank_ignore = true
+end
+
 for _, fluid in pairs(data.raw['fluid']) do
+  if fluid.hidden then goto continue end
+  if fluid.fietff_storage_tank_ignore then goto continue end
+
+  local icons1 = table.deepcopy(data.raw['item'][mod_prefix .. 'item-' .. 1].icons)
+  local icons2 = fluid.icons and table.deepcopy(fluid.icons) or {{icon = fluid.icon, icon_size = fluid.icon_size, icon_mipmaps = fluid.icon_mipmaps}}
+
   data:extend{
     {
       type = 'storage-tank',
       name = string.format(mod_prefix .. 'storage-tank-%s', fluid.name),
       localised_name = {"entity-name.fietff-storage-tank-fluidname", fluid.name},
 
+      subgroup = mod_prefix .. 'storage-tanks',
+      icons = util.combine_icons(icons1, icons2, {scale = 0.5}),
+
+      collision_mask = {},
       collision_box = {{-0.4, -0.4}, {0.4, 0.4}},
       selection_box = {{-0.4, -0.4}, {0.4, 0.4}},
       selection_priority = 51,
@@ -71,8 +79,9 @@ for _, fluid in pairs(data.raw['fluid']) do
       max_health = 100,
 
       -- flags = {"hide-alt-info"},
+      show_fluid_icon = false,
     }
   }
-end
 
--- data.raw['storage-tank']['kr-fluid-storage-2'].fluid_box.filter = 'water'
+  ::continue::
+end
