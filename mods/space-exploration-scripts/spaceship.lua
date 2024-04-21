b169c77bde72e9ef7c10c739336d616b5b8947f2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-04-21 14:24:12 +0200

    Fix `attempt to index global 'core_util' (a nil value)`

--- mods/space-exploration-scripts/spaceship.lua
+++ mods/space-exploration-scripts/spaceship.lua
@@ -1,6 +1,7 @@
 -- step 1
 mod_prefix = 'se-'
 Event = {addListener = function() end}
+core_util = require('__core__/lualib/util.lua')
 SpaceshipClamp = require('__space-exploration__.scripts.spaceship-clamp')
 
 -- step 2
@@ -8,6 +9,7 @@ local Spaceship = require('__space-exploration__.scripts.spaceship')
 
 -- step 3
 SpaceshipClamp = nil
+core_util = nil
 Event = nil
 mod_prefix = nil
 
