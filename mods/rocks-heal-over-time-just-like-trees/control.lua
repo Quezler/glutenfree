0a464bbab8f4de523d91498b0f197b7f5ba05d7f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-06-26 11:03:49 +0200

    Optimize mod for auto glaive

--- mods/rocks-heal-over-time-just-like-trees/control.lua
+++ mods/rocks-heal-over-time-just-like-trees/control.lua
@@ -1,16 +1,12 @@
 local function already_in_damaged_rocks(entity)
-  for _, e in pairs(global.damaged_rocks) do
-    if e.valid and entity.surface.index == e.surface.index and entity.position.x == e.position.x and entity.position.y == e.position.y then
-      return true
-    end
-  end
+  local key = entity.position.x .. ',' .. entity.position.x .. ',' .. entity.surface.name
 
-  return false
+  return global.damaged_rocks[key] ~= nil
 end
 
 local function maybe_add_to_damaged_rocks(entity)
   if string.find(entity.name, "rock") and 1 > entity.get_health_ratio() then
-    if global.skip_duplicate_check or not already_in_damaged_rocks(entity) then
+    if not already_in_damaged_rocks(entity) then
       table.insert(global.damaged_rocks, entity)
     end
   end
@@ -21,30 +17,26 @@ script.on_event(defines.events.on_entity_damaged, function(event)
 end, {{filter = 'type', type = 'simple-entity'}})
 
 local function on_init(event)
+  global = {}
   global.damaged_rocks = {}
-  global.skip_duplicate_check = true
 
   for _, surface in pairs(game.surfaces) do
     for _, entity in pairs(surface.find_entities_filtered{type = 'simple-entity'}) do
       maybe_add_to_damaged_rocks(entity)
     end
   end
-
-  global.skip_duplicate_check = false
-  log('#global.damaged_rocks = ' .. #global.damaged_rocks)
 end
 
 script.on_init(on_init)
--- script.on_configuration_changed(on_configuration_changed)
+script.on_configuration_changed(on_init)
 
 script.on_nth_tick(60 * 10, function(event) -- heal by 1 health every 10 seconds, this is on-par with trees.
-  for i = #global.damaged_rocks, 1, -1 do
-    local damaged_rock = global.damaged_rocks[i]
+  for _, damaged_rock in ipairs(global.damaged_rocks) do
 
     if damaged_rock.valid and 1 > damaged_rock.get_health_ratio() then
       damaged_rock.health = damaged_rock.health + 1
     else
-      table.remove(global.damaged_rocks, i)
+      global.damaged_rocks[_] = nil
     end
   end
 end)
