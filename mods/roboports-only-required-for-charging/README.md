f20504d4887cb7954dcdf6fe186ff47a24ddf320
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-08-26 15:02:29 +0200

    Publish surface wide roboport mod

---
+++ mods/roboports-only-required-for-charging/README.md
@@ -0,0 +1,5 @@
+Just a proof of concept basically, not really feeling in the mood to expand roboports outwards to new remote mining sites and wait for them to build themselves, ofc you'll still need roboports along the way to make sure the bots can charge.
+
+Honestly it feels a bit too uncanny valley with the roboports not able to show their own green/orange ranges without me doing some force shenannigans that's outside the scope of this proof of concept.
+
+Also the mod currently does not care about special surfaces, multiple teams, the entity somehow being deleted, etc.
---
+++ mods/roboports-only-required-for-charging/control.lua
@@ -0,0 +1,22 @@
+local function on_surface_created(event)
+  local surface = game.get_surface(event.surface_index)
+  assert(surface)
+
+  assert(surface.find_entity('rorfc-roboport', {0, 0}) == nil)
+
+  surface.create_entity{
+    name = 'rorfc-roboport',
+    position = {0, 0},
+    force = 'player',
+  }
+
+  assert(surface.find_entity('rorfc-roboport', {0, 0}) ~= nil)
+end
+
+script.on_event(defines.events.on_surface_created, on_surface_created)
+
+script.on_init(function(event)
+  for _, surface in pairs(game.surfaces) do
+    on_surface_created({surface_index = surface.index})
+  end
+end)
