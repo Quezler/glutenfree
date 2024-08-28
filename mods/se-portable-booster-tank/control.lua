f74861a7d5ed69cfbdb5ac1be269bc4279731960
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-08-28 12:38:10 +0200

    Support construction robots

--- mods/se-portable-booster-tank/control.lua
+++ mods/se-portable-booster-tank/control.lua
@@ -15,7 +15,7 @@ local function get_tank_capacity(entity_name)
   return tank_capacity[entity_name]
 end
 
-script.on_event(defines.events.on_player_mined_entity, function(event)
+local function on_mined_entity(event)
   local itemstack, slot = event.buffer.find_item_stack(event.entity.name)
   assert(itemstack and itemstack.is_item_with_tags)
 
@@ -29,12 +29,14 @@ script.on_event(defines.events.on_player_mined_entity, function(event)
     fluid_temperature = fluid.temperature,
   })
 
-  fluid_color = game.fluid_prototypes[fluid.name].flow_color
+  global.next_id = global.next_id + 1
 
   local blocks = 17 -- matches the width the box already gets expanded to due to the 'Space Exploration - portable booster tank' prototype history (on my screen)
   local fullness = fluid.amount / get_tank_capacity(event.entity.name) -- 0-1
   local full_blocks = math.max(1, math.floor(blocks * fullness))
   local empty_blocks = blocks - full_blocks
+
+  fluid_color = game.fluid_prototypes[fluid.name].flow_color
 
   itemstack.custom_description = {'',
   string.format('[fluid=%s] ', fluid.name), '[color=255,230,192][font=default-bold]', {'fluid-name.se-liquid-rocket-fuel'}, '[/font][/color]',
@@ -45,10 +47,10 @@ script.on_event(defines.events.on_player_mined_entity, function(event)
   '[color=gray]', string.rep('█', empty_blocks), '[/color]',
   '\n',
   util.format_number(fluid.amount, true), ' at ', {'format-degrees-c-compact', math.ceil(fluid.temperature)}}
+end
 
-  global.next_id = global.next_id + 1
-
-end, booster_tanks_filter)
+script.on_event(defines.events.on_player_mined_entity, on_mined_entity, booster_tanks_filter)
+script.on_event(defines.events.on_robot_mined_entity, on_mined_entity, booster_tanks_filter)
 
 script.on_init(function(event)
  global.next_id = 1 -- to make sure booster tanks are unable to stack
