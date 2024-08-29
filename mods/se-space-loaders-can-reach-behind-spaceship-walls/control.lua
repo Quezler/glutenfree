ff4986a93bb3be8db9614c2c5e8a61520013eaee
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-08-29 19:59:32 +0200

    Rework the mod into a loader with 1 extra reach

--- mods/se-space-loaders-can-reach-behind-spaceship-walls/control.lua
+++ mods/se-space-loaders-can-reach-behind-spaceship-walls/control.lua
@@ -1,45 +1,58 @@
-local human_direction = {
-  [defines.direction.north] = 'north',
-  [defines.direction.east] = 'east',
-  [defines.direction.south] = 'south',
-  [defines.direction.west] = 'west',
-}
+-- local human_direction = {
+--   [defines.direction.north] = 'north',
+--   [defines.direction.east] = 'east',
+--   [defines.direction.south] = 'south',
+--   [defines.direction.west] = 'west',
+-- }
 
-local util = require('util')
+-- local util = require('util')
 
-commands.add_command('loader-through-spaceship-wall', nil, function(command)
-  local player = assert(game.get_player(command.player_index))
+-- commands.add_command('loader-through-spaceship-wall', nil, function(command)
+--   local player = assert(game.get_player(command.player_index))
 
-  local selected = player.selected
-  if selected == nil then return end
-  if selected.name ~= 'kr-se-loader' then return end
+--   local selected = player.selected
+--   if selected == nil then return end
+--   if selected.name ~= 'kr-se-loader' then return end
 
-  local surface = selected.surface
-  local direction = selected.loader_type == "input" and selected.direction or util.oppositedirection(selected.direction)
-  game.print(human_direction[direction])
+--   local surface = selected.surface
+--   local direction = selected.loader_type == "input" and selected.direction or util.oppositedirection(selected.direction)
+--   game.print(human_direction[direction])
 
-  local try_container_at = util.moveposition({selected.position.x, selected.position.y}, direction, 2)
+--   local try_container_at = util.moveposition({selected.position.x, selected.position.y}, direction, 2)
 
-  rendering.draw_circle{
-    color = {1, 1, 1},
-    radius = 0.1,
-    filled = true,
-    surface = surface,
-    target = try_container_at,
-    time_to_live = 60,
-  }
+--   rendering.draw_circle{
+--     color = {1, 1, 1},
+--     radius = 0.1,
+--     filled = true,
+--     surface = surface,
+--     target = try_container_at,
+--     time_to_live = 60,
+--   }
 
-  local containers = surface.find_entities_filtered{
-    position = try_container_at,
-    type = {'container', 'logistic_container'},
-    force = selected.force,
-    limit = 1,
-  }
+--   local containers = surface.find_entities_filtered{
+--     position = try_container_at,
+--     type = {'container', 'logistic_container'},
+--     force = selected.force,
+--     limit = 1,
+--   }
 
-  if #containers == 0 then return end
-  local container = containers[1]
+--   if #containers == 0 then return end
+--   local container = containers[1]
 
-  container.teleport(util.moveposition({container.position.x, container.position.y}, direction, -1))
-  selected.update_connections()
-  container.teleport(util.moveposition({container.position.x, container.position.y}, direction,  1))
+--   container.teleport(util.moveposition({container.position.x, container.position.y}, direction, -1))
+--   selected.update_connections()
+--   container.teleport(util.moveposition({container.position.x, container.position.y}, direction,  1))
+-- end)
+
+script.on_event(defines.events.on_player_setup_blueprint, function(event)
+  local player = assert(game.get_player(event.player_index))
+  local item = player.blueprint_to_setup
+
+  local entities = item.get_blueprint_entities() or {}
+  for _, entity in ipairs(entities) do
+    if entity.name == 'kr-se-loader-spaceship' then
+      entity.name = 'kr-se-loader'
+    end
+  end
+  item.set_blueprint_entities(entities)
 end)
---
+++ mods/se-space-loaders-can-reach-behind-spaceship-walls/data.lua
@@ -0,0 +1,7 @@
+local loader = table.deepcopy(data.raw['loader-1x1']['kr-se-loader'])
+
+loader.name = loader.name .. '-spaceship'
+loader.container_distance = loader.container_distance + 1
+loader.placeable_by = {item = "kr-se-loader", count = 1}
+
+data:extend{loader}
