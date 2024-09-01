916ecdca896411c9c9d44c114de6d76f7da2b116
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-09-01 17:59:32 +0200

    Mod portal release

---
+++ mods/k2-no-tree-and-rock-recipes/README.md
@@ -0,0 +1,1 @@
+Honestly it should be a Krastorio 2 startup option, they have plenty options already, one more won't hurt.
---
+++ mods/k2-no-tree-and-rock-recipes/data.lua
@@ -0,0 +1,10 @@
+data.raw['technology']['kr-decorations'] = nil
+
+-- allright so apparently krastorio has this check (GNU LGPLv3) in their code,
+-- so all i apparently needed to do was disable the tech, i went into making this mod
+-- fully prepared i had to find and yeet all the rocks and trees but someone saw this coming.
+
+-- local tech = data.raw["technology"]["kr-decorations"]
+-- if not tech then
+--   return
+-- end
