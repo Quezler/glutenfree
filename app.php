ceb8fb400c389f6d272889dbeaa4c972c53578af
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-09-18 06:15:37 +0200

    Make file_get_contents() call /api/mods/ with an user agent

--- app.php
+++ app.php
@@ -12,6 +12,8 @@ $application = new Application();
 $dotenv = new Dotenv();
 $dotenv->load(__GLUTENFREE__ . '/.env');
 
+ini_set('user_agent', 'https://github.com/Quezler/glutenfree');
+
 $application->add(new \App\Command\InitCommand());
 $application->add(new \App\Command\RsyncCommand());
 $application->add(new \App\Command\PublishCommand());
