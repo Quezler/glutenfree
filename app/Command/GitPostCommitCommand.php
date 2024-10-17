e27dd1ccd19a24e7d718559b7e9ea250fb796a44
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-17 21:22:16 +0200

    Try to detect which mods got modified in the last commit

--- app/Command/GitPostCommitCommand.php
+++ app/Command/GitPostCommitCommand.php
@@ -18,10 +18,33 @@ class GitPostCommitCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $modified_mod_directories = $this->getModifiedDirectories();
+        dump($modified_mod_directories);
+
         foreach (ExpansionMods::list() as $expansionMod) {
-            $expansionMod->tryAddNewSectionToChangelog();
+            if ($modified_mod_directories[$expansionMod->directory]) {
+//                $expansionMod->tryAddNewSectionToChangelog();
+            }
         }
 
         return Command::SUCCESS;
+    }
+
+    private function getModifiedDirectories(): array
+    {
+        exec('git log -1 HEAD --raw', $lines);
+
+        $modified_mod_directories = [];
+
+        foreach ($lines as $line) {
+            if (str_starts_with($line, ':')) {
+                preg_match('/mods_2\.0\/(.*)\//', $line, $matches);
+                if (count($matches) > 0) {
+                    $modified_mod_directories[] = $matches[1];
+                }
+            }
+        }
+
+        return $modified_mod_directories;
     }
 }
