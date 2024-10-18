2a6af4fb43b754df1119ce830db9985919beea00
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-18 20:27:25 +0200

    Rework entities on the hidden surface to make room for multiple forces

--- mods_2.0/008_awesome-sink/control.lua
+++ mods_2.0/008_awesome-sink/control.lua
@@ -9,71 +9,76 @@ script.on_init(function ()
   storage.version = 0
   storage.surfacedata = {}
 
-  local surface = game.surfaces[mod_surface_name]
+  local mod_surface = game.surfaces[mod_surface_name]
   assert(surface == nil, "contact the mod author for help with world that previously already had this mod installed.")
 
-  surface = game.create_surface(mod_surface_name)
-  surface.generate_with_lab_tiles = true
+  mod_surface = game.create_surface(mod_surface_name)
+  mod_surface.generate_with_lab_tiles = true
 
-  for surface_index, surface in pairs(game.surfaces) do
-    Handler.on_surface_created({surface_index = surface_index})
+  for _, surface in pairs(game.surfaces) do
+    Handler.on_surface_created({surface_index = surface.index})
   end
 end)
 
 function Handler.on_surface_created(event)
-  local surface = assert(game.get_surface(event.surface_index))
-  local surfacedata = {}
-
-  local mod_surface = game.surfaces[mod_surface_name]
-
-  local y_offset = 7 * (surface.index - 1)
-
-  surfacedata["awesome-shop"] = mod_surface.create_entity{
-    name = "awesome-shop",
-    force = "player",
-    position = {-1.5, 0.5 + y_offset},
+  storage.surfacedata[event.surface_index] = {
+    force_to_constant_combinator = {},
   }
-  surfacedata["awesome-shop"].link_id = surface.index
+end
 
-  surfacedata["display-panel"] = mod_surface.create_entity{
-    name = "display-panel",
-    force = "player",
-    position = {-1.5, 1.5 + y_offset},
-  }
-  -- local display_panel_control = surfacedata["display-panel"].get_control_behavior()
-  -- display_panel_control.set_message()
-
-  surfacedata["awesome-decider-combinator"] = mod_surface.create_entity{
-    name = "awesome-decider-combinator",
-    force = "player",
-    position = {-0.5, 1.0 + y_offset},
-    direction = defines.direction.north,
-  }
-
-  local green_out = surfacedata["awesome-decider-combinator"].get_wire_connector(defines.wire_connector_id.combinator_output_green)
-  local green_in  = surfacedata["awesome-decider-combinator"].get_wire_connector(defines.wire_connector_id.combinator_input_green)
-  green_out.connect_to(green_in, false)
-
-  surfacedata["awesome-decider-combinator"].get_control_behavior().parameters = decider_combinator_parameters
-
-  storage.surfacedata[surface.index] = surfacedata
+function Handler.on_surface_deleted(event)
+  error("surface deletion not handled yet.")
 end
 
 script.on_event(defines.events.on_surface_created, Handler.on_surface_created)
-script.on_event(defines.events.on_surface_deleted, Handler.on_surface_deleted) -- todo
+script.on_event(defines.events.on_surface_deleted, Handler.on_surface_deleted)
+
+function Handler.get_or_create_decider_combinator(surface_index, force_index)
+  local decider_combinator = storage.surfacedata[surface_index].force_to_constant_combinator[force_index]
+  if decider_combinator then return decider_combinator end
+
+  local mod_surface = game.surfaces[mod_surface_name]
+
+  local y_offset = 7 * (surface_index - 1)
+  local position = {0.5 - force_index, 1.0 + y_offset}
+  assert(mod_surface.find_entity("awesome-decider-combinator", position) == nil)
+
+  decider_combinator = mod_surface.create_entity{
+    name = "awesome-decider-combinator",
+    force = force_index,
+    position = position,
+    direction = defines.direction.north,
+  }
+  assert(decider_combinator)
+
+  local green_out = decider_combinator.get_wire_connector(defines.wire_connector_id.combinator_output_green, false)
+  local green_in  = decider_combinator.get_wire_connector(defines.wire_connector_id.combinator_input_green, false)
+  green_out.connect_to(green_in, false, defines.wire_origin.player)
+
+  --- @diagnostic disable-next-line: inject-field
+  decider_combinator.get_control_behavior().parameters = decider_combinator_parameters
+  decider_combinator.combinator_description = string.format("surface %d (%s)\nforce %d (%s)",
+    surface_index, game.surfaces[surface_index].name,
+    force_index, game.forces[force_index].name
+  )
+
+  storage.surfacedata[surface_index].force_to_constant_combinator[force_index] = decider_combinator
+  return decider_combinator
+end
 
 function Handler.register_awesome_sink(entity)
+  local decider_combinator = Handler.get_or_create_decider_combinator(entity.surface.index, entity.force.index)
 end
 
 function Handler.on_created_entity(event)
   local entity = event.entity or event.destination
 
-  assert(entity.force.name == "player", "due to hidden surface entity positioning this mod currently supports 1 force.")
-
   if entity.name == "awesome-sink" then
     Handler.register_awesome_sink(entity)
   elseif entity.name == "awesome-shop" then
     entity.link_id = entity.surface.index
+  else
+    error(string.format("%s (%s)", entity.name, entity.type))
   end
 end
 
@@ -84,8 +89,8 @@ for _, event in ipairs({
   defines.events.script_raised_revive,
   defines.events.on_entity_cloned,
 }) do
-  script.on_event(event, on_created_entity, {
-    {filter = 'name', name = 'awesome-sink'},
-    {filter = 'name', name = 'awesome-shop'},
+  script.on_event(event, Handler.on_created_entity, {
+    {filter = "name", name = "awesome-sink"},
+    {filter = "name", name = "awesome-shop"},
   })
 end
