dc03e921dfbcd7e0cdd55800a7e50eb3c0d369e2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-18 08:17:39 +0200

    Cleanup prototype code

--- mods_2.0/007_platform-construction-only-no-construction-robots/control.lua
+++ mods_2.0/007_platform-construction-only-no-construction-robots/control.lua
@@ -1,27 +1,15 @@
-local type_to_string = {}
-for k, v in pairs(defines.robot_order_type) do
-  type_to_string[v] = k
-end
-
 script.on_event(defines.events.on_tick, function(event)
   local logistic_networks = game.forces["player"].logistic_networks["nauvis"] or {}
 
   for _, logistic_network in ipairs(logistic_networks) do
     for _, construction_robot in ipairs(logistic_network.construction_robots) do
-      local robot_order_queue = construction_robot.robot_order_queue
+      local robot_order = construction_robot.robot_order_queue[1]
 
-      for _, worker_robot_order in ipairs(robot_order_queue) do
-        worker_robot_order.type_human = type_to_string[worker_robot_order.type]
-      end
-
-      local current_order = robot_order_queue[1]
-      if current_order and current_order.target then -- target can sometimes be optional
+      if robot_order and robot_order.target then -- target can sometimes be optional
         -- todo: construction robots sleep when there is no enemy around, pr or spawn invisible biters?
         -- looks like ->activeNeighbourForcesSet/show-active-forces-around debug is rather generous btw
-        assert(construction_robot.teleport(current_order.target.position))
+        assert(construction_robot.teleport(robot_order.target.position))
       end
-
-      log(serpent.block( robot_order_queue ))
     end
   end
 end)
