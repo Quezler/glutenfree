6ef0fbebfa208a29df2513c925a4c45a67433b8e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-18 07:47:40 +0200

    Experiment with teleporting each bot to their target

---
+++ mods_2.0/007_platform-construction-only-no-construction-robots/control.lua
@@ -0,0 +1,25 @@
+local type_to_string = {}
+for k, v in pairs(defines.robot_order_type) do
+  type_to_string[v] = k
+end
+
+script.on_event(defines.events.on_tick, function(event)
+  local logistic_networks = game.forces["player"].logistic_networks["nauvis"] or {}
+
+  for _, logistic_network in ipairs(logistic_networks) do
+    for _, construction_robot in ipairs(logistic_network.construction_robots) do
+      local robot_order_queue = construction_robot.robot_order_queue
+
+      for _, worker_robot_order in ipairs(robot_order_queue) do
+        worker_robot_order.type_human = type_to_string[worker_robot_order.type]
+      end
+
+      local current_order = robot_order_queue[1]
+      if current_order then
+        assert(construction_robot.teleport(current_order.target.position))
+      end
+
+      log(serpent.block( robot_order_queue ))
+    end
+  end
+end)
