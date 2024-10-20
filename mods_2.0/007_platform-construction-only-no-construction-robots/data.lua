df8a6482002d604451be473bbc03c8d41873b2ba
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-20 20:48:33 +0200

    Turn construction robots invisible

---
+++ mods_2.0/007_platform-construction-only-no-construction-robots/data.lua
@@ -0,0 +1,12 @@
+local construction_robot = data.raw["construction-robot"]["construction-robot"]
+
+construction_robot.idle = util.empty_sprite()
+-- construction_robot.idle_with_cargo = util.empty_sprite()
+construction_robot.in_motion = util.empty_sprite()
+-- construction_robot.in_motion_with_cargo = util.empty_sprite()
+construction_robot.shadow_idle = util.empty_sprite()
+-- construction_robot.shadow_idle_with_cargo = util.empty_sprite()
+construction_robot.shadow_in_motion = util.empty_sprite()
+-- construction_robot.shadow_in_motion_with_cargo = util.empty_sprite()
+construction_robot.working = util.empty_sprite()
+construction_robot.shadow_working = util.empty_sprite()
