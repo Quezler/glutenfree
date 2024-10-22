4bc333cf308cc81e1b03d916873a4e83f443e148
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-22 20:45:59 +0200

    Place linked chest on rotated chomper tile

--- mods_2.0/010_upcycler/control.lua
+++ mods_2.0/010_upcycler/control.lua
@@ -10,6 +10,8 @@ function Handler.on_created_entity(event)
 
   local inserted = entity.get_inventory(defines.inventory.furnace_source).insert({name = "upcycle-any-quality", count = 1})
   assert(inserted > 0)
+
+  Handler.get_or_create_linkedchest_then_move(entity)
 end
 
 for _, event in ipairs({
@@ -22,4 +24,32 @@ for _, event in ipairs({
   script.on_event(event, Handler.on_created_entity, {
     {filter = "name", name = "upcycler"},
   })
+end
+
+function Handler.get_or_create_linkedchest_then_move(entity)
+  local linked_chest = entity.surface.find_entities_filtered{
+    name = "linked-chest",
+    area = entity.bounding_box,
+    limit = 1,
+  }
+
+  linked_chest = #linked_chest and linked_chest[1] or nil
+
+  if linked_chest == nil then
+    linked_chest = entity.surface.create_entity{
+      name = "linked-chest",
+      force = "neutral",
+      position = entity.position,
+    }
+  end
+
+  if entity.direction == defines.direction.north then
+    linked_chest.teleport({entity.position.x, entity.position.y + 1})
+  elseif entity.direction == defines.direction.east then
+    linked_chest.teleport({entity.position.x - 2, entity.position.y})
+  elseif entity.direction == defines.direction.south then
+    linked_chest.teleport({entity.position.x - 1, entity.position.y - 2})
+  elseif entity.direction == defines.direction.west then
+    linked_chest.teleport({entity.position.x + 1, entity.position.y - 1})
+  end
 end
