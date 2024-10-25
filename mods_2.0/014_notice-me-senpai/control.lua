1949f33324424382ef007512974f414b3ad9b9e9
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-25 14:34:56 +0200

    Do not check for held item every time the player moves

--- mods_2.0/014_notice-me-senpai/control.lua
+++ mods_2.0/014_notice-me-senpai/control.lua
@@ -3,29 +3,34 @@ for _, item in pairs(prototypes.item) do
   local place_result = item.place_result
   if place_result and place_result.type == "mining-drill" then
     is_mining_drill[item.name] = true
+  else
+    is_mining_drill[item.name] = false -- not strictly needed
   end
 end
 
-script.on_event(defines.events.on_player_changed_position, function(event)
+-- true, false, nil
+local function is_player_holding_drill(player)
+  if player.cursor_ghost then
+    return is_mining_drill[player.cursor_ghost.name.name]
+  end
+
+  if player.cursor_stack.valid_for_read then
+    return is_mining_drill[player.cursor_stack.prototype.name]
+  end
+end
+
+script.on_init(function()
+  storage.playerdata = {}
+end)
+
+script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
   local player = game.get_player(event.player_index)
   assert(player)
 
-  -- game.print(serpent.line(player.position))
-  -- game.print(serpent.line(player.cursor_ghost))
+  game.print(event.tick .. ' ' .. serpent.line( is_player_holding_drill(player) ))
+end)
 
-  local item_prototype = player.cursor_ghost and player.cursor_ghost.name or nil
-  if item_prototype == nil then
-    if player.cursor_stack.valid_for_read then
-      item_prototype = player.cursor_stack.prototype
-    end
-  end
 
-  -- game.print(serpent.line(item_prototype))
-  -- game.print(serpent.line(is_mining_drill))
-  -- game.print(serpent.line(item_prototype.name))
-
-  if item_prototype == nil then return end -- player is holding neither an item or ghost
-  if not is_mining_drill[item_prototype.name] then return end
-
-  game.print("drill held @ " .. serpent.line(player.position))
+script.on_event(defines.events.on_player_changed_position, function(event)
+  --
 end)
