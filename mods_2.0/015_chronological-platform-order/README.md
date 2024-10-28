95d87b5cd9153cff76e53d5ae138d4e01e33cb83
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-28 21:13:01 +0100

    Mod portal release

---
+++ mods_2.0/015_chronological-platform-order/README.md
@@ -0,0 +1,5 @@
+It is annoying when the order of the platforms change when they get renamed, since it is not always desirable.
+
+This mod adds some invisible text to the start of each platform name that forces them to render chronologically.
+
+Note that you will need game version 2.0.13 or above, which is currently not yet out, not even as an experimental.
---
+++ mods_2.0/015_chronological-platform-order/control.lua
@@ -0,0 +1,52 @@
+function string:startswith(start)
+  return self:sub(1, #start) == start
+end
+
+local function handle_surface(surface)
+  local platform = surface.platform
+  if not platform then return end
+
+  -- /c game.print(game.player.surface.platform.name)
+  -- log(surface.name .. ' ' .. platform.name)
+
+  -- local prefix = string.format("[color=1,1,1,0.%04d][/color]", surface.index)
+  local surface_name_without_platform_hyphen = string.sub(surface.name, string.len("platform-") + 1)
+  -- local prefix = string.format("[color=1,1,1,0.%04d][/color]", tonumber(surface_name_without_platform_hyphen))
+  local decimal = string.format("%04d", tonumber(surface_name_without_platform_hyphen))
+  local prefix = string.format("[color=1,1,1,0.%s][/color]", decimal)
+  local detected_prefix = string.sub(platform.name, 1, string.len(prefix))
+
+  if detected_prefix ~= prefix then
+    -- in case the user added text to the front, search for our sorting markers further down in the string and kill them
+    local platform_name = string.gsub(platform.name, "%[color=1,1,1,0%.".. decimal .. "%]%[/color%]", "")
+    platform.name = prefix .. platform_name
+    -- log(surface.name .. ' ' .. platform.name)
+  end
+
+  log(surface.name .. ' ' .. platform.name)
+end
+
+local function handle_surfaces()
+  for _, surface in pairs(game.surfaces) do
+    handle_surface(surface)
+  end
+end
+
+script.on_init(function()
+  handle_surfaces()
+end)
+
+-- there is no hub renamed event
+-- script.on_event(defines.events.on_surface_renamed, function(event)
+--   handle_surface(game.surfaces[event.surface_index])
+-- end)
+
+script.on_event(defines.events.on_gui_closed, function(event)
+  if event.entity then
+    if event.entity.name == "space-platform-hub" then
+      handle_surface(event.entity.surface)
+    end
+  end
+end)
+
+-- [color=1,1,1,0.990001][/color]
