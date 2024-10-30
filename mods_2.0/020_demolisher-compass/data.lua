e93a02574d509a0f2ed6f2e5eb40a0eeafde2141
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-10-30 14:47:29 +0100

    Load the compass sprite into items

--- mods_2.0/020_demolisher-compass/data.lua
+++ mods_2.0/020_demolisher-compass/data.lua
@@ -19,3 +19,23 @@ for _, demolisher_name in ipairs(demolisher_names) do
   assert(prototype.created_effect == nil, "another mod has added a created effect to a demolisher, guess we'll need to share now.")
   prototype.created_effect = created_effect
 end
+
+for i = 0, 27 do
+  local delay = (i == 7 or i == 21) and "15" or "05"
+  local icon = string.format("__demolisher-compass__/graphics/icons/compass/frame_%02d_delay-0.%ss.png", i, delay)
+  -- log(icon)
+
+  data:extend{{
+    type = "item",
+    -- name = "demolisher-compass-" .. i,
+    name = string.format("demolisher-compass-%02d", i),
+    localised_name = {"item-name.demolisher-compass"},
+
+    icon = icon,
+    icon_size = 160,
+
+    stack_size = 1,
+    flags = {"not-stackable", "only-in-cursor"},
+    -- hidden_in_factoriopedia = true,
+  }}
+end
