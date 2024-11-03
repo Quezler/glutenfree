2cc48cd571dfb48d2580f560b5feb885646f14df
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-03 11:02:29 +0100

    Stash leftovers from october 20 (post radar range lag)
    https://forums.factorio.com/viewtopic.php?f=7&t=116154

--- mods_2.0/007_platform-construction-only-no-construction-robots/data.lua
+++ mods_2.0/007_platform-construction-only-no-construction-robots/data.lua
@@ -1,12 +1,19 @@
 local construction_robot = data.raw["construction-robot"]["construction-robot"]
 
-construction_robot.idle = util.empty_sprite()
--- construction_robot.idle_with_cargo = util.empty_sprite()
-construction_robot.in_motion = util.empty_sprite()
--- construction_robot.in_motion_with_cargo = util.empty_sprite()
-construction_robot.shadow_idle = util.empty_sprite()
--- construction_robot.shadow_idle_with_cargo = util.empty_sprite()
-construction_robot.shadow_in_motion = util.empty_sprite()
--- construction_robot.shadow_in_motion_with_cargo = util.empty_sprite()
-construction_robot.working = util.empty_sprite()
-construction_robot.shadow_working = util.empty_sprite()
+local function turn_construction_robots_invisible()
+  construction_robot.idle = util.empty_sprite()
+  -- construction_robot.idle_with_cargo = util.empty_sprite()
+  construction_robot.in_motion = util.empty_sprite()
+  -- construction_robot.in_motion_with_cargo = util.empty_sprite()
+  construction_robot.shadow_idle = util.empty_sprite()
+  -- construction_robot.shadow_idle_with_cargo = util.empty_sprite()
+  construction_robot.shadow_in_motion = util.empty_sprite()
+  -- construction_robot.shadow_in_motion_with_cargo = util.empty_sprite()
+  construction_robot.working = util.empty_sprite()
+  construction_robot.shadow_working = util.empty_sprite()
+end
+
+-- turn_construction_robots_invisible()
+
+data.raw["roboport"]["roboport"].radar_range = 2
+data.raw["roboport"]["roboport"].construction_radius = 1000000
