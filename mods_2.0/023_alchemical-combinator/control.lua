4f5bc1f792652b0172244c7106d4d99fb266fe8b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-04 21:23:55 +0100

    Create the -active variant whilst you're hovering it

--- mods_2.0/023_alchemical-combinator/control.lua
+++ mods_2.0/023_alchemical-combinator/control.lua
@@ -1,18 +1,36 @@
+-- local is_alchemical_combinator = {
+--   ["alchemical-combinator"] = true,
+--   ["alchemical-combinator-active"] = true,
+-- }
+
 script.on_event(defines.events.on_selected_entity_changed, function(event)
   local player = game.get_player(event.player_index)
   assert(player)
+  local selected = player.selected
 
-  if player.selected and player.selected.name == "alchemical-combinator" then
+  if selected and selected.name == "alchemical-combinator" then
+    selected.surface.create_entity{
+      name = "alchemical-combinator-active",
+      force = selected.force,
+      position = selected.position,
+      direction = selected.direction,
+      create_build_effect_smoke = false,
+    }
+    return
+  end
+
+  if selected and selected.name == "alchemical-combinator-active" then
     player.play_sound{
       path = "alchemical-combinator-charge",
-      position = player.selected.position,
+      position = selected.position,
     }
   end
 
-  if event.last_entity and event.last_entity.name == "alchemical-combinator" then
+  if event.last_entity and event.last_entity.name == "alchemical-combinator-active" then
     player.play_sound{
       path = "alchemical-combinator-uncharge",
       position = event.last_entity.position,
     }
+    event.last_entity.destroy()
   end
 end)
--- mods_2.0/023_alchemical-combinator/data.lua
+++ mods_2.0/023_alchemical-combinator/data.lua
@@ -7,15 +7,26 @@ combinator_item.name = combinator.name
 combinator.minable.result = combinator_item.name
 combinator_item.place_result = combinator.name
 
+local combinator_active = table.deepcopy(combinator) -- mine result = alchemical combinator
+combinator_active.name = "alchemical-combinator-active"
+
 combinator.icon = "__alchemical-combinator__/graphics/icons/alchemical-combinator.png"
 combinator_item.icon = "__alchemical-combinator__/graphics/icons/alchemical-combinator.png"
+combinator_active.icon = "__alchemical-combinator__/graphics/icons/alchemical-combinator-active.png"
 
 combinator.sprites.north.layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator.png"
 combinator.sprites.east .layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator.png"
 combinator.sprites.south.layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator.png"
 combinator.sprites.west .layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator.png"
 
-data:extend{combinator, combinator_item}
+combinator_active.sprites.north.layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator-active.png"
+combinator_active.sprites.east .layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator-active.png"
+combinator_active.sprites.south.layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator-active.png"
+combinator_active.sprites.west .layers[1].filename = "__alchemical-combinator__/graphics/entity/combinator/alchemical-combinator-active.png"
+
+combinator_active.selection_priority = (combinator.selection_priority or 50) + 1
+
+data:extend{combinator, combinator_item, combinator_active}
 
 local sound_charge = {
   type = "sound",
