5efbd11dcf67b610caa72c6953634880ff4b921f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-05 09:19:02 +0100

    Support prismatic belts

---
+++ mods_2.0/002_lane-balancers/data-updates.lua
@@ -0,0 +1,12 @@
+for _, config in ipairs({
+  {prefix = ''},
+  {prefix = 'fast-'},
+  {prefix = 'express-'},
+  {prefix = 'turbo-'},
+}) do
+  local balancer = data.raw["lane-splitter"][config.prefix .. "lane-splitter"]
+  local splitter = data.raw[     "splitter"][config.prefix ..      "splitter"]
+  if balancer and splitter then
+    balancer.belt_animation_set = splitter.belt_animation_set
+  end
+end
