773845b14b53dcb1f022b0f5768318f09fe42c1f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-06 12:36:54 +0100

    Try to see of swapping can cancel deletion

--- mods_2.0/025_space-platform-graveyard/control.lua
+++ mods_2.0/025_space-platform-graveyard/control.lua
@@ -6,22 +6,27 @@ local space_platform_max_size = {{-1000, -1000}, {1000, 1000}}
 script.on_event(defines.events.on_entity_died, function(event)
   if event.entity.name == "space-platform-hub" then
     game.print(event.entity.name)
-    -- event.entity.surface.platform.cancel_deletion()
 
-    -- event.entity.surface.create_entity{
-    --   name = "space-platform-hub",
-    --   force = event.entity.force,
-    --   position = event.entity.position,
-    -- }
+    local surface = event.entity.surface
+    local force = event.entity.force
+    local position = event.entity.position
 
-    local player = game.players["Quezler"]
-
-    player.cursor_stack.set_stack({name = "blueprint", count = 1})
-    player.cursor_stack.create_blueprint{
-      surface = event.entity.surface,
-      force = event.entity.force,
-      area = space_platform_max_size,
-      always_include_tiles = true,
+    event.entity.destroy()
+    surface.create_entity{
+      name = "space-platform-hub",
+      force = force,
+      position = position,
     }
+    surface.platform.cancel_deletion()
+
+    -- local player = game.players["Quezler"]
+
+    -- player.cursor_stack.set_stack({name = "blueprint", count = 1})
+    -- player.cursor_stack.create_blueprint{
+    --   surface = event.entity.surface,
+    --   force = event.entity.force,
+    --   area = space_platform_max_size,
+    --   always_include_tiles = true,
+    -- }
   end
 end)
