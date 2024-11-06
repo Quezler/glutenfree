c0d8744640ff9fbe34e2e1f75ce8c95b58409b1d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-06 13:19:59 +0100

    Create a platform in graveyard orbit

--- mods_2.0/025_space-platform-graveyard/control.lua
+++ mods_2.0/025_space-platform-graveyard/control.lua
@@ -5,19 +5,19 @@ local space_platform_max_size = {{-1000, -1000}, {1000, 1000}}
 
 script.on_event(defines.events.on_entity_died, function(event)
   if event.entity.name == "space-platform-hub" then
-    game.print(event.entity.name)
+    -- game.print(event.entity.name)
 
-    local surface = event.entity.surface
-    local force = event.entity.force
-    local position = event.entity.position
+    -- local surface = event.entity.surface
+    -- local force = event.entity.force
+    -- local position = event.entity.position
 
-    event.entity.destroy()
-    surface.create_entity{
-      name = "space-platform-hub",
-      force = force,
-      position = position,
-    }
-    surface.platform.cancel_deletion()
+    -- event.entity.destroy()
+    -- surface.create_entity{
+    --   name = "space-platform-hub",
+    --   force = force,
+    --   position = position,
+    -- }
+    -- surface.platform.cancel_deletion()
 
     -- local player = game.players["Quezler"]
 
@@ -28,15 +28,25 @@ script.on_event(defines.events.on_entity_died, function(event)
     --   area = space_platform_max_size,
     --   always_include_tiles = true,
     -- }
+
+    -- game.print(event.entity.surface.platform.starter_pack)
+    local space_platform = event.entity.force.create_space_platform{
+      name = event.entity.surface.platform.name,
+      planet = "space-platform-graveyard",
+      -- starter_pack = event.entity.surface.platform.starter_pack, -- https://forums.factorio.com/120077
+      starter_pack = "space-platform-starter-pack",
+    }
+
+    space_platform.apply_starter_pack()
   end
 end)
 
-script.on_event(defines.events.on_entity_damaged, function(event)
-  if event.final_health == 0 then
-    game.print("death?")
-    event.entity.health = 1
-    -- event.entity.health = 0
-  end
-end, {
-  {filter = "type", type = "space-platform-hub"},
-})
+-- script.on_event(defines.events.on_entity_damaged, function(event)
+--   if event.final_health == 0 then
+--     game.print("death?")
+--     event.entity.health = 1
+--     -- event.entity.health = 0
+--   end
+-- end, {
+--   {filter = "type", type = "space-platform-hub"},
+-- })
---
+++ mods_2.0/025_space-platform-graveyard/data.lua
@@ -0,0 +1,11 @@
+data:extend{{
+  type = "planet",
+  name = "space-platform-graveyard",
+
+  icon = "__core__/graphics/icons/mip/ghost-entity.png",
+
+  distance = 0,
+  orientation = 0,
+
+  hidden = true,
+}}
