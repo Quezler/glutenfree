7fb380c3e4155efa5636469dd4443be3e53bc7f7
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 16:50:42 +0100

    Replace blueprint jank with .get_logistic_sections() (thanks boskid!)

--- mods_2.0/007_platform-construction-only-no-construction-robots/README.md
+++ mods_2.0/007_platform-construction-only-no-construction-robots/README.md
@@ -3,5 +3,3 @@ when they try to fulfill an entity ghost the platform build animation will play 
 
 The animation might not entirely match the platform speed in terms of how quick their neighbours start, how long they stay up, etc,
 there is a lot going on on the c++ side in terms of the building animations so i took some creative liberty when porting it to lua.
-
-**warning!** in versions below 2.0.16 your game can freeze/crash when deconstructing stuff, but constructing new stuff to see the animation is safe.
--- mods_2.0/007_platform-construction-only-no-construction-robots/scripts/logistic-network.lua
+++ mods_2.0/007_platform-construction-only-no-construction-robots/scripts/logistic-network.lua
@@ -2,46 +2,20 @@ local LogisticNetwork = {}
 
 local function remove_all_construction_robot_requests_from_roboport(entity)
   if entity.type ~= "roboport" then return true end -- character toggling editor mode?
-  local inventory = game.create_inventory(1)
-  inventory.insert({name = "blueprint"})
-  inventory[1].create_blueprint{
-    surface = entity.surface,
-    force = entity.force,
-    area = entity.bounding_box,
-  }
-
-  local blueprint_entities = inventory[1].get_blueprint_entities() or {}
-  assert(#blueprint_entities == 1, '#blueprint_entities > 1')
-  assert(blueprint_entities[1].name == entity.name, string.format("entity name is %s but expected %s.", blueprint_entities[1].name, entity.name))
-
-  -- error(serpent.block(blueprint_entities[1]))
 
   local dirty = false
 
-  local request_filters = blueprint_entities[1].request_filters
-  if request_filters then
-
-    for _, section in ipairs(request_filters.sections) do
-      for _, filter in ipairs(section.filters or {}) do
-        if filter.name == "construction-robot" and filter.count > 0 then
-          filter.count = 0
-          section.name = nil
-          dirty = true
-        end
+  local logistic_sections = entity.get_logistic_sections()
+  for _, section in ipairs(logistic_sections.sections) do
+    for i, filter in ipairs(section.filters) do
+      if filter.value.name == "construction-robot" and filter.min > 0 then
+        filter.min = 0
+        dirty = true
+        section.set_slot(i, filter)
       end
-    end
-
-    if dirty then
-      inventory[1].set_blueprint_entities(blueprint_entities)
-      inventory[1].build_blueprint{
-        surface = entity.surface,
-        force = entity.force,
-        position = entity.position,
-      }
     end
   end
 
-  inventory.destroy()
   return dirty
 end
 
