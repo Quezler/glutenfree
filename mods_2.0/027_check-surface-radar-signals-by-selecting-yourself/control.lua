d7e011fef3da69dc7e6a9cf5e26857b8e2971f53
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 19:07:41 +0100

    Cleanup control stage

--- mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
+++ mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
@@ -4,38 +4,6 @@ script.on_init(function()
 
 end)
 
--- script.on_nth_tick(60, function(event)
---   local player = game.players["Quezler"]
-
---   local textfield = player.gui.center[mod_prefix .. 'textfield']
---   if textfield == nil then
---     textfield = player.gui.center.add{
---       -- type = 'empty-widget',
---       type = 'frame',
---       name = mod_prefix .. 'textfield',
---     }
---   else
---   end
-
---   textfield.style.width = 100
---   textfield.style.height = 200
---   textfield.style.bottom_margin = 150
---   textfield.raise_hover_events = true
---   textfield.game_controller_interaction = defines.game_controller_interaction.never
---   textfield.ignored_by_interaction = true
--- end)
-
--- running this does cause it to fire the event, neat
--- /c game.player.gui.center["csrsbsy-textfield"].ignored_by_interaction = false
-
--- script.on_event(defines.events.on_gui_hover, function(event)
---   game.print("on_gui_hover")
--- end)
-
--- script.on_event(defines.events.on_gui_leave, function(event)
---   game.print("on_gui_leave")
--- end)
-
 -- script.on_event(defines.events.on_player_changed_position, function(event)
 --   game.print(event.tick)
 -- end)
@@ -46,14 +14,15 @@ end)
 
 commands.add_command('proxy-me', nil, function(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
-  -- {{id = {name = "quality-module-3"}, items = {in_inventory = {{inventory = 4, stack = 0}}}}}
-
   player.surface.create_entity{
     name = mod_prefix .. "item-request-proxy",
     force = player.force,
     position = player.position,
 
     target = player.character,
-    modules = {{id = {name = "radar"}, items = {in_inventory = {{inventory = 255, stack = 0}}}}}
+
+    -- `{inventory = 255, stack = 0}` is not needed apparently to keep the proxy alive,
+    -- neat because it stops the missing items alert when outside roboport range.
+    modules = {{id = {name = "radar"}, items = {in_inventory = {} }}}
   }
 end)
