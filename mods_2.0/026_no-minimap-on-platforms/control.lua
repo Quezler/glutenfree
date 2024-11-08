06c6a8cdf8a1b33d6a3f08924575512e32b4960f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 13:23:38 +0100

    Mod portal release

---
+++ mods_2.0/026_no-minimap-on-platforms/control.lua
@@ -0,0 +1,4 @@
+script.on_event(defines.events.on_player_changed_surface, function(event)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+  player.game_view_settings.show_minimap = player.surface.platform == nil
+end)
