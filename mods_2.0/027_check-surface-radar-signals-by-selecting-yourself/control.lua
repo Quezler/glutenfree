dc93b5a9ddb8f4f06d25a98753165715a455215c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 15:44:11 +0100

    Purge zoom related code

--- mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
+++ mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
@@ -1,8 +1,5 @@
 local mod_prefix = 'csrsbsy-'
 
-local max_zoom = 3
-local min_zoom = 
-
 script.on_init(function()
 
 end)
@@ -34,17 +31,6 @@ script.on_event(defines.events.on_gui_leave, function(event)
   game.print("on_gui_leave")
 end)
 
-script.on_event(defines.events.on_tick, function(event)
-  local player = game.get_player("Quezler") --[[@as LuaPlayer]]
-  game.print(serpent.line(player.zoom))
-end)
-
--- script.on_event("csrsbsy-zoom-in", function(event)
---   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
---   game.print(string.format("zoomed in to %f @ %d", player.zoom, event.tick))
--- end)
-
--- script.on_event("csrsbsy-zoom-out", function(event)
---   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
---   game.print(string.format("zoomed out to %f @ %d", player.zoom, event.tick))
+-- script.on_event(defines.events.on_tick, function(event)
+--   local player = game.get_player("Quezler") --[[@as LuaPlayer]]
 -- end)
