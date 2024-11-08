dc06cb3936caa749e05aa2becff1e0801816080f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 15:29:49 +0100

    Try to follow the zoom level

--- mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
+++ mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
@@ -10,7 +10,8 @@ script.on_nth_tick(60, function(event)
   local textfield = player.gui.center[mod_prefix .. 'textfield']
   if textfield == nil then
     textfield = player.gui.center.add{
-      type = 'empty-widget',
+      -- type = 'empty-widget',
+      type = 'frame',
       name = mod_prefix .. 'textfield',
     }
   else
@@ -28,4 +29,12 @@ end)
 
 script.on_event(defines.events.on_gui_leave, function(event)
   game.print("on_gui_leave")
+end)
+
+script.on_event("csrsbsy-zoom-in", function(event)
+  game.print(string.format("zoomed in @ %d", event.tick))
+end)
+
+script.on_event("csrsbsy-zoom-out", function(event)
+  game.print(string.format("zoomed out @ %d", event.tick))
 end)
---
+++ mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/data.lua
@@ -0,0 +1,17 @@
+data:extend{{
+  type = "custom-input",
+  name = "csrsbsy-zoom-in",
+
+  linked_game_control = "zoom-in",
+  key_sequence = "mouse-wheel-up",
+  action = "lua",
+}}
+
+data:extend{{
+  type = "custom-input",
+  name = "csrsbsy-zoom-out",
+
+  linked_game_control = "zoom-out",
+  key_sequence = "mouse-wheel-down",
+  action = "lua",
+}}
