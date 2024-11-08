bca4b3e12ba35ea189c9f803f16c23062da335d5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 15:50:31 +0100

    Stash research

--- mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
+++ mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/control.lua
@@ -21,7 +21,12 @@ script.on_nth_tick(60, function(event)
   textfield.style.height = 200
   textfield.style.bottom_margin = 150
   textfield.raise_hover_events = true
+  textfield.game_controller_interaction = defines.game_controller_interaction.never
+  textfield.ignored_by_interaction = true
 end)
+
+-- running this does cause it to fire the event, neat
+-- /c game.player.gui.center["csrsbsy-textfield"].ignored_by_interaction = false
 
 script.on_event(defines.events.on_gui_hover, function(event)
   game.print("on_gui_hover")
