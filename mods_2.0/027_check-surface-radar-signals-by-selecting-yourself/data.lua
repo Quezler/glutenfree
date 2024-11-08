f7f9d47cf4becb743e405c2c06a0e4154e986b62
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-08 20:35:22 +0100

    Make the pole a higher selection priority to avoid losing it when flying about

--- mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/data.lua
+++ mods_2.0/027_check-surface-radar-signals-by-selecting-yourself/data.lua
@@ -6,7 +6,7 @@ proxy.name = mod_prefix .. proxy.name
 local character = data.raw["character"]["character"]
 proxy.selection_box = character.selection_box
 proxy.collision_box = character.collision_box
-proxy.selection_priority = (character.selection_priority or 50) - 2
+proxy.selection_priority = (character.selection_priority or 50) - 1
 proxy.minable.mining_time = 1000000
 
 data:extend{proxy}
@@ -23,7 +23,7 @@ data:extend{{
     {character.selection_box[2][1] + 0.5, character.selection_box[2][2] + 0.5},
   },
   collision_box = character.collision_box,
-  selection_priority = (character.selection_priority or 50) - 1,
+  selection_priority = (character.selection_priority or 50) + 1,
 
   flags = {"placeable-off-grid", "not-on-map"},
   collision_mask = {layers = {}},
