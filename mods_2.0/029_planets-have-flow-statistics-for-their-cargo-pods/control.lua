e777cdc3a96069d8309042444098fd0e166141a1
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-11 16:50:00 +0100

    Refractor

--- mods_2.0/029_planets-have-flow-statistics-for-their-cargo-pods/control.lua
+++ mods_2.0/029_planets-have-flow-statistics-for-their-cargo-pods/control.lua
@@ -14,26 +14,25 @@ script.on_init(function()
 
   storage.deathrattles = {}
 
-  -- for _, surface in pairs(game.surfaces) do
-  --   if get_flow_surface_name[surface.name] then
-  --     for _, entity in pairs(surface.find_entities_filtered({name = {"rocket-silo-rocket"}})) do
-  --       Handler.handle_launched_rocket(entity)
-  --     end
-  --     -- for _, entity in pairs(surface.find_entities_filtered({name = {"cargo-pod"}})) do
-  --     --   Handler.handle_launched_rocket(entity)
-  --     -- end
-  --   end
-  -- end
-
-  Handler.adopt_launched_rockets()
+  for _, surface in pairs(game.surfaces) do
+    if get_flow_surface_name[surface.name] then
+      for _, entity in pairs(surface.find_entities_filtered({name = {"rocket-silo-rocket"}})) do
+        Handler.handle_launched_rocket(entity)
+      end
+      -- the cargo-pod search will find rockets with cargo pods too, but since we did rockets first we can filter them out.
+      for _, entity in pairs(surface.find_entities_filtered({name = {"cargo-pod"}})) do
+        Handler.on_cargo_pod_created(entity, true) -- filtering them out by not assuming they're transitioning surface :)
+      end
+    end
+  end
 end)
 
 script.on_configuration_changed(function()
-  Handler.adopt_launched_rockets() -- todo: remove
+
 end)
 
 script.on_event(defines.events.on_tick, function(event)
-  for _, cargo_pod in ipairs(storage.new_cargo_pods or {}) do
+  for _, cargo_pod in ipairs(storage.new_cargo_pods) do
     Handler.on_cargo_pod_created(cargo_pod)
   end
   storage.new_cargo_pods = {}
@@ -52,8 +51,8 @@ script.on_event(defines.events.on_tick, function(event)
 end)
 
 local function get_flow_surface(planet_name)
-  -- todo: assert space location name is of type planet
   local flow_surface_name = get_flow_surface_name[planet_name]
+  assert(flow_surface_name, planet_name .. " is not a planet")
 
   local flow_surface = game.surfaces[flow_surface_name]
   if flow_surface == nil then
@@ -61,39 +60,37 @@ local function get_flow_surface(planet_name)
     flow_surface.localised_name = {"", string.format("[entity=cargo-pod] [planet=%s] ", planet_name), {"space-location-name." .. planet_name}}
   end
 
-  -- order
-
   return flow_surface
 end
 
-function Handler.on_cargo_pod_created(entity)
+function Handler.on_cargo_pod_created(entity, abort_if_duplicate)
   storage.cargo_pods = storage.cargo_pods or {} -- todo: remove
   local struct = storage.cargo_pods[entity.unit_number]
+  if struct and abort_if_duplicate then return end
   if struct then -- known cargo pod, just tansitioned to the other surface
+    game.print("cargo pod from platform transition to planet")
+
+    local flow_surface = get_flow_surface(entity.surface.planet.name)
+    local statistics = entity.force.get_item_production_statistics(flow_surface)
+    for _, item in ipairs(storage.cargo_pods[entity.unit_number].items) do
+      statistics.on_flow({name = item.name, quality = item.quality}, item.count)
+    end
+
+    storage.cargo_pods[entity.unit_number] = nil
+    return
   end
 
   local platform = entity.surface.platform
   if platform then
     local inventory = entity.get_inventory(defines.inventory.cargo_unit)
-    game.print(platform.space_location.name)
+    game.print("cargo pod launched from platform")
 
     storage.cargo_pods[entity.unit_number] = {
       entity = entity,
       items = inventory.get_contents(),
     }
   else
-    if storage.cargo_pods[entity.unit_number] then
-      game.print("cargo pod from platform transition to planet")
-
-      local flow_surface = get_flow_surface(entity.surface.planet.name)
-      local statistics = entity.force.get_item_production_statistics(flow_surface)
-      for _, item in ipairs(storage.cargo_pods[entity.unit_number].items) do
-        statistics.on_flow({name = item.name, quality = item.quality}, item.count)
-      end
-      game.players["Quezler"].opened = statistics
-
-      storage.cargo_pods[entity.unit_number] = nil
-    end
+    -- cargo pods likely created underground in a silo
   end
   -- game.print(event.entity.unit_number .. event.entity.surface.name .. event.entity.procession_tick)
 
@@ -112,6 +109,7 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
   assert(event.target_entity.name == "cargo-pod")
   assert(event.target_entity.type == "cargo-pod")
 
+  game.print(string.format("new cargo pod: %d @ %s", event.target_entity.unit_number, event.target_entity.surface.name))
   table.insert(storage.new_cargo_pods, event.target_entity)
 end)
 
@@ -123,19 +121,4 @@ end
 
 script.on_event(defines.events.on_rocket_launch_ordered, function(event)
   Handler.handle_launched_rocket(event.rocket)
-end)
-
--- once during on_init, to catch rockets already in flight
-function Handler.adopt_launched_rockets()
-  for _, surface in pairs(game.surfaces) do
-    if get_flow_surface_name[surface.name] then
-      for _, entity in pairs(surface.find_entities_filtered({name = {"rocket-silo-rocket"}})) do
-        Handler.handle_launched_rocket(entity)
-      end
-    end
-  end
-end
-
-script.on_event("cargo-pod-production", function(event)
-  
 end)
