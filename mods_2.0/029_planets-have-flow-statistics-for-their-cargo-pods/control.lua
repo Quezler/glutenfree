260f93dbf6b9daf94f8a4487f7fbe119a32bdb06
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-11 14:53:40 +0100

    Check that cargo pods have items in them during their next tick

--- mods_2.0/029_planets-have-flow-statistics-for-their-cargo-pods/control.lua
+++ mods_2.0/029_planets-have-flow-statistics-for-their-cargo-pods/control.lua
@@ -3,6 +3,13 @@ script.on_init(function()
   storage.cargo_pods = {}
 end)
 
+script.on_event(defines.events.on_tick, function(event)
+  if storage.inventory then
+    game.print(serpent.line(storage.inventory.get_contents()))
+    storage.inventory = nil
+  end
+end)
+
 local function on_cargo_pod_created(event)
   game.print(event.entity.unit_number .. event.entity.surface.name .. event.entity.procession_tick)
 
@@ -12,6 +19,8 @@ local function on_cargo_pod_created(event)
   if inventory.is_empty() == false then -- in their first tick their inventory is empty, unless they transition surface
     game.print("cargo pod transitioned to destination surface")
   end
+
+  storage.inventory = event.entity.get_inventory(defines.inventory.cargo_unit)
 end
 
 script.on_event(defines.events.on_script_trigger_effect, function(event)
