01740902aff984d6b5eb3f1b643b636dfd6188a2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-12 18:41:58 +0100

    Start walking to the front of the belt

--- mods_2.0/030_read-belt-contents-hold-all-belts-read-belt-count/scripts/combinator.lua
+++ mods_2.0/030_read-belt-contents-hold-all-belts-read-belt-count/scripts/combinator.lua
@@ -1,8 +1,34 @@
+-- local function trace_belt(belt)
+--   game.print(serpent.line( belt.belt_neighbours ))
+
+--   local in_front = belt.belt_neighbours.output[1]
+--   if in_front and in_front.type == "transport-belt" then
+--   end
+-- end
+
+local function get_next_belt(belt)
+  rendering.draw_circle{
+    surface = belt.surface,
+    target = belt.position,
+    radius = 0.1,
+    color = {1, 1, 1, 1},
+    filled = true,
+    time_to_live = 30,
+  }
+
+  local in_front = belt.belt_neighbours.outputs[1]
+  if in_front and in_front.type == "transport-belt" then
+    get_next_belt(in_front)
+  end
+end
+
 script.on_nth_tick(60, function(event)
   for struct_id, struct in pairs(storage.structs) do
     if is_belt_read_holding_all_belts(struct.belt) == false then
       game.print("nth 60 delete")
       delete_struct(struct)
+    else
+      get_next_belt(struct.belt)
     end
   end
 end)
