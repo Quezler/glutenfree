9aca52ace17bb130d78b1c520cc80cc5ad9ed9a5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-12 11:05:17 +0100

    Add circuit selector with caption

--- mods_2.0/030_read-belt-contents-hold-all-belts-read-belt-count/control.lua
+++ mods_2.0/030_read-belt-contents-hold-all-belts-read-belt-count/control.lua
@@ -5,10 +5,10 @@ script.on_event(defines.events.on_gui_opened, function(event)
   if entity and entity.type == "transport-belt" then
     local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
 
-    local frame = player.gui.relative["frame_name"]
+    local frame = player.gui.relative[frame_name]
     if frame then frame.destroy() end
 
-    player.gui.relative.add{
+    frame = player.gui.relative.add{
       type = "frame",
       name = frame_name,
       caption = { "gui-control-behavior-modes.read-belt-count" },
@@ -16,6 +16,29 @@ script.on_event(defines.events.on_gui_opened, function(event)
         gui = defines.relative_gui_type.transport_belt_gui,
         position = defines.relative_gui_position.right,
       },
+    }
+
+    local inner = frame.add{
+      type = "frame",
+      style = "inside_shallow_frame_with_padding",
+    }
+
+    -- inner.style.vertical_align = "center"
+
+    local flow = inner.add{
+      type = "flow",
+      style = "player_input_horizontal_flow"
+    }
+
+    flow.add{
+      type = "label",
+      caption = {"gui-control-behavior-modes-guis.control-signal"}
+    }
+
+    flow.add{
+      type = "choose-elem-button",
+      elem_type = "signal",
+      signal = {type = "virtual", name = "signal-B"},
     }
   end
 end)
