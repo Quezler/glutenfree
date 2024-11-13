da61b2971289d58583552f7de15c6ac670f5e420
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-13 13:35:21 +0100

    Draft up code to support being conditionally selected by players

--- mods_2.0/031_display-panel-can-display-circuit-amount-as-text/control.lua
+++ mods_2.0/031_display-panel-can-display-circuit-amount-as-text/control.lua
@@ -11,13 +11,18 @@ function Handler.on_init(event)
       Handler.on_created_entity({entity = entity})
     end
   end
+
+  storage.selected_struct_ids = {}
 end
 
 function Handler.on_created_entity(event)
   local entity = event.entity or event.destination
 
   storage.structs[entity.unit_number] = {
+    id = entity.unit_number,
     entity = entity,
+
+    selected_by = nil,
   }
 end
 
@@ -25,10 +30,8 @@ local function is_nil_or_number(string)
   return string == nil or tonumber(string, 10) ~= nil
 end
 
-local function tick_display_panel(entity)
-  local struct = storage.structs[entity.unit_number]
-  assert(struct)
-
+local function tick_display_panel(struct)
+  local entity = struct.entity
   game.print(string.format("@%d ticked display panel #%d", game.tick, entity.unit_number))
 
   local cb = entity.get_control_behavior()
@@ -61,18 +64,23 @@ for _, event in ipairs({
   })
 end
 
+local function mark_struct_as_selected(struct, player)
+  storage.selected_struct_ids[struct.id] = true
+  struct.selected_by = player
+end
+
 script.on_event(defines.events.on_selected_entity_changed, function(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
   local entity = player.selected
   if entity and entity.type == "display-panel" then
-    tick_display_panel(entity)
+    tick_display_panel(storage.structs[entity.unit_number])
   end
 end)
 
 script.on_event(defines.events.on_tick, function(event)
   for _, struct in pairs(storage.structs) do
     if struct.entity.valid then
-      tick_display_panel(struct.entity)
+      tick_display_panel(struct)
     end
   end
 end)
