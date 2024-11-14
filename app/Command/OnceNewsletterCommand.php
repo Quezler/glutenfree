de6a72e7f7d33fd761bc8e2a6f8f75473d416557
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-14 22:14:53 +0100

    Require the presence of newsletter-for-mods-made-by-quezler

--- app/Command/OnceNewsletterCommand.php
+++ app/Command/OnceNewsletterCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Misc\ExpansionMods;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,9 +24,45 @@ class OnceNewsletterCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         foreach (ExpansionMods::list() as $mod) {
-            $info_json = file_get_contents($mod->get_info_json_pathname());
-            $info_json = str_replace('    "dependencies": [', "    \"dependencies\": [\n        \"~ newsletter-for-mods-made-by-quezler\",\n", $info_json);
-            file_put_contents($mod->get_info_json_pathname(), $info_json);
+//            // stage 1
+//            $info_json = file_get_contents($mod->get_info_json_pathname());
+//            $info_json = str_replace('    "dependencies": [', "    \"dependencies\": [\n        \"~ newsletter-for-mods-made-by-quezler\",\n", $info_json);
+//            file_put_contents($mod->get_info_json_pathname(), $info_json);
+
+//            // stage 2
+//            $changelog = file_get_contents($mod->get_changelog_txt_pathname());
+//            $lines = explode(PHP_EOL, $changelog);
+//            $found = false;
+//            foreach ($lines as $i => $line) {
+//                if ($line == "    - Require the presence of newsletter-for-mods-made-by-quezler") {
+//                    if ($found == false) {
+//                        $found = true;
+//                    } else {
+//                        unset($lines[$i]);
+//                    }
+//                }
+//            }
+//            file_put_contents($mod->get_changelog_txt_pathname(), implode(PHP_EOL, $lines));
+
+//            // stage 3
+//            $changelog = file_get_contents($mod->get_changelog_txt_pathname());
+//            $lines = explode(PHP_EOL, $changelog);
+//            $lines[2] = 'Date: 2024. 11. 14';
+//            file_put_contents($mod->get_changelog_txt_pathname(), implode(PHP_EOL, $lines));
+
+//            // stage 4
+//            $mod->setInfoJsonVersion($mod->getLastVersionFromChangelog());
+
+//            // stage 5
+//            if ($mod->name != "newsletter-for-mods-made-by-quezler") {
+//                $command = $this->getApplication()->find('build:mod');
+//
+//                try {
+//                    $command->run(new ArrayInput(['name' => $mod->name, '--update' => true]), $output);
+//                } catch (\Exception $e) {
+//                    dump($e->getMessage());
+//                }
+//            }
         }
 
         return Command::SUCCESS;
