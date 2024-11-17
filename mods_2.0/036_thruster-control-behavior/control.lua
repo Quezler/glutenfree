ee11abab42d6f5cd6a085925053ff804b0ff81ab
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-17 12:00:56 +0100

    Just listen to all the events in case of editor/character building

--- mods_2.0/036_thruster-control-behavior/control.lua
+++ mods_2.0/036_thruster-control-behavior/control.lua
@@ -25,11 +25,11 @@ function Handler.on_created_entity(event)
 end
 
 for _, event in ipairs({
-  -- defines.events.on_built_entity,
-  -- defines.events.on_robot_built_entity,
+  defines.events.on_built_entity,
+  defines.events.on_robot_built_entity,
   defines.events.on_space_platform_built_entity,
-  -- defines.events.script_raised_built,
-  -- defines.events.script_raised_revive,
+  defines.events.script_raised_built,
+  defines.events.script_raised_revive,
   defines.events.on_entity_cloned,
 }) do
   script.on_event(event, Handler.on_created_entity, {
