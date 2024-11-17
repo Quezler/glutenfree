cd805228e7e8c4c48fb55444da6439a51143ac8b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-17 14:50:08 +0100

    Set the thruster status based on the power switch state

--- mods_2.0/036_thruster-control-behavior/control.lua
+++ mods_2.0/036_thruster-control-behavior/control.lua
@@ -17,8 +17,23 @@ local function create_struct()
 end
 
 local function struct_set_thruster(struct, thruster)
+  assert(thruster)
+  assert(thruster.valid)
+
   struct.thruster = thruster
+
   storage.deathrattles[script.register_on_object_destroyed(thruster)] = {type = "thruster", struct_id = struct.id}
+  storage.unit_number_to_struct_id[thruster.unit_number] = struct.id
+end
+
+local function struct_set_power_switch(struct, power_switch)
+  assert(power_switch)
+  assert(power_switch.valid)
+
+  struct.power_switch = power_switch
+
+  storage.deathrattles[script.register_on_object_destroyed(power_switch)] = {type = "power-switch", struct_id = struct.id}
+  storage.unit_number_to_struct_id[power_switch.unit_number] = struct.id
 end
 
 local function get_control_behavior_position(thruster)
@@ -30,7 +45,7 @@ function Handler.on_init()
   storage.structs = {}
   storage.deathrattles = {}
 
-  -- storage.unit_number_to_struct_id = {}
+  storage.unit_number_to_struct_id = {}
 
   for _, surface in pairs(game.surfaces) do
     for _, entity in pairs(surface.find_entities_filtered{name = "thruster"}) do
@@ -68,8 +83,9 @@ local function on_created_thruster(entity)
   struct.surface = entity.surface
   struct.position = entity.position
   struct_set_thruster(struct, entity)
+  struct_set_power_switch(struct, power_switch)
 
-  struct.power_switch = power_switch
+  Handler.on_power_switch_touched(power_switch)
 end
 
 local function on_created_thruster_control_behavior(entity)
@@ -113,6 +129,8 @@ script.on_event(defines.events.on_object_destroyed, function(event)
     local struct = assert(storage.structs[deathrattle.struct_id])
     assert(struct.power_switch.valid)
 
+    storage.unit_number_to_struct_id[event.useful_id] = nil
+
     if deathrattle.type == "thruster" then
       local new_thruster = surface_find_entity_or_ghost(struct.surface, struct.position, "thruster")
       if new_thruster then
@@ -121,12 +139,12 @@ script.on_event(defines.events.on_object_destroyed, function(event)
         if new_thruster.type == "entity-ghost" and struct.power_switch.type ~= "entity-ghost" then
           struct.power_switch.destructible = true
           assert(struct.power_switch.die(struct.power_switch.force))
-          struct.power_switch = surface_find_entity_or_ghost(struct.surface, get_control_behavior_position(new_thruster), "thruster-control-behavior")
+          struct_set_power_switch(struct, surface_find_entity_or_ghost(struct.surface, get_control_behavior_position(new_thruster), "thruster-control-behavior"))
           struct.power_switch.minable = false
         elseif new_thruster.type ~= "entity-ghost" and struct.power_switch.type == "entity-ghost" then
           struct.power_switch.destructible = true
           local _, new_power_switch = struct.power_switch.revive{}
-          struct.power_switch = assert(new_power_switch)
+          struct_set_power_switch(struct, new_power_switch)
           struct.power_switch.destructible = false
         end
       else
@@ -135,5 +153,52 @@ script.on_event(defines.events.on_object_destroyed, function(event)
       end
     end
 
+  end
+end)
+
+-- we trigger this event when a power switch has likely received new settings,
+-- it is no guarantee since blueprints can also configure them,
+-- and we also won't listen to open gui's every tick.
+function Handler.on_power_switch_touched(entity)
+  local struct_id = storage.unit_number_to_struct_id[entity.unit_number]
+  assert(struct_id)
+  local struct = storage.structs[struct_id]
+  assert(struct)
+
+  if entity.power_switch_state == true then
+    struct.thruster.active = true
+    struct.thruster.custom_status = nil
+  else
+    struct.thruster.active = false
+    struct.thruster.custom_status = {
+      diode = defines.entity_status_diode.red,
+      label = {"entity-status.disabled-by-control-behavior"},
+    }
+  end
+end
+
+script.on_event(defines.events.on_selected_entity_changed, function(event)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+  local entity = player.selected
+
+  if entity and (entity.type == "entity-ghost" and entity.ghost_name or entity.name) == "thruster-control-behavior" then
+    Handler.on_power_switch_touched(entity)
+  end
+end)
+
+script.on_event(defines.events.on_gui_closed, function(event)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+  local entity = event.entity
+
+  if entity and (entity.type == "entity-ghost" and entity.ghost_name or entity.name) == "thruster-control-behavior" then
+    Handler.on_power_switch_touched(entity)
+  end
+end)
+
+script.on_event(defines.events.on_entity_settings_pasted, function(event)
+  local entity = event.destination
+
+  if entity and (entity.type == "entity-ghost" and entity.ghost_name or entity.name) == "thruster-control-behavior" then
+    Handler.on_power_switch_touched(entity)
   end
 end)
