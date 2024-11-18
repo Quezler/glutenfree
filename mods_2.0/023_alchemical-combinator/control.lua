43e42b4395aba0be87aa4ec163401ce8f3b39311
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-18 21:20:40 +0100

    Fix a similar issue to the last one related to the everything signal

--- mods_2.0/023_alchemical-combinator/control.lua
+++ mods_2.0/023_alchemical-combinator/control.lua
@@ -306,6 +306,7 @@ function Handler.tick_active_struct(struct)
   else
     local signals = struct.alchemical_combinator.get_signals(defines.wire_connector_id.combinator_output_red, defines.wire_connector_id.combinator_output_green)
     local parameters = struct.alchemical_combinator.get_control_behavior().parameters
+    parameters.conditions = parameters.conditions or {}
 
     -- trick the -active combinator into enabeling its outside side
     table.insert(parameters.conditions, {
