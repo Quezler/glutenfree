f5312a3676758a917eb4e2cb044d9f45902cf774
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-24 20:40:33 +0100

    Add uninstall option

--- mods_2.0/026_no-minimap-on-platforms/control.lua
+++ mods_2.0/026_no-minimap-on-platforms/control.lua
@@ -1,4 +1,19 @@
+local setting_name_uninstalled = "no-minimap-on-platforms--uninstalled"
+
 script.on_event(defines.events.on_player_changed_surface, function(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+  if settings.global[setting_name_uninstalled].value == true then return end
   player.game_view_settings.show_minimap = player.surface.platform == nil
+end)
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+  if event.setting_type == "runtime-global" then
+    if event.setting == setting_name_uninstalled then
+      if settings.global[setting_name_uninstalled].value then
+        for _, player in pairs(game.players) do
+          player.game_view_settings.show_minimap = true
+        end
+      end
+    end
+  end
 end)
---
+++ mods_2.0/026_no-minimap-on-platforms/settings.lua
@@ -0,0 +1,6 @@
+data:extend{{
+  type = "bool-setting",
+  name = "no-minimap-on-platforms--uninstalled",
+  setting_type = "runtime-global",
+  default_value = false,
+}}
