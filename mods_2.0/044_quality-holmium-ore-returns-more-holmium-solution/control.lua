c4f6ea36485d638023920a529acbea1372dd0347
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-28 17:20:05 +0100

    Begin conversion to structs and deathrattles

--- mods_2.0/044_quality-holmium-ore-returns-more-holmium-solution/control.lua
+++ mods_2.0/044_quality-holmium-ore-returns-more-holmium-solution/control.lua
@@ -12,10 +12,19 @@ end)
 local mod_surface_name = "holmium-chemical-plant"
 local coin_item_name = "coupon-for-holmium-solution"
 
+local function new_struct(table, struct)
+  assert(struct.id, serpent.block(struct))
+  assert(table[struct.id] == nil)
+  table[struct.id] = struct
+  return struct
+end
+
 local Handler = {}
 
 script.on_init(function()
   storage.x_offset = 0
+  storage.structs = {}
+  storage.deathrattles = {}
 
   local mod_surface = game.planets[mod_surface_name].create_surface()
   mod_surface.generate_with_lab_tiles = true
@@ -58,49 +67,63 @@ end
 function Handler.on_created_entity(event)
   local entity = event.entity or event.destination
 
-  local linked_chest_a = entity.surface.create_entity{
+  local struct = new_struct(storage.structs, {
+    id = entity.unit_number,
+
+    linked_chest_a = nil,
+    linked_chest_b = nil,
+    inserter_1 = nil,
+    infinity_chest_1 = nil,
+    inserter_2 = nil,
+    linked_chest_c = nil,
+    arithmetic_1 = nil,
+    arithmetic_2 = nil,
+    arithmetic_3 = nil,
+  })
+
+  struct.linked_chest_a = entity.surface.create_entity{
     name = "holmium-chemical-plant-linked-chest",
     force = "neutral",
     position = util.moveposition({entity.position.x, entity.position.y}, entity.direction, -1),
   }
-  linked_chest_a.destructible = false
-  linked_chest_a.link_id = storage.x_offset
-  entity.drop_target = linked_chest_a
+  struct.linked_chest_a.destructible = false
+  struct.linked_chest_a.link_id = storage.x_offset
+  entity.drop_target = struct.linked_chest_a
 
-  local linked_chest_b = game.surfaces[mod_surface_name].create_entity{
+  struct.linked_chest_b = game.surfaces[mod_surface_name].create_entity{
     name = "holmium-chemical-plant-linked-chest",
     force = "neutral",
     position = {0.5 + storage.x_offset, -0.5},
   }
-  linked_chest_b.link_id = storage.x_offset
+  struct.linked_chest_b.link_id = storage.x_offset
 
-  local inserter_1 = game.surfaces[mod_surface_name].create_entity{
+  struct.inserter_1 = game.surfaces[mod_surface_name].create_entity{
     name = "fast-inserter",
     force = "neutral",
     position = {0.5 + storage.x_offset, -1.5},
     direction = defines.direction.south,
   }
-  assert(inserter_1)
-  inserter_1.use_filters = true
-  inserter_1.inserter_filter_mode = "blacklist"
-  inserter_1.set_filter(1, {
+  assert(struct.inserter_1)
+  struct.inserter_1.use_filters = true
+  struct.inserter_1.inserter_filter_mode = "blacklist"
+  struct.inserter_1.set_filter(1, {
     comparator = "=",
     name = coin_item_name,
     quality = "normal"
   })
-  inserter_1.pickup_target = linked_chest_b
+  struct.inserter_1.pickup_target = struct.linked_chest_b
 
-  local inserter_1_cb = inserter_1.get_or_create_control_behavior() --[[@as LuaInserterControlBehavior]]
-  inserter_1_cb.circuit_read_hand_contents = true
-  inserter_1_cb.circuit_hand_read_mode = defines.control_behavior.inserter.hand_read_mode.pulse
+  struct.inserter_1_cb = struct.inserter_1.get_or_create_control_behavior() --[[@as LuaInserterControlBehavior]]
+  struct.inserter_1_cb.circuit_read_hand_contents = true
+  struct.inserter_1_cb.circuit_hand_read_mode = defines.control_behavior.inserter.hand_read_mode.pulse
 
-  local infinity_chest_1 = game.surfaces[mod_surface_name].create_entity{
+  struct.infinity_chest_1 = game.surfaces[mod_surface_name].create_entity{
     name = "infinity-chest",
     force = "neutral",
     position = {0.5 + storage.x_offset, -2.5},
   }
-  infinity_chest_1.remove_unfiltered_items = true
-  infinity_chest_1.infinity_container_filters = {
+  struct.infinity_chest_1.remove_unfiltered_items = true
+  struct.infinity_chest_1.infinity_container_filters = {
     {
       count = 1,
       index = 1,
@@ -109,44 +132,43 @@ function Handler.on_created_entity(event)
     }
   }
 
-  local inserter_2 = game.surfaces[mod_surface_name].create_entity{
+  struct.inserter_2 = game.surfaces[mod_surface_name].create_entity{
     name = "fast-inserter",
     force = "neutral",
     position = {0.5 + storage.x_offset, -3.5},
     direction = defines.direction.south,
   }
-  assert(inserter_2)
-  local inserter_2_cb = inserter_2.get_or_create_control_behavior() --[[@as LuaInserterControlBehavior]]
+  assert(struct.inserter_2)
+  inserter_2_cb = struct.inserter_2.get_or_create_control_behavior() --[[@as LuaInserterControlBehavior]]
   inserter_2_cb.circuit_read_hand_contents = true
   inserter_2_cb.circuit_hand_read_mode = defines.control_behavior.inserter.hand_read_mode.pulse
   inserter_2_cb.circuit_enable_disable = true
   inserter_2_cb.circuit_condition = {
-    -- condition = {
-      comparator = "<",
-      constant = 0,
-      first_signal = {
-        name = coin_item_name
-      }
-    -- }
+    comparator = "<",
+    constant = 0,
+    first_signal = {
+      name = coin_item_name
+    }
   }
 
-  local linked_chest_c = game.surfaces[mod_surface_name].create_entity{
+  struct.linked_chest_c = game.surfaces[mod_surface_name].create_entity{
     name = "holmium-chemical-plant-linked-chest",
     force = "neutral",
     position = {0.5 + storage.x_offset, -4.5},
   }
-  linked_chest_c.link_id = storage.x_offset
-  inserter_2.drop_target = linked_chest_c
+  struct.linked_chest_c.link_id = storage.x_offset
+
+  struct.inserter_2.drop_target = struct.linked_chest_c
 
   -- takes in the quality signal and determines the multiplier amount
-  local arithmetic_1 = game.surfaces[mod_surface_name].create_entity{
+  struct.arithmetic_1 = game.surfaces[mod_surface_name].create_entity{
     name = "arithmetic-combinator",
     force = "neutral",
     position = {0.5 + storage.x_offset, -6.0},
     direction = defines.direction.north,
   }
-  assert(arithmetic_1)
-  arithmetic_1_cb = arithmetic_1.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
+  assert(struct.arithmetic_1)
+  arithmetic_1_cb = struct.arithmetic_1.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
   arithmetic_1_cb.parameters = {
     first_signal = {
       name = "signal-each",
@@ -170,21 +192,21 @@ function Handler.on_created_entity(event)
     }
   }
 
-  local green_in = arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_input_green, false)
-  local red_in = arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_input_red, false)
+  local green_in = struct.arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_input_green, false)
+  local red_in = struct.arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_input_red, false)
 
   assert(green_in.connect_to(storage.constant_combinator.get_wire_connector(defines.wire_connector_id.circuit_green, false), false))
-  assert(red_in.connect_to(inserter_1.get_wire_connector(defines.wire_connector_id.circuit_red, false), false))
+  assert(red_in.connect_to(struct.inserter_1.get_wire_connector(defines.wire_connector_id.circuit_red, false), false))
 
   -- turns the multiplier signal negative, so the inserter's posive pulse can subtract from it
-  local arithmetic_2 = game.surfaces[mod_surface_name].create_entity{
+  struct.arithmetic_2 = game.surfaces[mod_surface_name].create_entity{
     name = "arithmetic-combinator",
     force = "neutral",
     position = {0.5 + storage.x_offset, -8.0},
     direction = defines.direction.north,
   }
-  assert(arithmetic_2)
-  arithmetic_2_cb = arithmetic_2.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
+  assert(struct.arithmetic_2)
+  arithmetic_2_cb = struct.arithmetic_2.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
   arithmetic_2_cb.parameters = {
     first_signal = {
       name = coin_item_name
@@ -204,18 +226,18 @@ function Handler.on_created_entity(event)
     }
   }
 
-  local arithmetic_2_red_in = arithmetic_2.get_wire_connector(defines.wire_connector_id.combinator_input_red, false)
-  assert(arithmetic_2_red_in.connect_to(arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)))
+  struct.arithmetic_2_red_in = struct.arithmetic_2.get_wire_connector(defines.wire_connector_id.combinator_input_red, false)
+  assert(struct.arithmetic_2_red_in.connect_to(struct.arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)))
 
   -- memory cell
-  local arithmetic_3 = game.surfaces[mod_surface_name].create_entity{
+  struct.arithmetic_3 = game.surfaces[mod_surface_name].create_entity{
     name = "arithmetic-combinator",
     force = "neutral",
     position = {0.5 + storage.x_offset, -10.0},
     direction = defines.direction.north,
   }
-  assert(arithmetic_3)
-  arithmetic_3_cb = arithmetic_3.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
+  assert(struct.arithmetic_3)
+  arithmetic_3_cb = struct.arithmetic_3.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
   arithmetic_3_cb.parameters = {
     first_signal = {
       name = coin_item_name
@@ -235,12 +257,12 @@ function Handler.on_created_entity(event)
     }
   }
 
-  local arithmetic_3_red_in = arithmetic_3.get_wire_connector(defines.wire_connector_id.combinator_input_red, false)
-  assert(arithmetic_3_red_in.connect_to(arithmetic_2.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)))
-  assert(arithmetic_3_red_in.connect_to(arithmetic_3.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)))
+  local arithmetic_3_red_in = struct.arithmetic_3.get_wire_connector(defines.wire_connector_id.combinator_input_red, false)
+  assert(arithmetic_3_red_in.connect_to(struct.arithmetic_2.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)))
+  assert(arithmetic_3_red_in.connect_to(struct.arithmetic_3.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)))
 
-  local arithmetic_3_red_out = arithmetic_3.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)
-  assert(arithmetic_3_red_out.connect_to(inserter_2.get_wire_connector(defines.wire_connector_id.circuit_red, false)))
+  local arithmetic_3_red_out = struct.arithmetic_3.get_wire_connector(defines.wire_connector_id.combinator_output_red, false)
+  assert(arithmetic_3_red_out.connect_to(struct.inserter_2.get_wire_connector(defines.wire_connector_id.circuit_red, false)))
 
   storage.x_offset = storage.x_offset + 1
 end
