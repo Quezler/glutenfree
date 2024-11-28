a18ba358bdb3074725352e0795bd4526c40b9b2b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-28 19:23:25 +0100

    Setup the redeeming prototypes

--- mods_2.0/044_quality-holmium-ore-returns-more-holmium-solution/data-final-fixes.lua
+++ mods_2.0/044_quality-holmium-ore-returns-more-holmium-solution/data-final-fixes.lua
@@ -7,3 +7,49 @@ for _, quality in pairs(data.raw["quality"]) do
   end
 end
 data.raw["item"]["holmium-solution-quality-based-productivity"].localised_description = {"", "[font=default-bold]", table.concat(lines, "\n"), "[/font]"}
+
+local crafting_category = {
+  type = "recipe-category",
+  name = "holmium-chemical-plant-assembling-machine"
+}
+
+local assembler = {
+  type = "assembling-machine",
+  name = "holmium-chemical-plant-assembling-machine",
+  collision_mask = {layers = {}},
+  collision_box = {{-0.25, -0.25}, {0.25, 0.25}},
+  selection_box = {{-0.25, -0.25}, {0.25, 0.25}},
+  fluid_boxes =
+  {
+    {
+      production_type = "output",
+      volume = 1,
+      pipe_connections = {{ flow_direction="output", direction = defines.direction.south, position = {0, 0} }},
+    }
+  },
+  energy_usage = "1W",
+  energy_source = {type = "void"},
+  crafting_speed = 10,
+  hidden = true,
+  selection_priority = 51,
+  -- selectable_in_game = false,
+  crafting_categories = {crafting_category.name},
+  flags = {"not-on-map", "hide-alt-info"},
+}
+
+local recipe = table.deepcopy(data.raw["recipe"]["holmium-solution"])
+recipe.name = "holmium-chemical-plant-assembling-machine"
+recipe.ingredients = {{type = "item", name = "coupon-for-holmium-solution", amount = 1, ignored_by_stats = 1}}
+recipe.hide_from_player_crafting = true
+recipe.hide_from_signal_gui = true
+recipe.category = crafting_category.name
+recipe.enabled = true
+assert(#recipe.results == 1)
+
+assembler.fixed_recipe = recipe.name
+assembler.fixed_quality = "normal"
+
+table.insert(assembler.flags, "no-automated-item-removal")
+table.insert(assembler.flags, "no-automated-item-insertion")
+
+data:extend{crafting_category, assembler, recipe}
