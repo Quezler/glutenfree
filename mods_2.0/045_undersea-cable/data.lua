10ca0f91c6869e8f1182585de0f8515b80530e2e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-29 16:01:01 +0100

    Fork the offshore pump

--- mods_2.0/045_undersea-cable/data.lua
+++ mods_2.0/045_undersea-cable/data.lua
@@ -1,12 +1,12 @@
 local entity = table.deepcopy(data.raw["heat-pipe"]["heat-pipe"])
 entity.name = "undersea-cable"
 entity.icon = "__undersea-cable__/graphics/icons/undersea-cable.png"
-entity.collision_mask = {layers = {}}
+entity.collision_mask = {layers={ground_tile=true}}
 entity.heat_buffer.connections = nil
 
 for _, connection_sprite in pairs(entity.connection_sprites) do
   for j, sheet in pairs(connection_sprite) do
-    assert(tonumber(j), serpent.block(connection_sprite)) -- sheet array and not just one
+    assert(tonumber(j), serpent.block(connection_sprite)) -- ensure numeric array
     sheet.tint = {0.25, 0.25, 0.25, 0.25}
   end
 end
@@ -17,3 +17,7 @@ table.insert(data.raw["planet"]["fulgora"].lightning_properties.exemption_rules,
   type = "id",
   string = entity.name,
 })
+
+local offshore_pump = table.deepcopy(data.raw["offshore-pump"]["offshore-pump"])
+offshore_pump.name = "undersea-cable-landing-point"
+data:extend{offshore_pump}
