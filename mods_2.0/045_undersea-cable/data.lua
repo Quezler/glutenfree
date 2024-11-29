e15f6a42fe472a740ebe3bed0241787991d14c31
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-29 14:21:17 +0100

    Initial commit

---
+++ mods_2.0/045_undersea-cable/data.lua
@@ -0,0 +1,18 @@
+local entity = table.deepcopy(data.raw["heat-pipe"]["heat-pipe"])
+entity.name = "undersea-cable"
+entity.collision_mask = {layers = {}}
+entity.heat_buffer.connections = nil
+
+for _, connection_sprite in pairs(entity.connection_sprites) do
+  for j, sheet in pairs(connection_sprite) do
+    assert(tonumber(j), serpent.block(connection_sprite)) -- sheet array and not just one
+    sheet.tint = {0.25, 0.25, 0.25, 0.25}
+  end
+end
+
+data:extend{entity}
+
+table.insert(data.raw["planet"]["fulgora"].lightning_properties.exemption_rules, {
+  type = "id",
+  string = entity.name,
+})
