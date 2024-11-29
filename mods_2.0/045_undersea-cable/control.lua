a1fea1b551991b0141eeb471d13c526f2c99d9b5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-29 17:35:09 +0100

    Get a positive pathfinder result

--- mods_2.0/045_undersea-cable/control.lua
+++ mods_2.0/045_undersea-cable/control.lua
@@ -1,3 +1,5 @@
+require("util")
+
 local Handler = {}
 
 script.on_init(function()
@@ -17,9 +19,9 @@ function Handler.on_created_entity(event)
 
   local uint = entity.surface.request_path{
     bounding_box = {{-0.25, -0.25}, {0.25, 0.25}},
-    collision_mask = {layers={ground_tile=true, is_lower_object = true}},
-    start = storage.side_a.position,
-    goal = storage.side_b.position,
+    collision_mask = {layers={ground_tile=true, is_lower_object=true}},
+    start = util.moveposition({storage.side_a.position.x, storage.side_a.position.y}, storage.side_a.direction, 2),
+    goal = util.moveposition({storage.side_b.position.x, storage.side_b.position.y}, storage.side_b.direction, 2),
     force = "neutral",
     pathfind_flags = {low_priority = true, no_break = true}
   }
@@ -39,10 +41,12 @@ for _, event in ipairs({
   defines.events.on_entity_cloned,
 }) do
   script.on_event(event, Handler.on_created_entity, {
-    {filter = "name", name = "undersea-cable-landing-point"},
+    -- {filter = "name", name = "undersea-cable-landing-point"},
+    {filter = "name", name = "offshore-pump"},
   })
 end
 
 script.on_event(defines.events.on_script_path_request_finished, function(event)
+  assert(event.try_again_later == false)
   game.print(serpent.block(event))
 end)
