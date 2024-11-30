9393c101ac948d7704d1c828f518469c1304c88a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-11-30 15:51:35 +0100

    Deploy the first instance of new surfacedata tracking code

--- mods_2.0/045_undersea-data-cable/control.lua
+++ mods_2.0/045_undersea-data-cable/control.lua
@@ -4,10 +4,36 @@ local mod_surface_name = "undersea-data-cable"
 
 local Handler = {}
 
+local function refresh_surfacedata()
+  -- deleted old
+  for surface_index, surfacedata in pairs(storage.surfacedata) do
+    if surfacedata.surface.valid == false then
+      storage.surfacedata[surface_index] = nil
+    end
+  end
+
+  -- created new
+  for _, surface in pairs(game.surfaces) do
+    storage.surfacedata[surface.index] = storage.surfacedata[surface.index] or {
+      surface = surface,
+    }
+  end
+end
+
 script.on_init(function()
+  storage.surfacedata = {}
+  refresh_surfacedata()
+
   local mod_surface = game.planets[mod_surface_name].create_surface()
   mod_surface.generate_with_lab_tiles = true
 end)
+
+script.on_configuration_changed(function()
+  refresh_surfacedata()
+end)
+
+script.on_event(defines.events.on_surface_created, refresh_surfacedata)
+script.on_event(defines.events.on_surface_deleted, refresh_surfacedata)
 
 function Handler.on_created_entity(event)
   local entity = event.entity or event.destination
