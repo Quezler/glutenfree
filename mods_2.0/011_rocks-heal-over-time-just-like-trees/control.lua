7e28c6cf8daf78cd4ce228143bbe12cafa78390f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-12-12 19:48:24 +0100

    Move whitelist generation out of on_configuration_changed

--- mods_2.0/011_rocks-heal-over-time-just-like-trees/control.lua
+++ mods_2.0/011_rocks-heal-over-time-just-like-trees/control.lua
@@ -1,12 +1,44 @@
-local function get_unique_index(entity)
+local function should_whitelist(prototype)
+  -- nauvis
+  if string.find(prototype.name, "rock") then
+    return true
+  end
+
+  -- vulcanus
+  if string.find(prototype.name, "chimney") then
+    return true
+  end
+
+  -- gleba
+  if string.find(prototype.name, "stromatolite") then
+    return true
+  end
+
+  -- space exploration
+  if prototype.localised_name and prototype.localised_name[1] and prototype.localised_name[1] == "entity-name.meteorite" then
+    return true
+  end
+
+  return false
+end
+
+local entity_name_whitelisted = {}
+for _, prototype in pairs(prototypes.entity) do
+  if prototype.type == "simple-entity" then
+    entity_name_whitelisted[prototype.name] = should_whitelist(prototype)
+  end
+end
+log(serpent.block(entity_name_whitelisted))
+
+local function get_index(entity)
   return entity.position.x .. "," .. entity.position.y .. "," .. entity.surface.name
 end
 
 local function maybe_add_to_damaged_rocks(entity)
-  if storage.whitelisted_names[entity.name] and 1 > entity.get_health_ratio() then
-    local unique_index = get_unique_index(entity)
-    if not storage.damaged_rocks[unique_index] then
-      storage.damaged_rocks[unique_index] = entity
+  if entity_name_whitelisted[entity.name] and 1 > entity.get_health_ratio() then
+    local index = get_index(entity)
+    if not storage.damaged_rocks[index] then
+      storage.damaged_rocks[index] = entity
     end
   end
 end
@@ -15,36 +47,10 @@ script.on_event(defines.events.on_entity_damaged, function(event)
   maybe_add_to_damaged_rocks(event.entity)
 end, {{filter = "type", type = "simple-entity"}})
 
-local function should_whitelist(simple_entity)
-  if string.find(simple_entity.name, "rock") then
-    return true
-  end
-
-  if string.find(simple_entity.name, "chimney") then
-    return true
-  end
-
-  if string.find(simple_entity.name, "stromatolite") then
-    return true
-  end
-
-  if simple_entity.localised_name and simple_entity.localised_name[1] and simple_entity.localised_name[1] == "entity-name.meteorite" then
-    return true
-  end
-
-  return false
-end
-
-local function on_init(event)
+local function on_configuration_changed(event)
   storage = {}
   storage.damaged_rocks = {}
-  storage.whitelisted_names = {}
-
-  for _, simple_entity in pairs(prototypes.get_entity_filtered({{filter = "type", type = "simple-entity"}})) do
-    if should_whitelist(simple_entity) then
-      storage.whitelisted_names[simple_entity.name] = true
-    end
-  end
+  storage.whitelisted_names = nil
 
   for _, surface in pairs(game.surfaces) do
     for _, entity in pairs(surface.find_entities_filtered{type = "simple-entity"}) do
@@ -53,8 +59,8 @@ local function on_init(event)
   end
 end
 
-script.on_init(on_init)
-script.on_configuration_changed(on_init)
+script.on_init(on_configuration_changed)
+script.on_configuration_changed(on_configuration_changed)
 
 script.on_nth_tick(60 * 10, function(event) -- heal by 1 health every 10 seconds, this is on-par with trees.
   for _, damaged_rock in pairs(storage.damaged_rocks) do
