9077cf88702057e657e0ed16dace7b809b6a5d65
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-12-15 08:29:36 +0100

    Bump version

--- mods_2.0/047_advanced-centrifuge/data.lua
+++ mods_2.0/047_advanced-centrifuge/data.lua
@@ -2,3 +2,8 @@ require("prototypes/entities/buildings/advanced-centrifuge")
 require("prototypes/items/buildings")
 require("prototypes/recipes/buildings")
 require("prototypes/technologies/buildings")
+
+if mods["space-exploration"] then
+  data.raw["item"              ]["k11-advanced-centrifuge"].icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon-base.png"
+  data.raw["assembling-machine"]["k11-advanced-centrifuge"].icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon-base.png"
+end
