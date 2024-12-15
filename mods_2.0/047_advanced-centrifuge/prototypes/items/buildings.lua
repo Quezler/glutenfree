c73356b38f2d43da788bfc3f146acf9fd9182fb5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-12-15 08:09:38 +0100

    Experiment more with tinted icons

--- mods_2.0/047_advanced-centrifuge/prototypes/items/buildings.lua
+++ mods_2.0/047_advanced-centrifuge/prototypes/items/buildings.lua
@@ -8,10 +8,23 @@ data:extend({
   {
     type = "item",
     name = "k11-advanced-centrifuge",
-    icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon.png",
+    icons = {
+      {
+        icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon-base.png",
+      },
+      {
+        icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon-mask.png",
+      },
+      {
+        icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon-mask.png",
+        tint = {0, 0.75, 0, 1},
+      }
+    },
     subgroup = subgroup,
     order = "g[centrifuge]-a[advanced-centrifuge]", -- Needs adjustment
     place_result = "k11-advanced-centrifuge",
     stack_size = 10,
   }
 })
+
+data.raw["item"]["centrifuge"].icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon.png"
