23cb767c0fe932b2974cc1562ec02ed67b33e076
Patrick 'Quezler' Mounier <patrick@queztech.net>
2024-12-17 17:16:28 +0100

    Bump version

--- mods_2.0/047_advanced-centrifuge/prototypes/items/buildings.lua
+++ mods_2.0/047_advanced-centrifuge/prototypes/items/buildings.lua
@@ -4,6 +4,8 @@ if mods["space-exploration"] then
   subgroup = "radiation"
 end
 
+local item_sounds = require("__base__.prototypes.item_sounds")
+
 data:extend({
   {
     type = "item",
@@ -11,6 +13,9 @@ data:extend({
     icon = "__advanced-centrifuge__/graphics/advanced-centrifuge/advanced-centrifuge-icon.png",
     subgroup = subgroup,
     order = "g[centrifuge]-a[advanced-centrifuge]", -- Needs adjustment
+    inventory_move_sound = item_sounds.mechanical_inventory_move,
+    pick_sound = item_sounds.mechanical_inventory_pickup,
+    drop_sound = item_sounds.mechanical_inventory_move,
     place_result = "k11-advanced-centrifuge",
     stack_size = 10,
   }
