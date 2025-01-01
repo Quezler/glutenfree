7b5402d1773dd31fcb7dc629c06155cc967cc997
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-01 13:03:47 +0100

    If QualityRockets is installed, allow configuring how much quality boosts the storage cap

--- mods_2.0/048_rocket-silos-can-buffer-more-rockets/data-updates.lua
+++ mods_2.0/048_rocket-silos-can-buffer-more-rockets/data-updates.lua
@@ -5,3 +5,20 @@ local multiplier = settings.startup[mod_prefix .. "rocket-parts-storage-cap-mult
 
 local rocket_silo = data.raw["rocket-silo"]["rocket-silo"]
 rocket_silo.rocket_parts_storage_cap = rocket_silo.rocket_parts_required * multiplier
+
+if mods["QualityRockets"] then
+  local function get_storage_cap_for_quality(quality)
+    local formula = settings.startup[mod_prefix .. "quality-rockets-multiplier-math"].value
+    local number = load("return " .. formula, "get_storage_cap_for_quality", "t", {quality = quality, math = math, multiplier = multiplier})()
+    return number
+  end
+
+  rocket_silo.rocket_parts_storage_cap = get_storage_cap_for_quality(data.raw["quality"]["normal"])
+
+  for _, quality in pairs(data.raw["quality"]) do
+    local silo = data.raw["rocket-silo"][quality.name .. "-rocket-silo"]
+    if silo then
+      rocket_silo.rocket_parts_storage_cap = get_storage_cap_for_quality(quality)
+    end
+  end
+end
--- mods_2.0/048_rocket-silos-can-buffer-more-rockets/settings.lua
+++ mods_2.0/048_rocket-silos-can-buffer-more-rockets/settings.lua
@@ -2,10 +2,23 @@ local mod_prefix = "rocket-silos-can-buffer-more-rockets--"
 
 data:extend({
   {
-      type = "int-setting",
-      name = mod_prefix .. "rocket-parts-storage-cap-multiplier",
-      setting_type = "startup",
-      default_value = 1,
-      minimum_value = 1,
+    type = "int-setting",
+    name = mod_prefix .. "rocket-parts-storage-cap-multiplier",
+    setting_type = "startup",
+    default_value = 1,
+    minimum_value = 1,
+    order = "a",
   },
 })
+
+if mods["QualityRockets"] then
+  data:extend({
+    {
+      type = "string-setting",
+      name = mod_prefix .. "quality-rockets-multiplier-math",
+      setting_type = "startup",
+      default_value = "50 * (multiplier + quality.level)",
+      order = "b",
+    },
+  })
+end
