3a01588d5da2cf395b09fdc5fcc8f68fc4d11681
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-07 22:41:24 +0100

    Fix the working viz due to nesting

--- mods_2.0/050_energy-condenser-quality-edition/graphics/disruptor/pictures.lua
+++ mods_2.0/050_energy-condenser-quality-edition/graphics/disruptor/pictures.lua
@@ -41,27 +41,29 @@ local graphics_set = {
   },
   working_visualisations =
   {
-    fadeout = true,
-    animation = {
-      priority = "high",
-      width = 590,
-      height = 590,
-      frame_count = 80,
-      animation_speed = 0.5,
-      scale = 0.5,
-      draw_as_light = true,
-      blend_mode = "additive",
-      stripes =
-      {
+    {
+      fadeout = true,
+      animation = {
+        priority = "high",
+        width = 590,
+        height = 590,
+        frame_count = 80,
+        animation_speed = 0.5,
+        scale = 0.5,
+        draw_as_light = true,
+        blend_mode = "additive",
+        stripes =
         {
-          filename = mod_directory .. "/graphics/disruptor/disruptor-hr-emission-1.png",
-          width_in_frames = 8,
-          height_in_frames = 8,
-        },
-        {
-          filename = mod_directory .. "/graphics/disruptor/disruptor-hr-emission-2.png",
-          width_in_frames = 8,
-          height_in_frames = 2,
+          {
+            filename = mod_directory .. "/graphics/disruptor/disruptor-hr-emission-1.png",
+            width_in_frames = 8,
+            height_in_frames = 8,
+          },
+          {
+            filename = mod_directory .. "/graphics/disruptor/disruptor-hr-emission-2.png",
+            width_in_frames = 8,
+            height_in_frames = 2,
+          },
         },
       },
     },
