6548bf8d009fcb75c09922bdbaf16b7c98145f54
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-07 22:48:15 +0100

    Make it look good day and night

--- mods_2.0/050_energy-condenser-quality-edition/graphics/disruptor/pictures.lua
+++ mods_2.0/050_energy-condenser-quality-edition/graphics/disruptor/pictures.lua
@@ -50,7 +50,33 @@ local graphics_set = {
         frame_count = 80,
         animation_speed = 0.5,
         scale = 0.5,
-        draw_as_light = true,
+        -- draw_as_light = true,
+        blend_mode = "additive",
+        stripes =
+        {
+          {
+            filename = mod_directory .. "/graphics/disruptor/disruptor-hr-emission-1.png",
+            width_in_frames = 8,
+            height_in_frames = 8,
+          },
+          {
+            filename = mod_directory .. "/graphics/disruptor/disruptor-hr-emission-2.png",
+            width_in_frames = 8,
+            height_in_frames = 2,
+          },
+        },
+      },
+    },
+    {
+      fadeout = true,
+      animation = {
+        priority = "high",
+        width = 590,
+        height = 590,
+        frame_count = 80,
+        animation_speed = 0.5,
+        scale = 0.5,
+        draw_as_light = true, -- only difference
         blend_mode = "additive",
         stripes =
         {
