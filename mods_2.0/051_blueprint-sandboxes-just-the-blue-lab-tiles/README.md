a7cee3b6ad48a8ebef27de89ffd859085b5c2ad3
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-07 19:11:13 +0100

    Stash fork (probably not gonna publish, weird diagonals)

---
+++ mods_2.0/051_blueprint-sandboxes-just-the-blue-lab-tiles/README.md
@@ -0,0 +1,1 @@
+Texture-only fork of https://mods.factorio.com/mod/blueprint-sandboxes by https://mods.factorio.com/user/somethingtohide (MIT)
---
+++ mods_2.0/051_blueprint-sandboxes-just-the-blue-lab-tiles/data.lua
@@ -0,0 +1,2 @@
+data.raw["tile"]["lab-dark-1"].variants.main[1].picture = "__blueprint-sandboxes-just-the-blue-lab-tiles__/graphics/terrain/lab-tiles/lab-dark-1.png"
+data.raw["tile"]["lab-dark-2"].variants.main[1].picture = "__blueprint-sandboxes-just-the-blue-lab-tiles__/graphics/terrain/lab-tiles/lab-dark-2.png"
