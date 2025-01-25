bffd11ef9e6f17629fa0bc559d9f3b642ff18deb
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-25 14:28:00 +0100

    Reach mvp stage

--- mods_2.0/054_deconstruction-planner-toggle-entity-with-pipette/control.lua
+++ mods_2.0/054_deconstruction-planner-toggle-entity-with-pipette/control.lua
@@ -4,6 +4,14 @@ local mod_prefix = "deconstruction-planner-toggle-entity-with-pipette--"
 --   game.print(string.format("%d clear cursor", event.tick))
 -- end)
 
+script.on_init(function()
+  storage.inventories = {}
+end)
+
+script.on_load(function()
+  local count = table_size(storage.inventories)
+  assert(count == 0, string.format("expected 0 but found %d lingering inventories.", count))
+end)
 
 script.on_event(mod_prefix .. "pipette", function(event)
   game.print(string.format("%d pipette", event.tick))
@@ -18,7 +26,7 @@ script.on_event(mod_prefix .. "pipette", function(event)
   local selected_prototype = assert(event.selected_prototype)
   if selected_prototype.base_type ~= "entity" then return end
 
-  game.print("decon planner! " .. serpent.line(event))
+  -- game.print("decon planner! " .. serpent.line(event))
 
   local entity_filters = cursor_stack.entity_filters
   table.insert(entity_filters, {
@@ -26,4 +34,20 @@ script.on_event(mod_prefix .. "pipette", function(event)
     quality = selected_prototype.quality,
   })
   cursor_stack.entity_filters = entity_filters
+
+  storage.inventories[player.index] = game.create_inventory(1)
+  storage.inventories[player.index][1].swap_stack(cursor_stack)
+end)
+
+script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+  local cursor_stack = player.cursor_stack
+
+  local inventory = storage.inventories[player.index]
+
+  if inventory and cursor_stack.valid_for_read == false then
+    inventory[1].swap_stack(cursor_stack)
+    inventory.destroy()
+    storage.inventories[player.index] = nil
+  end
 end)
