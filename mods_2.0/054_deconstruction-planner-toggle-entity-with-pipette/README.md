a4b839cf3f94ac0e621ce213c4d8fadb40181a63
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-25 14:19:25 +0100

    Add primary logic

---
+++ mods_2.0/054_deconstruction-planner-toggle-entity-with-pipette/README.md
@@ -0,0 +1,1 @@
+An idea i had quite a while ago but lacked the knowledge to deal with the cursor clearing conflict for, but now i do, enjoy.
--- mods_2.0/054_deconstruction-planner-toggle-entity-with-pipette/control.lua
+++ mods_2.0/054_deconstruction-planner-toggle-entity-with-pipette/control.lua
@@ -4,6 +4,26 @@ local mod_prefix = "deconstruction-planner-toggle-entity-with-pipette--"
 --   game.print(string.format("%d clear cursor", event.tick))
 -- end)
 
+
 script.on_event(mod_prefix .. "pipette", function(event)
   game.print(string.format("%d pipette", event.tick))
+
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+  local cursor_stack = player.cursor_stack
+
+  if cursor_stack == nil then return end
+  if cursor_stack.valid_for_read == false then return end
+  if cursor_stack.is_deconstruction_item == false then return end
+
+  local selected_prototype = assert(event.selected_prototype)
+  if selected_prototype.base_type ~= "entity" then return end
+
+  game.print("decon planner! " .. serpent.line(event))
+
+  local entity_filters = cursor_stack.entity_filters
+  table.insert(entity_filters, {
+    name = selected_prototype.name,
+    quality = selected_prototype.quality,
+  })
+  cursor_stack.entity_filters = entity_filters
 end)
