bc6f8dfe563ed06dbb24affbb05fcbecebc89431
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-26 13:50:04 +0100

    Begin selection tool conversion

--- mods_2.0/013_quality-upgrade-planner/control.lua
+++ mods_2.0/013_quality-upgrade-planner/control.lua
@@ -1,63 +1,10 @@
 local mod_name = "quality-upgrade-planner"
 
-local function set_mapper(upgrade_planner, i, entity_prototype, quality_prototype)
-  upgrade_planner.set_mapper(i, "from", {type = "entity", name = entity_prototype.name})
-  upgrade_planner.set_mapper(i, "to"  , {type = "entity", name = entity_prototype.name, quality = quality_prototype.name})
-end
-
 local function on_configuration_changed()
   for _, inventory in ipairs(game.get_script_inventories(mod_name)[mod_name]) do
     inventory.destroy()
-  end
-
-  -- /c game.player.opened = game.get_script_inventories("quality-upgrade-planner")["quality-upgrade-planner"][1]
-  storage.inventory = game.create_inventory(1, "Quality upgrade planner")
-  storage.inventory[1].set_stack({name = "quality-book", count = 1})
-
-  local pages = storage.inventory[1].get_inventory(defines.inventory.item_main)
-  assert(pages)
-
-  for _, quality in pairs(prototypes.quality) do
-    if quality.hidden then goto continue end -- quality-unknown
-    -- game.print(quality.name)
-
-    pages.insert({name = "upgrade-planner", count = 1})
-    local upgrade_planner = pages[#pages]
-
-    upgrade_planner.set_stack({name = "upgrade-planner", count = 1})
-    upgrade_planner.preview_icons = {
-      {
-        index = 1,
-        signal = {type = "quality", name = quality.name},
-      }
-    }
-    upgrade_planner.label = string.format("[color=%f,%f,%f]", quality.color.r, quality.color.g, quality.color.b) .. quality.name:gsub("^%l", string.upper) .. "[/color]"
-
-    local i = 1
-    for _, entity in pairs(prototypes.entity) do
-      local success, error = pcall(set_mapper, upgrade_planner, i, entity, quality)
-      if success == false then
-        assert(error)
-        if string.find(error, " is not a valid upgrade source entity.") == nil then
-          log(error)
-        end
-      end
-      if success == true then i = i + 1 end
-    end
-
-    ::continue::
   end
 end
 
 script.on_init(on_configuration_changed)
 script.on_configuration_changed(on_configuration_changed)
-
-commands.add_command("quality-upgrade-planner", "Receive a quality upgrade planner.", function(command)
-  local player = game.get_player(command.player_index)
-  assert(player)
-
-  player.clear_cursor()
-  if player.cursor_stack.valid_for_read == true then return end -- cursor not cleared?
-
-  player.cursor_stack.set_stack(storage.inventory[1])
-end)
--- mods_2.0/013_quality-upgrade-planner/data.lua
+++ mods_2.0/013_quality-upgrade-planner/data.lua
@@ -1,8 +1,54 @@
-local quality_book = table.deepcopy(data.raw["blueprint-book"]["blueprint-book"])
+local item_sounds = require("__base__.prototypes.item_sounds")
 
-quality_book.name = "quality-book"
-quality_book.icon = "__quality-upgrade-planner__/graphics/icons/quality-book.png"
-quality_book.hidden = true
-table.insert(quality_book.flags, "only-in-cursor")
+local selection_tool = {
+  type = "selection-tool",
+  name = "quality-upgrade-planner",
+  icons = {
+    {icon = data.raw["upgrade-item"]["upgrade-planner"].icon},
+    {icon = "__core__/graphics/icons/any-quality.png", scale = 0.375},
+  },
+  flags = {"spawnable"},
+  subgroup = "tool",
+  order = "c[automated-construction]-d[quality-upgrade-planner]",
+  inventory_move_sound = item_sounds.planner_inventory_move,
+  pick_sound = item_sounds.planner_inventory_pickup,
+  drop_sound = item_sounds.planner_inventory_move,
+  stack_size = 1,
+  skip_fog_of_war = true,
+  select =
+  {
+    border_color = {71, 255, 73},
+    mode = {"upgrade"},
+    cursor_box_type = "not-allowed",
+    started_sound = { filename = "__core__/sound/upgrade-select-start.ogg" },
+    ended_sound = { filename = "__core__/sound/upgrade-select-end.ogg" }
+  },
+  alt_select =
+  {
+    border_color = {0, 0, 0, 0},
+    mode = {"nothing"},
+    cursor_box_type = "not-allowed",
+  },
+  reverse_select =
+  {
+    border_color = {246, 255, 0},
+    mode = {"downgrade"},
+    cursor_box_type = "not-allowed",
+    started_sound = { filename = "__core__/sound/upgrade-select-start.ogg" },
+    ended_sound = { filename = "__core__/sound/upgrade-select-end.ogg" }
+  },
+  reverse_alt_select =
+  {
+    border_color = {0, 0, 0, 0},
+    mode = {"nothing"},
+    cursor_box_type = "not-allowed",
+  },
+  super_forced_select =
+  {
+    border_color = {0, 0, 0, 0},
+    mode = {"nothing"},
+    cursor_box_type = "not-allowed",
+  },
+}
 
-data:extend{quality_book}
+data:extend{selection_tool}
