e7e768f1b91dbf1935cfee2bd70420a15be85f12
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-26 16:35:07 +0100

    Generate greyscale quality category icons

---
+++ mods_2.0/013_quality-upgrade-planner/graphics/icons/generate.sh
@@ -0,0 +1,8 @@
+# (cd ./mods_2.0/013_quality-upgrade-planner/graphics/icons && sh generate.sh)
+mkdir ./quality-category
+magick /Users/quezler/Documents/Tower/github/wube/Factorio/data/base/graphics/icons/transport-belt.png       -colorspace Gray quality-category/entities.png
+magick /Users/quezler/Documents/Tower/github/wube/Factorio/data/base/graphics/icons/inserter.png             -colorspace Gray quality-category/inserters.png
+magick /Users/quezler/Documents/Tower/github/wube/Factorio/data/base/graphics/icons/assembling-machine-2.png -colorspace Gray quality-category/recipes.png
+magick /Users/quezler/Documents/Tower/github/wube/Factorio/data/base/graphics/icons/beacon.png               -colorspace Gray quality-category/modules.png
+magick /Users/quezler/Documents/Tower/github/wube/Factorio/data/base/graphics/icons/logistic-robot.png       -colorspace Gray quality-category/requests.png
+magick /Users/quezler/Documents/Tower/github/wube/Factorio/data/base/graphics/icons/power-switch.png         -colorspace Gray quality-category/conditions.png
