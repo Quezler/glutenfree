7437526c284fb6c12cccf08623f2909ad9391ad8
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-30 16:05:43 +0100

    Add tooltip to productivity

--- mods_2.0/055_beacon-interface/control.lua
+++ mods_2.0/055_beacon-interface/control.lua
@@ -142,7 +142,10 @@ local function open_gui(entity, player)
       caption = string.upper(string.sub(effect, 1, 1)) .. string.sub(effect, 2),
     }
     label.style.font = "default-bold"
-    if effect == "quality" then
+    if effect == "productivity" then
+      label.caption = "[img=info] " .. label.caption
+      label.tooltip = {"beacon-interface.productivity-tooltip"}
+    elseif effect == "quality" then
       label.caption = "[img=info] " .. label.caption
       label.tooltip = {"beacon-interface.quality-tooltip"}
     end
