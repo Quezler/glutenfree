1acc98a15f6ffca300c2651e9ecab0adb34c2635
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-30 15:11:47 +0100

    Give the beacons during `/cheat all` regardless of ee

--- mods_2.0/055_beacon-interface/scripts/compatibility.lua
+++ mods_2.0/055_beacon-interface/scripts/compatibility.lua
@@ -9,17 +9,15 @@ if script.active_mods["EditorExtensions"] then
       inventory.insert({name = mod_prefix .. "beacon", count = 20})
     end
   end)
-
-  -- the first 4 lines inside this function are paraphrased from the MIT licenced "Editor Extentions" mod by raiguard
-  script.on_event(defines.events.on_console_command, function (event)
-    if event.command ~= "cheat" or not game.console_command_used then return end
-    local player = game.get_player(event.player_index)
-    if not player then return end
-    if event.parameters ~= "all" then return end
-
-    local inventory = player.get_inventory(defines.inventory.character_main) --[[@as LuaInventory]]
-    if inventory.get_item_count("ee-super-substation") > 0 then -- detect whether "ee.set_loadout()" ran
-      inventory.insert({name = mod_prefix .. "beacon", count = 20})
-    end
-  end)
 end
+
+-- the first 4 lines inside this function are paraphrased from the MIT licenced "Editor Extentions" mod by raiguard
+script.on_event(defines.events.on_console_command, function (event)
+  if event.command ~= "cheat" or not game.console_command_used then return end
+  local player = game.get_player(event.player_index)
+  if not player then return end
+  if event.parameters ~= "all" then return end
+
+  local inventory = player.get_inventory(defines.inventory.character_main) --[[@as LuaInventory]]
+  inventory.insert({name = mod_prefix .. "beacon", count = 20})
+end)
