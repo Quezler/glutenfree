a7083e0b666b6acd4ff9bd418324f51ce839e048
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-01-31 15:53:51 +0100

    Get a reset pulse every time the sum changes

--- mods_2.0/050_energy-condenser-quality-edition/control.lua
+++ mods_2.0/050_energy-condenser-quality-edition/control.lua
@@ -33,7 +33,9 @@ function Handler.on_created_entity(event)
     entity = entity,
 
     container = nil,
-    arithmetic_1 = nil,
+    arithmetic_1 = nil, -- each + 0 = S
+    arithmetic_2 = nil, -- each + 0 = each
+    decider_1 = nil, -- red T != green T = R 1
   })
 
   struct.container = entity.surface.create_entity{
@@ -63,7 +65,7 @@ function Handler.on_created_entity(event)
     },
     operation = "+",
     output_signal = {
-      name = "signal-T", -- T for total of all combined signals
+      name = "signal-S",
       type = "virtual"
     },
     second_constant = 0,
@@ -77,6 +79,96 @@ function Handler.on_created_entity(event)
     local red_out = struct.container.get_wire_connector(defines.wire_connector_id.circuit_red, true) --[[@as LuaWireConnector]]
     local red_in = struct.arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_input_red, false) --[[@as LuaWireConnector]]
     assert(red_out.connect_to(red_in, false, defines.wire_origin.script))
+  end
+
+  struct.arithmetic_2 = storage.surface.create_entity{
+    name = "arithmetic-combinator",
+    force = "neutral",
+    position = {0.5 + storage.index, -3.0},
+    direction = defines.direction.north,
+  }
+  assert(struct.arithmetic_2)
+  arithmetic_2_cb = struct.arithmetic_2.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
+  arithmetic_2_cb.parameters = {
+    first_signal = {
+      name = "signal-S",
+      type = "virtual"
+    },
+    first_signal_networks = {
+      green = true,
+      red = true
+    },
+    operation = "+",
+    output_signal = {
+      name = "signal-S",
+      type = "virtual"
+    },
+    second_constant = 0,
+    second_signal_networks = {
+      green = true,
+      red = true
+    }
+  }
+
+  do
+    local red_out = struct.arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_output_red, false) --[[@as LuaWireConnector]]
+    local red_in = struct.arithmetic_2.get_wire_connector(defines.wire_connector_id.combinator_input_red, false) --[[@as LuaWireConnector]]
+    assert(red_out.connect_to(red_in, false, defines.wire_origin.player))
+  end
+
+  struct.decider_1 = storage.surface.create_entity{
+    name = "decider-combinator",
+    force = "neutral",
+    position = {0.5 + storage.index, -5.0},
+    direction = defines.direction.north,
+  }
+  assert(struct.decider_1)
+  decider_1_cb = struct.decider_1.get_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
+  decider_1_cb.parameters = {
+    conditions = {
+      {
+        comparator = "≠",
+        compare_type = "or",
+        first_signal = {
+          name = "signal-S",
+          type = "virtual"
+        },
+        first_signal_networks = {
+          green = false,
+          red = true
+        },
+        second_signal = {
+          name = "signal-S",
+          type = "virtual"
+        },
+        second_signal_networks = {
+          green = true,
+          red = false
+        }
+      }
+    },
+    outputs = {
+      {
+        copy_count_from_input = false,
+        networks = {
+          green = true,
+          red = true
+        },
+        signal = {
+          name = "signal-R",
+          type = "virtual"
+        }
+      }
+    }
+  }
+
+  do
+    local red = struct.arithmetic_2.get_wire_connector(defines.wire_connector_id.combinator_output_red, false) --[[@as LuaWireConnector]]
+    local green = struct.arithmetic_1.get_wire_connector(defines.wire_connector_id.combinator_output_green, false) --[[@as LuaWireConnector]]
+    local red_in = struct.decider_1.get_wire_connector(defines.wire_connector_id.combinator_input_red, false) --[[@as LuaWireConnector]]
+    local green_in = struct.decider_1.get_wire_connector(defines.wire_connector_id.combinator_input_green, false) --[[@as LuaWireConnector]]
+    assert(red.connect_to(red_in, false, defines.wire_origin.player))
+    assert(green.connect_to(green_in, false, defines.wire_origin.player))
   end
 
   storage.index = storage.index + 1
