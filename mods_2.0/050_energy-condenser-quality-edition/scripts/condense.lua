5b85f79c6b384ae6fc4ff1e2773f1502239c388a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-03 16:59:23 +0100

    Initial attempt at preserving spoil percentages

--- mods_2.0/050_energy-condenser-quality-edition/scripts/condense.lua
+++ mods_2.0/050_energy-condenser-quality-edition/scripts/condense.lua
@@ -21,9 +21,36 @@ end
 -- log(serpent.block(next_quality_name))
 -- log(serpent.block(is_quality_before))
 
+local item_can_spoil = {}
+for _, item in pairs(prototypes.item) do
+  if item.get_spoil_ticks() > 0 then
+    item_can_spoil[item.name] = true
+  end
+end
+
+local function populate_spoil_percentages(inventory)
+  local spoil_percentages = {}
+
+  for slot = 1, #inventory do
+    local item = inventory[slot]
+    if item.valid_for_read and item_can_spoil[item.name] then
+      local key = item.quality.name .. "-" .. item.name
+      if spoil_percentages[key] then
+        spoil_percentages[key] = spoil_percentages[key] + item.spoil_percent / 2
+      else
+        spoil_percentages[key] = item.spoil_percent
+      end
+    end
+  end
+
+  return spoil_percentages
+end
+
 function Condense.trigger(struct)
   local target_quality = ensure_recipe_is_set(struct.entity).name
   local quality_points = math.floor(math.min((struct.entity.effects["quality"] or 0) * 100) + 0.5) -- 0-1000
+
+  local spoil_percentages = nil
 
   for _, item in ipairs(struct.container_inventory.get_contents()) do
     local next_quality_name = get_next_quality_name[item.quality]
@@ -41,8 +68,16 @@ function Condense.trigger(struct)
 
         log(string.format("%d x %s (%s) x %d%% = %d (%d + %f)", item.count, item.name, item.quality, quality_points / 10, integer, number, decimal))
 
+        local to_insert = {name = item.name, count = integer, quality = next_quality_name}
+        if item_can_spoil then
+          if spoil_percentages == nil then
+            spoil_percentages = populate_spoil_percentages(struct.container_inventory)
+          end
+          to_insert["spoil_percent"] = assert(spoil_percentages[item.quality .. "-" .. item.name])
+        end
+
         struct.container_inventory.remove(item) -- all items are consumed, this way there is always space.
-        struct.container_inventory.insert({name = item.name, count = integer, quality = next_quality_name})
+        struct.container_inventory.insert(to_insert)
       end
     end
   end
