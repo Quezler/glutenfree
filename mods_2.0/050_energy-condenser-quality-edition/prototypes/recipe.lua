ca8d8a4d210d3876f347eeed57c603e8b71fde6c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-03 12:41:02 +0100

    Use a whole bunch of items as placeholder

--- mods_2.0/050_energy-condenser-quality-edition/prototypes/recipe.lua
+++ mods_2.0/050_energy-condenser-quality-edition/prototypes/recipe.lua
@@ -1,3 +1,13 @@
+local item = {
+  type = "item",
+  name = mod_prefix .. "recipe",
+  icon = "__core__/graphics/empty.png",
+  icon_size = 1,
+  stack_size = 1,
+  flags = {"not-stackable"},
+  hidden = true,
+}
+
 local recipe_category = {
   type = "recipe-category",
   name = mod_prefix .. "recipe-category",
@@ -11,11 +21,11 @@ local recipe = {
   category = recipe_category.name,
   enabled = true,
   auto_recycle = false,
-  energy_required = 30,
-  ingredients = {{type = "item", name = "repair-pack", amount = 1}},
+  energy_required = 20,
+  ingredients = {{type = "item", name = item.name, amount = 1}},
   results = {},
   hide_from_player_crafting = true,
   hidden_in_factoriopedia = true,
 }
 
-data:extend{recipe_category, recipe}
+data:extend{item, recipe_category, recipe}
