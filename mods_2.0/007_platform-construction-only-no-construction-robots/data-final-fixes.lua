0bfa837ffa998a3387ed2857041b27b31d17c9e0
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-05 18:40:42 +0100

    Modify the construction robots in data-final-fixes

--- mods_2.0/007_platform-construction-only-no-construction-robots/data-final-fixes.lua
+++ mods_2.0/007_platform-construction-only-no-construction-robots/data-final-fixes.lua
@@ -1,1 +1,39 @@
 require("utility-constants")
+
+local construction_robot = data.raw["construction-robot"]["construction-robot"]
+
+local function turn_construction_robots_invisible()
+  construction_robot.idle = nil
+  construction_robot.in_motion = nil
+  construction_robot.shadow_idle = nil
+  construction_robot.shadow_in_motion = nil
+  construction_robot.working = nil
+  construction_robot.shadow_working = nil
+
+  construction_robot.sparks = nil
+  construction_robot.smoke = nil
+end
+
+turn_construction_robots_invisible()
+construction_robot.icon_draw_specification = {scale = 0}
+construction_robot.quality_indicator_scale = 0
+construction_robot.selectable_in_game = false
+construction_robot.speed = 10
+construction_robot.energy_per_move = nil
+construction_robot.energy_per_tick = nil
+
+local created_effect = {
+  type = "direct",
+  action_delivery = {
+    type = "instant",
+    source_effects = {
+      {
+        type = "script",
+        effect_id = "construction-robot-created",
+      },
+    }
+  }
+}
+
+assert(construction_robot.created_effect == nil)
+construction_robot.created_effect = created_effect
--- mods_2.0/007_platform-construction-only-no-construction-robots/data.lua
+++ mods_2.0/007_platform-construction-only-no-construction-robots/data.lua
@@ -1,22 +1,3 @@
-local construction_robot = data.raw["construction-robot"]["construction-robot"]
-
-local function turn_construction_robots_invisible()
-  construction_robot.idle = nil
-  construction_robot.in_motion = nil
-  construction_robot.shadow_idle = nil
-  construction_robot.shadow_in_motion = nil
-  construction_robot.working = nil
-  construction_robot.shadow_working = nil
-
-  construction_robot.sparks = nil
-  construction_robot.smoke = nil
-end
-
-turn_construction_robots_invisible()
-construction_robot.icon_draw_specification = {scale = 0}
-construction_robot.quality_indicator_scale = 0
-construction_robot.selectable_in_game = false
-
 local space_platform_entity_build_animations = require("__space-age__/graphics/entity/space-platform-build-anim/entity-build-animations")
 
 local function make_animation_prototype(a, b)
@@ -38,23 +19,6 @@ make_animation_prototype("front_left", "body")
 make_animation_prototype("front_right", "top")
 make_animation_prototype("front_right", "body")
 
-local created_effect = {
-  type = "direct",
-  action_delivery = {
-    type = "instant",
-    source_effects = {
-      {
-        type = "script",
-        effect_id = "construction-robot-created",
-      },
-    }
-  }
-}
-
-local construction_robot = data.raw["construction-robot"]["construction-robot"]
-assert(construction_robot.created_effect == nil)
-construction_robot.created_effect = created_effect
-
 -- borrowed from the ghosts-do-not-kick-you-out-of-their-gui mod
 data:extend{{
   type = "simple-entity",
@@ -70,7 +34,3 @@ data:extend{{
   selectable_in_game = false,
   hidden = true,
 }}
-
-construction_robot.speed = 10
-construction_robot.energy_per_move = nil
-construction_robot.energy_per_tick = nil
