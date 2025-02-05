511c1433a920cb42d4b932d145ccf12a89a01ee9
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-05 22:13:11 +0100

    Prototype a nothing effect

---
+++ mods_2.0/050_energy-condenser-quality-edition/data-final-fixes.lua
@@ -0,0 +1,11 @@
+require("shared")
+
+table.insert(data.raw["technology"]["automation-science-pack"].effects, {
+  type = "nothing",
+  icons = {
+    {icon = data.raw["assembling-machine"][mod_prefix .. "crafter"].icon},
+    {icon = "__core__/graphics/icons/any-quality.png", shift = {8, 8}, scale = 0.25},
+  },
+  mod_prefix .. "automation-science-pack",
+  effect_description = {"effect-description.quality-condenser-quality", tostring(12.5)}
+})
