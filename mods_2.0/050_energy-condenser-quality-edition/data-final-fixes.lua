19151f585a1be45e30d6a72d12a4c8858f56139d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-06 12:19:33 +0100

    Apply the technology modifiers

--- mods_2.0/050_energy-condenser-quality-edition/data-final-fixes.lua
+++ mods_2.0/050_energy-condenser-quality-edition/data-final-fixes.lua
@@ -1,13 +1,25 @@
 require("shared")
 
-local quality = -20
+local function string_split(s, delimiter)
+  local result = {}
+  for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+      table.insert(result, match)
+  end
+  return result
+end
 
-table.insert(data.raw["technology"]["automation-science-pack"].effects, {
-  type = "nothing",
-  icons = {
-    {icon = data.raw["assembling-machine"][mod_prefix .. "crafter"].icon},
-    {icon = "__core__/graphics/icons/any-quality.png", shift = {8, 8}, scale = 0.25},
-  },
-  mod_prefix .. "automation-science-pack",
-  effect_description = {"effect-description.quality-condenser-quality", quality > 0 and "+" or "", tostring(quality)}
-})
+local parts = settings.startup[mod_prefix .. "technology-effects"].value or "" --[[@as string]]
+for _, part in ipairs(string_split(parts, ",")) do
+  local technology_name, quality_modifier = table.unpack(string_split(part, "="))
+
+  assert(data.raw["technology"][technology_name], string.format("unknown technology: %s", technology_name))
+
+  table.insert(data.raw["technology"][technology_name].effects, {
+    type = "nothing",
+    icons = {
+      {icon = data.raw["assembling-machine"][mod_prefix .. "crafter"].icon},
+      {icon = "__core__/graphics/icons/any-quality.png", shift = {8, 8}, scale = 0.25},
+    },
+    effect_description = {"effect-description.quality-condenser-quality", tonumber(quality_modifier) > 0 and "+" or "", quality_modifier}
+  })
+end
