7953cf5d9ec43c0172d536c609f7d990d0885360
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-14 16:47:10 +0100

    Track entities and destroy when hitting limit

--- mods_2.0/060_you-can-only-build-x-entity-name-here-per-surface/control.lua
+++ mods_2.0/060_you-can-only-build-x-entity-name-here-per-surface/control.lua
@@ -1,3 +1,5 @@
+mod = {}
+
 local function refresh_surfacedata()
   -- deleted old
   for surface_index, surfacedata in pairs(storage.surfacedata) do
@@ -15,15 +17,18 @@ local function refresh_surfacedata()
   end
 end
 
+script.on_event(defines.events.on_surface_created, refresh_surfacedata)
+script.on_event(defines.events.on_surface_deleted, refresh_surfacedata)
+
 script.on_init(function()
   storage.limits = {}
 
   storage.surfacedata = {}
   refresh_surfacedata()
 
-  remote.call("you-can-only-build-x-entity-name-here-per-surface", "add_limit", "wooden-chest", {limit = 0})
-  remote.call("you-can-only-build-x-entity-name-here-per-surface", "add_limit", "iron-chest", {limit = 1})
-  remote.call("you-can-only-build-x-entity-name-here-per-surface", "add_limit", "steel-chest", {limit = 2})
+  remote.call("you-can-only-build-x-entity-name-here-per-surface", "add_limit", "wooden-chest", {count = 0})
+  remote.call("you-can-only-build-x-entity-name-here-per-surface", "add_limit", "iron-chest", {count = 1})
+  remote.call("you-can-only-build-x-entity-name-here-per-surface", "add_limit", "steel-chest", {count = 2})
 end)
 
 script.on_configuration_changed(function()
@@ -32,16 +37,40 @@ script.on_configuration_changed(function()
   refresh_surfacedata()
 end)
 
-script.on_event(defines.events.on_surface_created, refresh_surfacedata)
-script.on_event(defines.events.on_surface_deleted, refresh_surfacedata)
-
-local function on_created_entity(event)
-  local entity = event.entity or event.destination
-
-  game.print(string.format("%s (%d)", entity.name, storage.limits[entity.name].limit))
+function mod.sum_entities(entities, force)
+  local sum = 0
+  for unit_number, entity in pairs(entities) do
+    if entity.valid and entity.force == force then
+      sum = sum + 1
+    end
+  end
+  return sum
 end
 
-local function refresh_on_created_entity()
+function mod.on_created_entity(event)
+  local entity = event.entity or event.destination
+  local entity_name = entity.type == "entity-ghost" and entity.ghost_name or entity.name
+  local limit = assert(storage.limits[entity_name])
+
+  local surfacedata = storage.surfacedata[entity.surface.index]
+
+  -- we're not gonna seperate them by force, there are lots of events that can shuffle forces around,
+  -- and considering in 9 out of 10 cases they're all owned by the same force it is not that important,
+  -- also since the nature of this mod is to keep count low i doubt we get lots to go through constantly.
+  surfacedata.entities[entity_name] = surfacedata.entities[entity_name] or {}
+  local entity_count = mod.sum_entities(surfacedata.entities[entity_name], entity.force)
+
+  if entity_count >= limit.count then
+    entity.destroy()
+    return
+  end
+
+  surfacedata.entities[entity.name][assert(entity.unit_number)] = entity
+
+  -- game.print(string.format("%s (%d)", entity.name, storage.limits[entity.name].limit))
+end
+
+function mod.refresh_on_created_entity()
   local filters = {}
 
   for entity_name, limit in pairs(storage.limits) do
@@ -57,12 +86,12 @@ local function refresh_on_created_entity()
     defines.events.script_raised_revive,
     defines.events.on_entity_cloned,
   }) do
-    script.on_event(event, on_created_entity, filters)
+    script.on_event(event, mod.on_created_entity, filters)
   end
 end
 
 script.on_load(function()
-  refresh_on_created_entity()
+  mod.refresh_on_created_entity()
 end)
 
 remote.add_interface("you-can-only-build-x-entity-name-here-per-surface", {
@@ -76,7 +105,7 @@ remote.add_interface("you-can-only-build-x-entity-name-here-per-surface", {
     assert(type(entity) == "string")
     assert(storage.limits[entity] == nil)
     storage.limits[entity] = {
-      limit = assert(config.limit),
+      count = assert(config.count),
     }
     -- if config == nil or table_size(config) == 0 then
     --   storage.limits[entity] = nil
@@ -91,7 +120,7 @@ remote.add_interface("you-can-only-build-x-entity-name-here-per-surface", {
     -- assert(prototypes.entity[data.entity])
     -- storage.limits[data.entity_name]
 
-    refresh_on_created_entity()
+    mod.refresh_on_created_entity()
   end,
 })
 
