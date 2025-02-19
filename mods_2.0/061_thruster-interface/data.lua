b1eb0e58a458df24b4ee0c7b1f3fc0a477e89e91
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-19 16:09:53 +0100

    Get the thruster performance curve to show

--- mods_2.0/061_thruster-interface/data.lua
+++ mods_2.0/061_thruster-interface/data.lua
@@ -1,18 +1,39 @@
 require("shared")
 
-local thruster = table.deepcopy(data.raw["thruster"]["thruster"])
-thruster.name = mod_name
+local entity = table.deepcopy(data.raw["thruster"]["thruster"])
+entity.name = mod_name
 
-thruster.icons =
+entity.icons =
 {{
-  icon = thruster.icon,
+  icon = entity.icon,
   tint = {0.5, 0.5, 1}
 }}
-thruster.icon = nil
+entity.icon = nil
 
-thruster.minable.result = nil
-thruster.min_performance = {fluid_volume = 0, fluid_usage =  0, effectivity =  0}
-thruster.max_performance = {fluid_volume = 1, fluid_usage = 10, effectivity = 10}
+entity.min_performance = {fluid_volume =  0, fluid_usage =  0, effectivity =  1}
+entity.max_performance = {fluid_volume = 10, fluid_usage = 10, effectivity = 10}
+
+local item = table.deepcopy(data.raw["item"]["thruster"])
+item.name = mod_name
+
+item.icons = entity.icons
+item.icon = nil
+
+item.place_result = entity.name
+entity.minable.result = item.name
+
+local recipe = {
+  type = "recipe",
+  name = mod_name,
+  enabled = true,
+  ingredients =
+  {
+    {type = "item", name = "thruster", amount = 1},
+    {type = "item", name = "infinity-pipe", amount = 1},
+  },
+  energy_required = 1,
+  results = {{type="item", name = item.name, amount=1}}
+}
 
 -- table.insert(thruster.fuel_fluid_box.pipe_connections, {
 --   flow_direction = "input", connection_type = "linked", linked_connection_id = 1, position = {-1.5,  0},
@@ -37,4 +58,4 @@ thruster.max_performance = {fluid_volume = 1, fluid_usage = 10, effectivity = 10
 --   {flow_direction = "input-output", connection_type = "linked", linked_connection_id = 1},
 -- }
 
-data:extend{thruster}
+data:extend{entity, item, recipe}
