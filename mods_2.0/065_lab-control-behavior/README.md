c436c1c3bd438634902a9ec92eaaa7a8c261adf0
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-02-28 18:08:30 +0100

    Initial commit

---
+++ mods_2.0/065_lab-control-behavior/README.md
@@ -0,0 +1,1 @@
+The devs blessed us with the `proxy-container` prototype in `2.0.38`, so enjoy the spoils by checking your science levels with circuits.
---
+++ mods_2.0/065_lab-control-behavior/control.lua
@@ -0,0 +1,37 @@
+require("shared")
+local mod = {}
+
+script.on_init(function()
+  storage.deathrattles = {}
+  storage.structs = {}
+
+  for _, surface in pairs(game.surfaces) do
+    for _, entity in pairs(surface.find_entities_filtered({type = "lab"})) do
+      mod.on_created_entity({entity = entity})
+    end
+  end
+end)
+
+function mod.on_created_entity(event)
+  local entity = event.entity or event.destination
+
+  local struct = new_struct(storage.structs, {
+    id = entity.unit_number,
+    entity = entity,
+  })
+
+  game.print("new lab registered: " .. tostring(entity))
+end
+
+for _, event in ipairs({
+  defines.events.on_built_entity,
+  defines.events.on_robot_built_entity,
+  defines.events.on_space_platform_built_entity,
+  defines.events.script_raised_built,
+  defines.events.script_raised_revive,
+  defines.events.on_entity_cloned,
+}) do
+  script.on_event(event, mod.on_created_entity, {
+    {filter = "type", type = "lab"},
+  })
+end
