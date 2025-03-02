5aa1b0fdea4057556b23863adc9f6fcaed77a190
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-03-02 12:42:48 +0100

    Decouple the uplink from the assembling-machine-1

--- mods_2.0/066_character-inventory-uplink/data.lua
+++ mods_2.0/066_character-inventory-uplink/data.lua
@@ -1,5 +1,7 @@
 require("shared")
 
+local proxy_tint = {0.8, 0.1, 0.3}
+
 local Hurricane = require("graphics/hurricane")
 local skin = Hurricane.crafter({
   name = "suit-plug-outlet",
@@ -8,14 +10,59 @@ local skin = Hurricane.crafter({
   shadow_width = 400, shadow_height = 350,
 })
 
-local entity = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
-entity.name = mod_name
-entity.icon = skin.icon
-entity.graphics_set = skin.graphics_set
-entity.crafting_speed = 1
-entity.selection_box[2][2] = entity.selection_box[2][2] + 1
-entity.collision_box[2][2] = entity.collision_box[2][2] + 1
-entity.next_upgrade = nil
-data:extend{entity}
+local entity = {
+  type = "assembling-machine",
+  name = mod_name,
 
-data.raw["item"]["assembling-machine-1"].place_result = entity.name
+  icon = skin.icon,
+  graphics_set = skin.graphics_set,
+
+  crafting_speed = 1,
+  selection_box = {{-1.5, -1.5}, {1.5, 2.5}},
+  collision_box = {{-1.2, -1.2}, {1.2, 2.2}},
+
+  energy_usage = "1kW",
+  energy_source = {type = "void"},
+
+  icon_draw_specification = {shift = {0, 0.75}, scale = 0.75},
+
+  flags = {"player-creation", "placeable-player"},
+  circuit_wire_max_distance = 9,
+}
+
+local item = {
+  type = "item",
+  name = mod_name,
+
+  icon = skin.icon,
+
+  stack_size = 5,
+  weight = 200*kg,
+  place_result = entity.name,
+}
+
+local recipe_category = {
+  type = "recipe-category",
+  name = mod_name,
+}
+
+
+local recipe = {
+  type = "recipe",
+  name = mod_prefix .. "active",
+
+  icon = data.raw["character"]["character"].icon,
+
+  energy_required = 1,
+
+  ingredients = {},
+  results = {},
+
+  category = recipe_category.name,
+}
+
+entity.crafting_categories = {recipe_category.name}
+entity.fixed_recipe = recipe.name
+entity.fixed_quality = "normal"
+
+data:extend{entity, item, recipe_category, recipe}
