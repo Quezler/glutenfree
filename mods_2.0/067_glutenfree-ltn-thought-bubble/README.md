df9ee63c6dcdd75a4e9ee20375a4b8f211cbc8ab
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-03-02 15:25:34 +0100

    Ported to 2.0

---
+++ mods_2.0/067_glutenfree-ltn-thought-bubble/README.md
@@ -0,0 +1,5 @@
+As long as a train is underway to its pickup station, all its locomotives will preview what the train is going to pickup "alt mode style", once it arrives at the pickup station it'll vanish again until the next time it gets dispatched from the depot.
+
+The "fake" alt mode sits in the uncanny valley, help with finetuning the looks is appreciated.
+
+Currently only draws one item instead of 4 (or more) like you'd might expect, again: help is welcome.
---
+++ mods_2.0/067_glutenfree-ltn-thought-bubble/scripts/bubble.lua
@@ -0,0 +1,162 @@
+local bubble = {}
+
+function bubble.init()
+  storage.deliveries = {}
+  storage.deliveries_table_was_previously_empty = true
+
+  storage.entries = {}
+  rendering.clear('glutenfree-ltn-thought-bubble')
+end
+
+function bubble.on_dispatcher_updated(event)
+  -- game.print('on_dispatcher_updated @ ' .. event.tick)
+  storage.deliveries = event.deliveries
+
+  if storage.deliveries_table_was_previously_empty then
+    storage.deliveries_table_was_previously_empty = false
+    -- game.print('on_dispatcher_updated @ ' .. event.tick)
+
+    for _, delivery in pairs(storage.deliveries) do
+      if delivery.train.valid then
+        bubble.update_train(delivery.train)
+      end
+    end
+  else
+    for _, train_id in pairs(event.new_deliveries) do
+      if storage.deliveries[train_id].train.valid then
+        bubble.update_train(storage.deliveries[train_id].train)
+      end
+    end
+  end
+end
+
+function bubble.on_delivery_created(event)
+  -- game.print('on_delivery_created @ ' .. event.tick)
+  storage.deliveries[event.train.id] = event
+
+  bubble.update_train(event.train)
+end
+
+function bubble.on_train_schedule_changed(event)
+  bubble.update_train(event.train)
+end
+
+function bubble.update_train(train)
+  if not storage.entries then return end -- LTN updating all trains prior to our init/load
+
+  local delivery = storage.deliveries[train.id]
+  if not delivery or not bubble.train_is_approaching_station(train, delivery.from) then
+
+    if storage.entries[train.id] then
+      for _, sprite in ipairs(storage.entries[train.id].sprites) do
+        sprite.destroy()
+      end
+      storage.entries[train.id] = nil
+    end
+
+    return
+  end
+
+  if storage.entries[train.id] then return end -- there's already a thought bubble active
+
+  local entry = {
+    train = train,
+    sprites = {},
+  }
+
+  local what = bubble.pick_one_what_from_shipment(delivery.shipment)
+  if not what then
+    game.print(serpent.block(delivery))
+    return
+  end
+  local shipment_sprite = what:gsub(',','.')
+
+  for _, locomotive in ipairs(bubble.get_locomotives(train)) do
+    entry.sprites[#entry.sprites+1] = rendering.draw_sprite{
+      sprite = 'utility.entity_info_dark_background',
+      target = {entity = locomotive, offset = {0, -0.75}},
+      surface = locomotive.surface,
+      -- target_offset = {0, -0.75}, -- data.raw['locomotive']['locomotive'].alert_icon_shift - 0.2
+      x_scale = 0.75, -- looks good-ish
+      y_scale = 0.75, -- looks good-ish
+      only_in_alt_mode = true,
+    }
+
+    entry.sprites[#entry.sprites+1] = rendering.draw_sprite{
+      sprite = shipment_sprite,
+      target = {entity = locomotive, offset = {0, -0.75}},
+      surface = locomotive.surface,
+      -- target_offset = {0, -0.75}, -- data.raw['locomotive']['locomotive'].alert_icon_shift - 0.2
+      x_scale = 0.80, -- looks good-ish
+      y_scale = 0.80, -- looks good-ish
+      only_in_alt_mode = true,
+    }
+  end
+
+  storage.entries[train.id] = entry
+end
+
+-- our code cannot handle multiple sprites, so pick 1
+function bubble.pick_one_what_from_shipment(shipment)
+  for what, count in pairs(shipment) do
+    -- ^ == "item,se-iridium-blastcake"
+    return what
+  end
+
+  return nil
+end
+
+-- unwrap front & back movers into one
+function bubble.get_locomotives(train)
+  local locomotives = {}
+
+  for front_or_back, array in pairs(train.locomotives) do
+    for _, locomotive in ipairs(array) do
+      table.insert(locomotives, locomotive)
+    end
+  end
+
+  return locomotives
+end
+
+function bubble.train_is_approaching_station(train, station_backer_name)
+  -- can't approach without a schedule
+  if not train.schedule then return false end
+
+  -- require more than 2 stops (depot + temp + station)
+  if #train.schedule.records < 3 then return false end
+
+  -- is there still a (temporary) stop between the train and this station?
+  for i = train.schedule.current + 1, #train.schedule.records do
+    if train.schedule.records[i].station then
+      if train.schedule.records[i].station == station_backer_name then
+        return true
+      end
+    end
+  end
+
+  -- currently at this station, or already went past it
+  return false
+end
+
+function bubble.gc()
+  local valid_train_ids = {}
+
+  for _, train in pairs(game.train_manager.get_trains({})) do
+    valid_train_ids[train.id] = true
+  end
+
+  for train_id, entry in pairs(storage.entries) do
+    if not valid_train_ids[train_id] or not entry.train.valid then
+
+      for _, sprite in ipairs(storage.entries[train_id].sprites) do
+        sprite.destroy()
+      end
+
+      storage.entries[train_id] = nil
+
+    end
+  end
+end
+
+return bubble
