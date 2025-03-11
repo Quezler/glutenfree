0c06da971f3a942e0228b46c9dff03f31e230ea8
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-03-11 16:15:22 +0100

    Import prior work

---
+++ mods_2.0/070_hurricane-sandbox/data.lua
@@ -0,0 +1,10 @@
+require("shared")
+
+local Hurricane = require("graphics/hurricane")
+local skin = Hurricane.crafter({
+  name = "radio-station",
+  width = 1280, height = 870,
+  total_frames = 20, rows = 3, -- custom
+  shadow_width = 400, shadow_height = 350,
+  shift = {0, 0.25}, -- custom
+})
