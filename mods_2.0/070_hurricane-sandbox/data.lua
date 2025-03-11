fe90e4756188f8e1b90fb04dd0df37b71581772b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-03-11 18:02:29 +0100

    List all assets and manually map them

--- mods_2.0/070_hurricane-sandbox/data.lua
+++ mods_2.0/070_hurricane-sandbox/data.lua
@@ -2,6 +2,7 @@ require("shared")
 
 local Hurricane = require("graphics/hurricane")
 local skin = Hurricane.crafter({
+  directory = mod_directory .. "factorio-sprites",
   name = "radio-station",
   width = 1280, height = 870,
   total_frames = 20, rows = 3, -- custom
---
+++ mods_2.0/070_hurricane-sandbox/factorio-sprites.php
@@ -0,0 +1,46 @@
+<?php
+
+// (cd mods_2.0/070_hurricane-sandbox && composer install)
+// (cd mods_2.0/070_hurricane-sandbox && php factorio-sprites.php)
+require __DIR__ . '/vendor/autoload.php';
+
+function check_directory($directory): void
+{
+    $finder = new \Symfony\Component\Finder\Finder();
+    foreach ($finder->in($directory)->directories()->depth(0)->sortByName() as $folder) {
+        if (!str_starts_with($folder->getFilename(), '_')) {
+//            dump($folder->getRealPath());
+            echo $folder->getBasename() . "\n";
+        }
+    }
+}
+
+//check_directory(__DIR__ . '/factorio-sprites/_deprecated');
+check_directory(__DIR__ . '/factorio-sprites');
+
+// todo: imagecreatefrompng() & imagecolorat()
+const figma = [
+    "alloy-forge"           => [ "8x8" , 120],
+    "arc-furnace"           => [ "5x5" ,  60],
+    "atom-forge"            => [ "6x6" ,  80],
+    "chemical-stager"       => [ "5x5" ,  60],
+    "conduit"               => [ "3x3" ,  60],
+    "core-extractor"        => ["11x11", 120],
+    "electricity-extractor" => [ "8x8" ,   1],
+    "fluid-extractor"       => [ "8x8" ,   1],
+    "fusion-reactor"        => [ "6x6" ,  60],
+    "glass-furnace"         => [ "4x4" ,  80],
+    "gravity-assembler"     => [ "5x5" , 100],
+    "greenhouse"            => [ "5x5" , 128],
+    "item-extractor"        => [ "8x8" ,   1],
+    "lumber-mill"           => [ "8x8" ,  80],
+    "manufacturer"          => [ "4x4" , 128],
+    "oxidizer"              => [ "4x4" ,  60],
+    "pathogen-lab"          => [ "7x7" ,  60],
+    "photometric-lab"       => [ "5x5" ,  80],
+    "quantum-stabilizer"    => [ "6x6" , 100],
+    "radio-station"         => [ "2x2" ,  20],
+    "research-center"       => [ "9x9" ,  80],
+    "scrubber"              => [ "3x3" ,  60],
+    "thermal-plant"         => [ "6x6" ,  60],
+];
