4c6fadb7eaecc923445c5e765d1fd7df07ae803c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-03-12 12:04:49 +0100

    Have an inactive and an active row for each entity

--- mods_2.0/070_hurricane-sandbox/control.lua
+++ mods_2.0/070_hurricane-sandbox/control.lua
@@ -1,10 +1,15 @@
 require("util")
 require("shared")
 
+local highest_tile_height = 0
 local is_hurricane_entity = {}
 for _, prototype in pairs(prototypes.entity) do
   if util.string_starts_with(prototype.name, mod_prefix) then
     is_hurricane_entity[prototype.name] = true
+
+    if prototype.tile_height > highest_tile_height then
+      highest_tile_height = prototype.tile_height
+    end
   end
 end
 
@@ -16,11 +21,20 @@ script.on_init(function()
   for name, _ in pairs(is_hurricane_entity) do
     local prototype = prototypes.entity[name]
     x = x + (prototype.tile_width / 2)
-    local entity = surface.create_entity{
+
+    local entity1 = surface.create_entity{
       name = name,
       force = force,
       position = {x, -6},
     }
+    entity1.disabled_by_script = true
+
+    local entity2 = surface.create_entity{
+      name = name,
+      force = force,
+      position = {x, -6 - highest_tile_height - 1},
+    }
+
     x = x + (prototype.tile_width / 2) + 1
   end
 end)
