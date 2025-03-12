724b545a0c7d9f1c37750d4dca138e7263647ab4
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-03-12 11:28:31 +0100

    Space them out based on their width

--- mods_2.0/070_hurricane-sandbox/control.lua
+++ mods_2.0/070_hurricane-sandbox/control.lua
@@ -12,13 +12,15 @@ script.on_init(function()
   local surface = game.get_surface("nauvis") --[[@as LuaSurface]]
   local force = game.forces["player"]
 
-  local i = 0
+  local x = 3
   for name, _ in pairs(is_hurricane_entity) do
-    i = i + 1
-    surface.create_entity{
+    local prototype = prototypes.entity[name]
+    x = x + (prototype.tile_width / 2)
+    local entity = surface.create_entity{
       name = name,
       force = force,
-      position = {10 * i, 0},
+      position = {x, -6},
     }
+    x = x + (prototype.tile_width / 2) + 1
   end
 end)
