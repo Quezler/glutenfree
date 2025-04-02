97dbc2aeda6b42ec6a10c6f9b19408a008e81ab0
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-04-02 13:29:39 +0200

    Design the luagui

--- mods_2.0/073_flushable-fluid-wagons/control.lua
+++ mods_2.0/073_flushable-fluid-wagons/control.lua
@@ -1,61 +1,112 @@
-local _storage = {player_should_open = {}}
 
-local function on_configuration_changed()
-  storage.is_flushable = {}
-  for entity_name, prototype in pairs(prototypes.get_entity_filtered({{filter = "type", type = "fluid-wagon"}})) do
-    storage.is_flushable[entity_name .. "-flushable"] = true
-  end
-end
 
-script.on_init(function(event)
-  storage.structs = {}
-  storage.structs_count = 0
-  storage.tank_number_to_wagon = {}
-  on_configuration_changed()
+script.on_init(function()
+  storage.playerdata = {}
 end)
 
-script.on_configuration_changed(on_configuration_changed)
+script.on_configuration_changed(function()
+  --
+end)
 
-local function tick_struct(struct)
-  if struct.tank.valid == false then return false, "tank.valid" end
-  if struct.wagon.valid == false then return false, "wagon.valid" end
+local gui_frame_name = "flushable-fluid-wagons--frame"
 
-  if struct.player.opened == struct.wagon then return true end
-  if struct.player.opened ~= struct.tank then return false, "opened ~= tank" end
-  assert(struct.player.connected) -- can opened be non-nil if a player is offline?
+local function open_gui(player, entity)
+  local frame = player.gui.screen[gui_frame_name]
+  if frame then frame.destroy() end
 
-  local fluid_name, fluid_amount = next(struct.wagon.get_fluid_contents())
-  -- game.print(fluid_name .. fluid_amount)
-  if fluid_name then
-    struct.tank.clear_fluid_inside()
-    struct.tank.insert_fluid({name = fluid_name, amount = fluid_amount})
-  end
+  frame = player.gui.screen.add{
+    type = "frame",
+    name = gui_frame_name,
+    direction = "vertical",
+    caption = entity.prototype.localised_name,
+    tags = {
+      unit_number = entity.unit_number,
+    }
+  }
+  frame.style.minimal_width = 448
 
-  if struct.tank.position ~= struct.wagon.position then
-    struct.tank.teleport(struct.wagon.position)
+  local inner = frame.add{
+    type = "frame",
+    name = "inner",
+    style = "inside_shallow_frame_with_padding",
+    direction = "vertical",
+  }
+
+  local entity_preview = inner.add{
+    type = "entity-preview",
+    style = "wide_entity_button",
+  }
+  entity_preview.entity = entity
+
+  local label = inner.add{
+    type = "label",
+    caption = {"gui-pipe.this-contents", entity.localised_name},
+  }
+  label.style.font = "default-semibold"
+  label.style.top_margin = 5
+  label.style.bottom_margin = 5
+
+  local gui_table = inner.add{
+    type = "table",
+    column_count = 4,
+  }
+
+  local sprite_button = gui_table.add{
+    type = "sprite-button",
+    sprite = "fluid/" .. "water",
+    style = "transparent_slot",
+    elem_tooltip = {type = "fluid", name = "water"},
+  }
+
+  gui_table.add{
+    type = "label",
+    caption = "50k",
+    tooltip = string.format("%.4f", 1),
+  }
+
+  local pusher = gui_table.add{
+    type = "empty-widget",
+  }
+  pusher.style.horizontally_stretchable = true
+
+  gui_table.add{
+    type = "sprite-button",
+    sprite = "utility/trash",
+    style = "tool_button_flush_fluid",
+    tooltip = {"gui-pipe.flush-this", "water"}
+  }
+
+  player.opened = frame
+  frame.force_auto_center()
+end
+
+local function on_tick_playerdata(playerdata)
+  local player = playerdata.player
+
+  if not player.valid then return false end
+  if not player.connected then return false end
+
+  if not playerdata.entity.valid then return false end
+  if not playerdata.opened then
+    open_gui(player, playerdata.entity)
+    playerdata.opened = true
   end
 end
 
 local function on_tick(event)
-  for unit_number, struct in pairs(storage.structs) do
-    local keep, why = tick_struct(struct)
-    if keep == false then
-      game.print(event.tick .. " on_tick")
-      struct.tank.destroy()
-      storage.tank_number_to_wagon[struct.tank_unit_number] = nil
-
-      storage.structs[unit_number] = nil
-      storage.structs_count = storage.structs_count - 1
+  for player_index, playerdata in pairs(storage.playerdata) do
+    if on_tick_playerdata(playerdata) == false then
+      storage.playerdata[player_index] = nil
     end
   end
 
-  if storage.structs_count == 0 then
+  if not next(storage.playerdata) then
     script.on_event(defines.events.on_tick, nil)
   end
 end
 
 script.on_load(function(event)
-  if storage.structs_count > 0 then
+  if next(storage.playerdata) then
     script.on_event(defines.events.on_tick, on_tick)
   end
 end)
@@ -65,64 +116,38 @@ script.on_event("open-fluid-wagon", function(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
 
   local entity = player.selected
-  if entity == nil then return end
-  if entity.type ~= "fluid-wagon" then return end
+  if entity == nil or entity.type ~= "fluid-wagon" then return end
 
-  game.print(event.tick .. " open-fluid-wagon")
+  local fluid = entity.get_fluid(1)
+  if fluid == nil then return end -- empty fluid wagon (to allow access to equipment)
 
-  local fluid_name, fluid_amount = next(entity.get_fluid_contents())
-  if fluid_name == nil then return end -- empty fluid wagon (to allow access to equipment)
-
-  local tank = entity.surface.create_entity{
-    name = entity.name .. "-flushable",
-    force = entity.force,
-    position = entity.position,
-  }
-
-  assert(tank)
-  tank.destructible = false
-  tank.insert_fluid({name = fluid_name, amount = fluid_amount})
-
-  _storage.player_should_open[player.index] = tank
-  storage.tank_number_to_wagon[tank.unit_number] = entity
-
-  storage.structs[tank.unit_number] = { -- tank's unit number so multiple per wagon can exist
-    wagon_unit_number = entity.unit_number,
-    wagon = entity,
-
-    tank_unit_number = tank.unit_number,
-    tank = tank,
-
+  storage.playerdata[player.index] = {
     player = player,
+    entity = entity,
+    opened = false,
   }
 
-  storage.structs_count = storage.structs_count + 1
   script.on_event(defines.events.on_tick, on_tick)
-
-  local coin = {name = "coin", count = 1}
-  if player.get_main_inventory().insert(coin) then
-    player.get_main_inventory().remove(coin)
-  end
 end)
 
-script.on_event(defines.events.on_player_main_inventory_changed, function(event)
-  game.print(event.tick .. " on_player_main_inventory_changed")
-  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+-- script.on_event(defines.events.on_player_main_inventory_changed, function(event)
+--   game.print(event.tick .. " on_player_main_inventory_changed")
+--   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
 
-  local tank = _storage.player_should_open[player.index]
-  if tank == nil then return end
-  _storage.player_should_open[player.index] = nil
+--   local tank = _storage.player_should_open[player.index]
+--   if tank == nil then return end
+--   _storage.player_should_open[player.index] = nil
 
-  if tank.valid == false then return end
-  player.opened = tank
-end)
+--   if tank.valid == false then return end
+--   player.opened = tank
+-- end)
 
-script.on_event(defines.events.on_player_flushed_fluid, function(event)
-  if storage.is_flushable[event.entity.name] then
-    local wagon = storage.tank_number_to_wagon[event.entity.unit_number]
-    wagon.clear_fluid_inside()
-  end
-end)
+-- script.on_event(defines.events.on_player_flushed_fluid, function(event)
+--   if storage.is_flushable[event.entity.name] then
+--     local wagon = storage.tank_number_to_wagon[event.entity.unit_number]
+--     wagon.clear_fluid_inside()
+--   end
+-- end)
 
 -- /c game.print(game.player.opened.get_fluid_contents()["water"])
 -- /c game.player.opened.insert_fluid({name = "water", amount = 50000})
@@ -136,3 +161,10 @@ end)
 --     end
 --   end
 -- end)
+
+script.on_event(defines.events.on_gui_closed, function(event)
+  local element = event.element
+  if element and element.name == gui_frame_name then
+    element.destroy()
+  end
+end)
--- mods_2.0/073_flushable-fluid-wagons/data.lua
+++ mods_2.0/073_flushable-fluid-wagons/data.lua
@@ -4,6 +4,5 @@ data:extend({
     name = "open-fluid-wagon",
     key_sequence = "",
     linked_game_control = "open-gui",
-    include_selected_prototype = true,
   }
 })
