3f30e28d85b6ff89a1bf6077c223213044e64df2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-04-11 20:34:49 +0200

    Ported to 2.0

--- mods_2.0/081_glutenfree-se-rocket-launch-pad-label/control.lua
+++ mods_2.0/081_glutenfree-se-rocket-launch-pad-label/control.lua
@@ -1,4 +1,4 @@
-local handler = require('scripts.launchpad')
+local handler = require("scripts.launchpad")
 
 --
 
@@ -25,11 +25,13 @@ script.on_configuration_changed(handler.on_configuration_changed)
 for _, event in ipairs({
   defines.events.on_built_entity,
   defines.events.on_robot_built_entity,
+  defines.events.on_space_platform_built_entity,
   defines.events.script_raised_built,
   defines.events.script_raised_revive,
+  defines.events.on_entity_cloned,
 }) do
   script.on_event(event, handler.on_created_entity, {
-    {filter = 'name', name = 'se-rocket-launch-pad'},
+    {filter = "name", name = "se-rocket-launch-pad"},
   })
 end
 
--- mods_2.0/081_glutenfree-se-rocket-launch-pad-label/scripts/launchpad.lua
+++ mods_2.0/081_glutenfree-se-rocket-launch-pad-label/scripts/launchpad.lua
@@ -1,27 +1,26 @@
-local Zone = require('__space-exploration-scripts__.zone')
+local Zone = require("__space-exploration-scripts__.zone")
 
 local launchpad = {}
 
 function launchpad.init()
-  global.entries = {}
+  storage.entries = {}
 
   for _, surface in pairs(game.surfaces) do
-    for _, entity in pairs(surface.find_entities_filtered({name = 'se-rocket-launch-pad'})) do
-
+    for _, entity in pairs(surface.find_entities_filtered({name = "se-rocket-launch-pad"})) do
       launchpad.register_silo(entity)
     end
   end
 
-  global.has_opened_every_silo = false
+  storage.has_opened_every_silo = false
 end
 
 function launchpad.on_configuration_changed()
-  global.has_opened_every_silo = false
-  global.deathrattles = nil
+  storage.has_opened_every_silo = false
+  storage.deathrattles = nil
 end
 
 function launchpad.on_created_entity(event)
-  local entity = event.created_entity or event.entity or event.destination
+  local entity = event.entity or event.destination
 
   launchpad.register_silo(entity)
 
@@ -35,10 +34,12 @@ function launchpad.on_created_entity(event)
 end
 
 function launchpad.register_silo(entity)
-  global.entries[entity.unit_number] = {
+  storage.entries[entity.unit_number] = {
     unit_number = entity.unit_number,
     container = entity,
-    label = nil,
+
+    destination = nil, --[[@as LuaRenderObject]]
+    position = nil, --[[@as LuaRenderObject]]
   }
 end
 
@@ -49,7 +50,7 @@ function get_child(parent, name)
     end
   end
 
-  error('could not find a child named ['.. name ..'].')
+  error("could not find a child named [".. name .."].")
 end
 
 -- remove leading whitespace from string.
@@ -62,45 +63,49 @@ function remove_rich_text(s)
 
   -- split at each space
   for word in s:gmatch("%S+") do
-    if word:sub(1, 1) == '[' or word:sub(-1, -1) == ']' then
+    if word:sub(1, 1) == "[" or word:sub(-1, -1) == "]" then
       -- ignore
     else
       table.insert(words, word)
     end
   end
 
-  return table.concat(words, ' ')
+  return table.concat(words, " ")
 end
 
 function launchpad.on_gui_opened(event)
   if not event.entity then return end
-  if event.entity.name ~= 'se-rocket-launch-pad' then return end
+  if event.entity.name ~= "se-rocket-launch-pad" then return end
 
-  local player = game.get_player(event.player_index)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
 
   -- after an update, have a player open every silo so we can get all fresh values
-  if not global.has_opened_every_silo then
-    global.has_opened_every_silo = true
+  if not storage.has_opened_every_silo then
+    storage.has_opened_every_silo = true
 
     local shifu = player.opened
     player.opened = nil -- or else it fails to open this silo if its either the only or first entry
-    for _, entry in pairs(global.entries) do
+    for _, entry in pairs(storage.entries) do
       if entry.container.valid then
+        player.centered_on = entry.container
         player.opened = entry.container
       else
-        global.entries[_] = nil
+        storage.entries[_] = nil
       end
     end
     player.opened = shifu
+    if player.opened_gui_type == defines.gui_type.entity then
+      player.centered_on = player.opened --[[@as LuaEntity]]
+    end
   else
-    local container = get_child(player.gui.relative, 'se-rocket-launch-pad-gui')
+    local container = get_child(player.gui.relative, "se-rocket-launch-pad-gui")
     local launchpad_gui_frame = container.children[2] -- style = inside_shallow_frame
-    local launchpad_gui_inner = get_child(launchpad_gui_frame, 'launchpad_gui_inner')
+    local launchpad_gui_inner = get_child(launchpad_gui_frame, "launchpad_gui_inner")
 
-    local zones_dropdown = get_child(launchpad_gui_inner, 'launchpad-list-zones')
+    local zones_dropdown = get_child(launchpad_gui_inner, "launchpad-list-zones")
     local destination_text = launchpad.get_destination(zones_dropdown)
 
-    local landingpads_dropdown = get_child(launchpad_gui_inner, 'launchpad-list-landing-pad-names')
+    local landingpads_dropdown = get_child(launchpad_gui_inner, "launchpad-list-landing-pad-names")
     local position_text = launchpad.get_position(landingpads_dropdown)
 
     launchpad.update_by_unit_number(event.entity.unit_number, destination_text or "Any landing pad with name", position_text or "None - General vicinity")
@@ -108,16 +113,16 @@ function launchpad.on_gui_opened(event)
 end
 
 function launchpad.on_gui_selection_state_changed(event)
-  if event.element.name == 'launchpad-list-zones' then
+  if event.element.name == "launchpad-list-zones" then
     local unit_number = event.element.parent.parent.parent.tags.unit_number
-    if not unit_number then error('could not get this silo\'s unit number.') end
+    if not unit_number then error("could not get this silo\"s unit number.") end
 
     launchpad.update_by_unit_number(unit_number, launchpad.get_destination(event.element) or "Any landing pad with name", "None - General vicinity")
   end
 
-  if event.element.name == 'launchpad-list-landing-pad-names' then
+  if event.element.name == "launchpad-list-landing-pad-names" then
     local unit_number = event.element.parent.parent.parent.tags.unit_number
-    if not unit_number then error('could not get this silo\'s unit number.') end
+    if not unit_number then error("could not get this silo\"s unit number.") end
 
     launchpad.update_by_unit_number(unit_number, nil, launchpad.get_position(event.element) or "None - General vicinity")
   end
@@ -150,41 +155,39 @@ function launchpad.get_position(landingpads_dropdown)
 end
 
 function launchpad.update_by_unit_number(unit_number, destination, position)
-  local entry = global.entries[unit_number]
+  local entry = storage.entries[unit_number]
 
   if destination then
-    if entry.destination_id == nil then
-      entry.destination_id = rendering.draw_text{
+    if entry.destination == nil then
+      entry.destination = rendering.draw_text{
         text = destination,
         color = {1, 1, 1, 1},
         surface = entry.container.surface,
         position = entry.container.position,
-        target = entry.container,
-        target_offset = {0, 1.55},
-        alignment = 'center',
+        target = {entity = entry.container, offset = {0, 1.55}},
+        alignment = "center",
         use_rich_text = true,
         -- scale_with_zoom = true,
       }
     else
-      rendering.set_text(entry.destination_id, destination)
+      entry.destination.text = destination
     end
   end
 
   if position then
-    if entry.position_id == nil then
-      entry.position_id = rendering.draw_text{
+    if entry.position == nil then
+      entry.position = rendering.draw_text{
         text = position,
         color = {1, 1, 1, 1},
         surface = entry.container.surface,
         position = entry.container.position,
-        target = entry.container,
-        target_offset = {0, 2.25},
-        alignment = 'center',
+        target = {entity = entry.container, offset = {0, 2.25}},
+        alignment = "center",
         use_rich_text = true,
         -- scale_with_zoom = true,
       }
     else
-      rendering.set_text(entry.position_id, position)
+      entry.position.text = position
     end
   end
 end
