5ef62ebfc54ba3a36713a738a46c1db0bff9328c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-04-16 19:04:22 +0200

    Replace hardcoded directions with defines (Resolves #34)

--- mods_2.0/068_glutenfree-ltn-announcer/scripts/ltn.lua
+++ mods_2.0/068_glutenfree-ltn-announcer/scripts/ltn.lua
@@ -1,22 +1,22 @@
 local ltn = {}
 
 function ltn.search_area(entity)
-  if entity.direction == 0 then --SN
+  if entity.direction == defines.direction.north then --SN
     return {
       {entity.position.x + 0.001 - 1, entity.position.y + 0.001 - 1},
       {entity.position.x - 0.001 + 1, entity.position.y - 0.001 + 1},
     }
-  elseif entity.direction == 2 then --WE
+  elseif entity.direction == defines.direction.east then --WE
     return {
       {entity.position.x + 0.001 - 1, entity.position.y + 0.001 - 1},
       {entity.position.x - 0.001 + 1, entity.position.y - 0.001 + 1},
     }
-  elseif entity.direction == 4 then --NS
+  elseif entity.direction == defines.direction.south then --NS
     return {
       {entity.position.x + 0.001 - 1, entity.position.y + 0.001 - 1},
       {entity.position.x - 0.001 + 1, entity.position.y - 0.001 + 1},
     }
-  elseif entity.direction == 6 then --EW
+  elseif entity.direction == defines.direction.west then --EW
     return {
       {entity.position.x + 0.001 - 1, entity.position.y + 0.001 - 1},
       {entity.position.x - 0.001 + 1, entity.position.y - 0.001 + 1},
@@ -27,13 +27,13 @@ function ltn.search_area(entity)
 end
 
 function ltn.pos_for_speaker(entity)
-  if entity.direction == 0 then --SN
+  if entity.direction == defines.direction.north then --SN
     return {entity.position.x - 0, entity.position.y - 0}
-  elseif entity.direction == 2 then --WE
+  elseif entity.direction == defines.direction.east then --WE
     return {entity.position.x - 1, entity.position.y - 0}
-  elseif entity.direction == 4 then --NS
+  elseif entity.direction == defines.direction.south then --NS
     return {entity.position.x - 1, entity.position.y - 1}
-  elseif entity.direction == 6 then --EW
+  elseif entity.direction == defines.direction.west then --EW
     return {entity.position.x - 0, entity.position.y - 1}
   else
     error('direction: '.. entity.direction)
