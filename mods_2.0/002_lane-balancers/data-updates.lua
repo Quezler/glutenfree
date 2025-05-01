4c2be8d67c12a5b24fff55ed030de4b79be2b775
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-05-01 13:39:32 +0200

    Extend prismatic belt support to Krastorio 2

--- mods_2.0/002_lane-balancers/data-updates.lua
+++ mods_2.0/002_lane-balancers/data-updates.lua
@@ -1,8 +1,11 @@
+-- prismatic belt support
 for _, config in ipairs({
   {prefix = ''},
   {prefix = 'fast-'},
   {prefix = 'express-'},
   {prefix = 'turbo-'},
+  {prefix = 'kr-advanced-'},
+  {prefix = 'kr-superior-'},
 }) do
   local balancer = data.raw["lane-splitter"][config.prefix .. "lane-splitter"]
   local splitter = data.raw[     "splitter"][config.prefix ..      "splitter"]
