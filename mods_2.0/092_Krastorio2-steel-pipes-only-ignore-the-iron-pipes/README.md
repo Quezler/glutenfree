b2513280a456ed4d67eada2e322a1f2f9593ae71
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-05-02 12:50:37 +0200

    Cleanup

--- mods_2.0/092_Krastorio2-steel-pipes-only-ignore-the-iron-pipes/README.md
+++ mods_2.0/092_Krastorio2-steel-pipes-only-ignore-the-iron-pipes/README.md
@@ -1,2 +1,6 @@
 The theme of steel pipes is that they have a higher pressure and therefore need pumps before changing pipes,
 whilst honestly 95% of players would likely just prefer them to not connect to normal pipes to run them alongside.
+
+Underground pipes will be allowed to connect to pipes of the other type since they are directional, which makes it intentional.
+
+Due to unavoidable code similarities the `data-final-fixes.lua` file is to be considered `GNU LGPLv3`, the rest is `MIT`.
--- mods_2.0/092_Krastorio2-steel-pipes-only-ignore-the-iron-pipes/data.lua
+++ mods_2.0/092_Krastorio2-steel-pipes-only-ignore-the-iron-pipes/data.lua
@@ -1,12 +1,11 @@
-assert(data.raw["pipe"]["kr-steel-pipe"], 'No mod enabled that adds ["pipe"]["kr-steel-pipe"].')
-
 require("util")
 
+-- global table shared between all mods for compat
 names_ignored_by_steel_pipes = util.list_to_map({
   "pipe",
---"pipe-to-ground", -- since these are directional, lets allow them?
 })
 
+assert(data.raw["pipe"]["kr-steel-pipe"], 'No mod enabled that adds ["pipe"]["kr-steel-pipe"].')
 data.raw["pipe-to-ground"]["kr-steel-pipe-to-ground"].fluid_box.pipe_connections[1].connection_category = "kr-steel-pipe"
 
 for _, pipe_connection in ipairs(data.raw["pipe-to-ground"]["kr-steel-pipe-to-ground"].fluid_box.pipe_connections) do
