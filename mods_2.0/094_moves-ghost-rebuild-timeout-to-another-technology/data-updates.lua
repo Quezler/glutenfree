a73b1e3cef93bd2306fc9982b979ddfea19b7081
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-05-06 16:38:08 +0200

    Ported to 2.0

--- mods_2.0/094_moves-ghost-rebuild-timeout-to-another-technology/data-updates.lua
+++ mods_2.0/094_moves-ghost-rebuild-timeout-to-another-technology/data-updates.lua
@@ -9,9 +9,9 @@ local function cut_effect(type, technology)
   error(string.format('Technology "%s" does not have an effect of type "%s".', technology.name, type))
 end
 
-local ghost_rebuild = cut_effect('ghost-time-to-live', data.raw['technology']['construction-robotics'])
-local to_technology = data.raw['technology'][settings.startup['move-ghost-rebuild-timeout-to'].value]
-if to_technology == nil then to_technology = data.raw['technology']['military'] end
+local ghost_rebuild = cut_effect("create-ghost-on-entity-death", data.raw["technology"]["construction-robotics"])
+local to_technology = data.raw["technology"][settings.startup["move-ghost-rebuild-timeout-to"].value]
+if to_technology == nil then to_technology = data.raw["technology"]["military"] end
 if to_technology == nil then error('Fallback technology "military" not found.') end
 
 table.insert(to_technology.effects, 1, ghost_rebuild)
