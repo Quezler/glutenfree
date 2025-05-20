6b6d7989d92807ec5730f5336a8c9d1706cd5b37
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-05-20 19:44:45 +0200

    Revert "Allow configuring transparency at startup"
    This reverts commit 0bb04a70206c3b7a5fff57d6eed0e375e4749b5b.
    
    # Conflicts:
    #	mods_2.0/089_elevated-pipes/changelog.txt

--- mods_2.0/089_elevated-pipes/prototypes/sprite.lua
+++ mods_2.0/089_elevated-pipes/prototypes/sprite.lua
@@ -10,7 +10,6 @@ local function sprite_with_shadow(name, config)
         scale = 0.5,
         width = 704,
         height = 704,
-        tint = config.tint,
       },
     }
   }
@@ -28,11 +27,8 @@ local function sprite_with_shadow(name, config)
   return sprite
 end
 
-local opacity = settings.startup[mod_prefix .. "opacity"].value -- 1
-local tint = {opacity, opacity, opacity, opacity}
-
-local horizontal_shadow = {shadow = "elevated-pipe-horizontal-center-shadow", tint = tint}
-local vertical_shadow = {shadow = "elevated-pipe-vertical-center-shadow", tint = tint}
+local horizontal_shadow = {shadow = "elevated-pipe-horizontal-center-shadow"}
+local vertical_shadow = {shadow = "elevated-pipe-vertical-center-shadow"}
 
 data:extend{
   sprite_with_shadow("elevated-pipe-horizontal-left", horizontal_shadow),
--- mods_2.0/089_elevated-pipes/settings.lua
+++ mods_2.0/089_elevated-pipes/settings.lua
@@ -2,20 +2,10 @@ require("shared")
 
 data:extend({
   {
-    order = "a",
     type = "int-setting",
     name = mod_prefix .. "max-underground-distance",
     setting_type = "startup",
-    minimum_value = 2,
     default_value = 10,
-  },
-    {
-    order = "b",
-    type = "double-setting",
-    name = mod_prefix .. "opacity",
-    setting_type = "startup",
-    minimum_value = 0,
-    default_value = 1,
-    maximum_value = 1,
+    minimum_value = 2,
   },
 })
