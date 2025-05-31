c70b6802001ad958e3ed642aaf5818cba641daa4
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-05-31 16:24:26 +0200

    Make it more obvious the player has to stand on the rails

--- mods_2.0/100_Trolley_Problem/control.lua
+++ mods_2.0/100_Trolley_Problem/control.lua
@@ -1,42 +1,42 @@
 script.on_event(defines.events.on_lua_shortcut, function(event)
   if event.prototype_name == "trolley-problem" then
     local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+
+    local track_below_player = get_track_below_player(player)
+    if not track_below_player then
+      return player.create_local_flying_text{text = "[item=rail] ?", position = player.position}
+    end
+
     local called = 0
-
     for _, train in pairs(game.train_manager.get_trains{surface = player.surface, force = player.force}) do
-
       if train_matches_player(train, player) then
-        if has_track_below_player(player)  then
+        local schedule = {}
+        schedule.current = 1
+        schedule.records = {}
 
-          local schedule = {}
-          schedule.current = 1
-          schedule.records = {}
+        if train.schedule then
+          schedule = train.schedule
+          assert(schedule)
 
-          if train.schedule then
-            schedule = train.schedule
-            assert(schedule)
-
-            -- remove any existing temporary station @ top
-            if(schedule.records[1].temporary == true) then
-              table.remove(schedule.records, 1)
-            end
+          -- remove any existing temporary station @ top
+          if(schedule.records[1].temporary == true) then
+            table.remove(schedule.records, 1)
           end
-
-          local record = {}
-          record.temporary = true
-          record.rail = get_track_below_player(player)
-          record.wait_conditions = {
-            {compare_type="or", type = "inactivity", ticks = 60 * 15},
-            {compare_type="or", type = "inactivity", ticks = 60 *  5},
-            {compare_type="and", type = "passenger_present"},
-          }
-
-          table.insert(schedule.records, 1, record)
-          train.schedule = schedule
-          train.go_to_station(1)
-          called = called + 1
         end
 
+        local record = {}
+        record.temporary = true
+        record.rail = get_track_below_player(player)
+        record.wait_conditions = {
+          {compare_type="or", type = "inactivity", ticks = 60 * 15},
+          {compare_type="or", type = "inactivity", ticks = 60 *  5},
+          {compare_type="and", type = "passenger_present"},
+        }
+
+        table.insert(schedule.records, 1, record)
+        train.schedule = schedule
+        train.go_to_station(1)
+        called = called + 1
       end
     end
 
@@ -48,8 +48,8 @@ end
 function train_matches_player(train, player)
   for _, movers in pairs(train.locomotives) do
     for _, locomotive in pairs(movers) do
-      if(locomotive.color) then -- locomotive color isn't required
-        if(identical_color(player.color, locomotive.color)) then
+      if locomotive.color  then -- locomotive color isn't required
+        if identical_color(player.color, locomotive.color) then
           return true
         end
       end
@@ -59,13 +59,11 @@ function train_matches_player(train, player)
 end
 
 function identical_color(color1, color2)
+  -- log(serpent.line({color1, color2}))
+  -- log(serpent.line{color1.r == color2.r, color1.g == color2.g, color1.b == color2.b})
   return color1.r == color2.r and color1.g == color2.g and color1.b == color2.b
 end
 
 function get_track_below_player(player)
-  return player.surface.find_entity("straight-rail", player.position) or player.surface.find_entity("curved-rail-a", player.position) or player.surface.find_entity("curved-rail-b", player.position)
-end
-
-function has_track_below_player(player)
-  return get_track_below_player(player) and true
+  return player.surface.find_entity("straight-rail", player.position) or player.surface.find_entity("curved-rail-a", player.position) or player.surface.find_entity("curved-rail-b", player.position) or player.surface.find_entity("half-diagonal-rail", player.position)
 end
