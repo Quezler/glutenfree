e91f809c6ce786c14fa6ce2837c07db0995b406f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-06 18:51:22 +0200

    Make the factory index guesser more elaborate

--- mods_2.0/069_magic-huts/scripts/buildings.lua
+++ mods_2.0/069_magic-huts/scripts/buildings.lua
@@ -1,17 +1,40 @@
 local Buildings = {}
 
+local function get_factory_index(event)
+  local entity = event.entity or event.destination
+
+  if event.player_index and storage.playerdata[event.player_index] then
+    local held_factory_index = storage.playerdata[event.player_index].held_factory_index
+    if held_factory_index then -- if nil, go for checking the ghost tags
+      return held_factory_index
+    end
+  end
+
+  if event.tags and event.tags[mod_prefix .. "factory-index"] then
+    return event.tags[mod_prefix .. "factory-index"]
+  end
+
+  if entity.last_user and storage.playerdata[entity.last_user.index] then
+    return storage.playerdata[entity.last_user.index].held_factory_index
+  end
+
+  return nil
+end
+
 Buildings.on_created_entity = function(event)
   local entity = event.entity or event.destination
-  local playerdata = storage.playerdata[entity.last_user.index]
+  local factory_index = get_factory_index(event)
 
   if entity.type == "entity-ghost" then
-    local tags = entity.tags or {}
-    tags[mod_prefix .. "factory-index"] = playerdata.held_factory_index
-    entity.tags = tags
+    if factory_index then
+      local tags = entity.tags or {}
+      tags[mod_prefix .. "factory-index"] = factory_index
+      entity.tags = tags
+    end
     return
   end
 
-  game.print(playerdata.held_factory_index or (event.tags and event.tags[mod_prefix .. "factory-index"]))
+  game.print(factory_index)
 end
 
 return Buildings
