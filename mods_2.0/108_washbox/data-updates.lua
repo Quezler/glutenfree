6b5932b11ec0ac9ee89b335ae9499f1a07258f80
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-17 13:11:30 +0200

    Add the k2 pollution filter washing recipe

---
+++ mods_2.0/108_washbox/data-updates.lua
@@ -0,0 +1,27 @@
+if data.raw["item"]["kr-used-pollution-filter"] and data.raw["item"]["kr-pollution-filter"] and data.raw["technology"]["kr-air-purification"] then
+  data:extend{{
+    type = "recipe",
+    name = "washbox--kr-restore-used-pollution-filter",
+    localised_name = {"recipe-name.kr-restore-used-pollution-filter"},
+    icon = "__Krastorio2Assets__/icons/recipes/restore-used-pollution-filter.png",
+    icon_size = 128,
+
+    enabled = false,
+    category = "washbox",
+    energy_required = 10,
+
+    ingredients = {
+      { type = "item", name = "kr-used-pollution-filter", amount = 1 },
+      { type = "fluid", name = "water", amount = 50 },
+    },
+
+    results = {
+      { type = "item", name = "kr-pollution-filter", amount = 1 },
+      { type = "fluid", name = "water", amount = 25 },
+    },
+  }}
+
+  table.insert(data.raw["technology"]["kr-air-purification"].effects, {
+    type = "unlock-recipe", recipe = "washbox--kr-restore-used-pollution-filter",
+  })
+end
