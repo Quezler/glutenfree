383295c54e49e5f95a34346eb08991da80a37ac4
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-19 22:50:23 +0200

    Without any feature flags the animation is invisible
    And added the missing thumbnail.

--- mods_2.0/109_space-platform-entity-build-animation-lib/data-updates.lua
+++
@@ -1,9 +0,0 @@
-local function any_feature_flag_enabled()
-  for feature_flag, boolean in pairs(feature_flags) do
-    if boolean then return true end
-  end
-end
-
-if not any_feature_flag_enabled() then
-  error("at least one feature flag must be enabled.")
-end
--- mods_2.0/109_space-platform-entity-build-animation-lib/data.lua
+++ mods_2.0/109_space-platform-entity-build-animation-lib/data.lua
@@ -1,11 +1,24 @@
 require("shared")
 
+local function any_feature_flag_enabled()
+  for feature_flag, boolean in pairs(feature_flags) do
+    if boolean then return true end
+  end
+end
+
+local is_any_feature_flag_enabled = any_feature_flag_enabled()
+
 local space_platform_entity_build_animations = require(mod_directory .. "/graphics/entity/space-platform-build-anim/entity-build-animations")
 
 for corner, animations in pairs(space_platform_entity_build_animations) do
   for name, animation in pairs(animations) do
     animation.type = "animation"
     animation.name = string.format(mod_prefix .. "%s-%s", corner, name)
+    if not is_any_feature_flag_enabled then
+      for _, layer in ipairs(animation.layers) do
+        layer.tint = {0, 0, 0, 0}
+      end
+    end
     data:extend{animation}
   end
 end
