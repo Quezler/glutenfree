47eb9ca19f4ae6271bc2e7f87e61fa7cb04f7d8c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-25 10:20:17 +0200

    Ported to 2.0

--- mods_2.0/116_se-signal-transmitter-new-channel-name-suggester/control.lua
+++ mods_2.0/116_se-signal-transmitter-new-channel-name-suggester/control.lua
@@ -1,21 +1,20 @@
-local Zone = require('__space-exploration-scripts__.zone')
+local Zone = require("__space-exploration-scripts__.zone")
 
-local mod_prefix = "" -- should be `aai-` for versions after v0.4.9
 local default_channel = "Default"
 
 script.on_init(function(event)
-  global.player_keystrokes_since_editing_channel = {}
+  storage.player_keystrokes_since_editing_channel = {}
 end)
 
 script.on_configuration_changed(function(event)
-  global.player_keystrokes_since_editing_channel = global.player_keystrokes_since_editing_channel or {}
+  storage.player_keystrokes_since_editing_channel = storage.player_keystrokes_since_editing_channel or {}
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
-  if event.element and event.element.name == mod_prefix .. "change_channel" then
-    global.player_keystrokes_since_editing_channel[event.player_index] = 0
+  if event.element and event.element.name == "aai-change_channel" then
+    storage.player_keystrokes_since_editing_channel[event.player_index] = 0
     if event.element.parent.children[1].caption == default_channel then
-      local player = game.get_player(event.player_index)
+      local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
       local coin = {name = "coin", count = 1}
       if player.get_main_inventory().insert(coin) then
         player.get_main_inventory().remove(coin)
@@ -25,7 +24,7 @@ script.on_event(defines.events.on_gui_click, function(event)
 end)
 
 script.on_event(defines.events.on_player_main_inventory_changed, function(event)
-  local player = game.get_player(event.player_index)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
   local opened = player.opened
 
   if opened and opened.object_name == "LuaGuiElement" and opened.name == "aai-signal-sender" then
@@ -42,15 +41,15 @@ script.on_event(defines.events.on_player_main_inventory_changed, function(event)
 end)
 
 script.on_event(defines.events.on_gui_text_changed, function(event)
-  if event.element and event.element.name == mod_prefix .. "write-channel" then
-    global.player_keystrokes_since_editing_channel[event.player_index] = global.player_keystrokes_since_editing_channel[event.player_index] + 1
+  if event.element and event.element.name == "aai-write-channel" then
+    storage.player_keystrokes_since_editing_channel[event.player_index] = storage.player_keystrokes_since_editing_channel[event.player_index] + 1
 
-    if event.element.text == "" and global.player_keystrokes_since_editing_channel[event.player_index] == 1 then
-      local player = game.get_player(event.player_index)
-      
+    if event.element.text == "" and storage.player_keystrokes_since_editing_channel[event.player_index] == 1 then
+      local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
+
       local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = player.surface.index})
       if not zone then return end
-  
+
       event.element.text = Zone._get_rich_text_name(zone)
       event.element.select_all()
     end
