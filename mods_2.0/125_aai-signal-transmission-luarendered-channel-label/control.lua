f9d9f15f0029eb3b77365664e4a4942c58fc1988
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-25 11:35:30 +0200

    Ported to 2.0

--- mods_2.0/125_aai-signal-transmission-luarendered-channel-label/control.lua
+++ mods_2.0/125_aai-signal-transmission-luarendered-channel-label/control.lua
@@ -1,8 +1,8 @@
-local Util = require('__aai-signal-transmission__.scripts.util')
+local Util = require("__aai-signal-transmission__.scripts.util")
 
 function on_created_entity(event)
-  local entity = event.created_entity or event.entity or event.destination
-  global.structs[entity.unit_number] = {
+  local entity = event.entity or event.destination
+  storage.structs[entity.unit_number] = {
     unit_number = entity.unit_number,
     entity = entity,
     opened = false,
@@ -19,10 +19,10 @@ function on_created_entity(event)
 end
 
 script.on_init(function(event)
-  global.structs = {}
+  storage.structs = {}
 
   for _, surface in pairs(game.surfaces) do
-    for _, entity in pairs(surface.find_entities_filtered({name = {'aai-signal-sender', 'aai-signal-receiver'}})) do
+    for _, entity in pairs(surface.find_entities_filtered({name = {"aai-signal-sender", "aai-signal-receiver"}})) do
       on_created_entity({entity = entity})
     end
   end
@@ -31,27 +31,28 @@ end)
 for _, event in ipairs({
   defines.events.on_built_entity,
   defines.events.on_robot_built_entity,
+  defines.events.on_space_platform_built_entity,
   defines.events.script_raised_built,
   defines.events.script_raised_revive,
   defines.events.on_entity_cloned,
 }) do
   script.on_event(event, on_created_entity, {
-    {filter = 'name', name = 'aai-signal-sender'},
-    {filter = 'name', name = 'aai-signal-receiver'},
+    {filter = "name", name = "aai-signal-sender"},
+    {filter = "name", name = "aai-signal-receiver"},
   })
 end
 
 function update_text(unit_number, channel)
-  local struct = global.structs[unit_number]
+  local struct = storage.structs[unit_number]
   if not struct then return end
 
   if Util.string_trim(channel) == "" then channel = "Default" end
 
   struct.opened = true
-  -- if struct.id then rendering.destroy(id) end
-  if struct.id == nil then
+  -- if struct.text then text.destroy() end
+  if struct.text == nil then
     local entity = struct.entity
-    struct.id = rendering.draw_text{
+    struct.text = rendering.draw_text{
       text = channel,
       color = {1, 1, 1},
       surface = entity.surface,
@@ -62,16 +63,16 @@ function update_text(unit_number, channel)
       use_rich_text = true,
     }
   else
-    rendering.set_text(struct.id, channel)
+    struct.text.text = channel
   end
 end
 
 script.on_event(defines.events.on_gui_opened, function(event)
-  local player = game.get_player(event.player_index)
+  local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
   local opened = player.opened
 
   if not opened then return end
-  if opened.object_name ~= "LuaGuiElement" then return end -- "LuaEquipmentGrid doesn't contain key name."
+  if opened.object_name ~= "LuaGuiElement" then return end -- "LuaEquipmentGrid doesn"t contain key name."
   if opened.name ~= "aai-signal-sender" then return end
 
   if not opened.children[2] then return end -- first time this element shows up its empty, so wait for the 2nd time.
@@ -81,11 +82,11 @@ script.on_event(defines.events.on_gui_opened, function(event)
   update_text(unit_number, channel)
 end)
 
-remote.add_interface('aai-signal-transmission-luarendered-channel-label', {update_text = update_text})
+remote.add_interface("aai-signal-transmission-luarendered-channel-label", {update_text = update_text})
 
-commands.add_command('aai-signal-transmission-luarendered-channel-label', nil, function(command)
-  local player = game.get_player(command.player_index)
-  for unit_number, struct in pairs(global.structs) do
+commands.add_command("aai-signal-transmission-luarendered-channel-label", nil, function(command)
+  local player = game.get_player(command.player_index) --[[@as LuaPlayer]]
+  for unit_number, struct in pairs(storage.structs) do
     if struct.entity.valid then
       player.opened = struct.entity
     end
