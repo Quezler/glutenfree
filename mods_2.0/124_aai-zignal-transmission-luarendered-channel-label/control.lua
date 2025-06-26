7abeec672865049f322314eb9e5c924531827a3d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-26 11:52:34 +0200

    Listen to the new gui element prefixes

--- mods_2.0/124_aai-zignal-transmission-luarendered-channel-label/control.lua
+++ mods_2.0/124_aai-zignal-transmission-luarendered-channel-label/control.lua
@@ -1,5 +1,5 @@
 script.on_event(defines.events.on_gui_selection_state_changed, function(event)
-  if event.element.name == "change_channel_dropdown" then
+  if event.element.name == "aai-change_channel_dropdown" then -- dropdown opened or updated
     local channel = event.element.items[event.element.selected_index]
     local unit_number = tonumber(event.element.parent.parent.parent.children[1].children[1].name)
     remote.call("aai-signal-transmission-luarendered-channel-label", "update_text", unit_number, channel)
@@ -7,7 +7,7 @@ script.on_event(defines.events.on_gui_selection_state_changed, function(event)
 end)
 
 script.on_event(defines.events.on_gui_confirmed, function(event)
-  if event.element.name == "write-channel" then -- enter pressed
+  if event.element.name == "aai-write-channel" then -- enter pressed
     local channel = event.element.text
     local unit_number = tonumber(event.element.parent.parent.parent.parent.children[1].children[1].name)
     remote.call("aai-signal-transmission-luarendered-channel-label", "update_text", unit_number, channel)
@@ -15,7 +15,7 @@ script.on_event(defines.events.on_gui_confirmed, function(event)
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
-  if event.element.name == "change-channel-confirm" then -- apply clicked
+  if event.element.name == "aai-change-channel-confirm" then -- apply clicked
     local channel = event.element.parent.children[1].text
     local unit_number = tonumber(event.element.parent.parent.parent.parent.children[1].children[1].name)
     remote.call("aai-signal-transmission-luarendered-channel-label", "update_text", unit_number, channel)
