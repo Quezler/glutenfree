cc71a3c0db42f20193445ef087b10acabb375fa5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-06-27 16:25:48 +0200

    Attempt to rework mod to use on_object_destroyed

--- mods_2.0/125_aai-signal-transmission-luarendered-channel-label/control.lua
+++ mods_2.0/125_aai-signal-transmission-luarendered-channel-label/control.lua
@@ -1,4 +1,5 @@
 local Util = require("__aai-signal-transmission__.scripts.util")
+require("scripts.luagui-pretty-print")
 
 function try_open_gui_with_someone(entity)
   for _, player in ipairs(entity.force.connected_players) do
@@ -13,7 +14,6 @@ end
 function on_created_entity(event)
   local entity = event.entity or event.destination
   storage.structs[entity.unit_number] = {
-    unit_number = entity.unit_number,
     entity = entity,
     text = nil,
   }
@@ -21,14 +21,19 @@ function on_created_entity(event)
   try_open_gui_with_someone(entity)
 end
 
-script.on_init(function(event)
+script.on_init(function()
   storage.structs = {}
+  storage.deathrattles = {}
 
   for _, surface in pairs(game.surfaces) do
     for _, entity in pairs(surface.find_entities_filtered({name = {"aai-signal-sender", "aai-signal-receiver"}})) do
       on_created_entity({entity = entity})
     end
   end
+end)
+
+script.on_configuration_changed(function()
+  storage.deathrattles = storage.deathrattles or {}
 end)
 
 for _, event in ipairs({
@@ -59,8 +64,7 @@ function update_text(unit_number, channel)
       color = {1, 1, 1},
       surface = entity.surface,
       position = entity.position,
-      target = entity,
-      target_offset = {0, -1.5},
+      target = {entity = entity, offset = {0, -1.5}},
       alignment = "center",
       use_rich_text = true,
     }
@@ -69,17 +73,25 @@ function update_text(unit_number, channel)
   end
 end
 
+local function raise_hover_events(root)
+  local aai_change_channel = root.children[2].children[2].children[1]["aai-change_channel"]
+  aai_change_channel.raise_hover_events = true
+end
+
 script.on_event(defines.events.on_gui_opened, function(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
   local opened = player.opened
 
   if not opened then return end
-  if opened.object_name ~= "LuaGuiElement" then return end -- "LuaEquipmentGrid doesn"t contain key name."
+  if opened.object_name ~= "LuaGuiElement" then return end -- "LuaEquipmentGrid doesn't contain key name."
   if opened.name ~= "aai-signal-sender" then return end
 
   if not opened.children[2] then return end -- first time this element shows up its empty, so wait for the 2nd time.
   local unit_number = tonumber(opened.children[1].children[1].name)
   local channel = opened.children[2].children[2].children[1].children[1].caption
+
+  local root = player.gui.screen["aai-signal-sender"]
+  raise_hover_events(root)
 
   update_text(unit_number, channel)
 end)
@@ -99,5 +111,50 @@ end)
 script.on_event(defines.events.on_entity_settings_pasted, function (event)
   if event.destination.name == "aai-signal-sender" or event.destination.name == "aai-signal-receiver" then
     try_open_gui_with_someone(event.destination)
+  end
+end)
+
+script.on_event(defines.events.on_gui_click, function(event)
+  log(LuaGuiPrettyPrint.path_to_element(event.element))
+end)
+
+script.on_event(defines.events.on_gui_selection_state_changed, function(event)
+  game.print(event.element.name)
+  if event.element.name == "aai-change_channel_dropdown" then -- dropdown opened or updated
+    local channel = event.element.items[event.element.selected_index]
+    local unit_number = tonumber(event.element.parent.parent.parent.children[1].children[1].name)
+    remote.call("aai-signal-transmission-luarendered-channel-label", "update_text", unit_number, channel)
+  end
+end)
+
+script.on_event(defines.events.on_gui_hover, function(event)
+  if event.element.name == "aai-change_channel" then
+    local root = event.element.parent.parent.parent.parent --[[@as LuaGuiElement]]
+    assert(root.name)
+    storage.deathrattles[script.register_on_object_destroyed(event.element)] = {
+      name = "aai-change_channel",
+      root = root,
+    }
+  end
+end)
+
+script.on_event(defines.events.on_object_destroyed, function(event)
+  local deathrattle = storage.deathrattles[event.registration_number]
+  if deathrattle then storage.deathrattles[event.registration_number] = nil
+    if deathrattle.name == "aai-change_channel" and deathrattle.root.valid then
+      local aai_change_channel_confirm = deathrattle.root.children[2].children[2].children[1]["aai-change-channel-confirm"]
+      if aai_change_channel_confirm then
+        storage.deathrattles[script.register_on_object_destroyed(aai_change_channel_confirm)] = {
+          name = "aai-change-channel-confirm",
+          root = deathrattle.root,
+        }
+      end
+    elseif deathrattle.name == "aai-change-channel-confirm" and deathrattle.root.valid then
+      local name = deathrattle.root.children[2].children[2].children[1]["show-channel"]
+      local unit_number = tonumber(deathrattle.root["unit_number"].children[1].name)
+      update_text(assert(unit_number), assert(name.caption))
+      raise_hover_events(deathrattle.root) -- in case the user wants to edit the channel again without closing the gui first
+    end
+    log(serpent.block(deathrattle))
   end
 end)
---
+++ mods_2.0/125_aai-signal-transmission-luarendered-channel-label/scripts/luagui-pretty-print.lua
@@ -0,0 +1,75 @@
+LuaGuiPrettyPrint = {}
+
+function LuaGuiPrettyPrint.path_to_caption(element, locale_key, breadcrumbs)
+  if element.caption then
+    if element.caption[1] == locale_key then return breadcrumbs end
+    if element.caption[1] == "" then -- glued together, check the first
+      if element.caption[2][1] == locale_key then return breadcrumbs end
+    end
+  end
+
+  for i, child in ipairs(element.children) do
+    local breadcrumb = string.format(".children[%d]", i)
+    if child.name ~= "" then breadcrumb = string.format("[%s]", child.name) end
+
+    local path = LuaGuiPrettyPrint.path_to_caption(child, locale_key, breadcrumbs .. breadcrumb)
+    if path then return path end
+  end
+end
+
+function LuaGuiPrettyPrint.path_to_tag(element, key, value, breadcrumbs)
+  if element.tags then
+    if element.tags[key] == value then return breadcrumbs end
+  end
+
+  for i, child in ipairs(element.children) do
+    local breadcrumb = string.format(".children[%d]", i)
+    if child.name ~= "" then breadcrumb = string.format("[%s]", child.name) end
+
+    local path = LuaGuiPrettyPrint.path_to_tag(child, key, value, breadcrumbs .. breadcrumb)
+    if path then return path end
+  end
+end
+
+function LuaGuiPrettyPrint.dump(element, silent)
+  local node = {
+    type = element.type,
+    name = element.name,
+    style = element.style and element.style.name,
+    caption = element.caption,
+    children_count = #element.children,
+  }
+
+  if element.tooltip then
+    node.tooltip = element.tooltip
+  end
+
+  node.children = {}
+  for _, child in ipairs(element.children) do
+    table.insert(node.children, LuaGuiPrettyPrint.dump(child, true))
+  end
+
+  if not silent then
+    log(serpent.block(node))
+  end
+
+  return node
+end
+
+function LuaGuiPrettyPrint.path_to_element(element, breadcrumbs)
+  breadcrumbs = breadcrumbs or ""
+
+  local breadcrumb = nil
+  if element.name ~= "" then
+    breadcrumb = "[\"" .. element.name .. "\"]"
+  else
+    breadcrumb = ".children[" .. element.get_index_in_parent() .. "]"
+  end
+
+  local parent = element.parent
+  if parent then
+    return LuaGuiPrettyPrint.path_to_element(parent, breadcrumb .. breadcrumbs)
+  else
+    return "player.gui." .. element.name .. breadcrumbs
+  end
+end
