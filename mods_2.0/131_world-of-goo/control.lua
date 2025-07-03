4c2054b229f2016a5282ad580e5202a37075894f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-03 18:41:26 +0200

    Use the og sprites

--- mods_2.0/131_world-of-goo/control.lua
+++ mods_2.0/131_world-of-goo/control.lua
@@ -1,15 +1,82 @@
+local function get_random_eye_name()
+  -- return "generic-eye-glass-2"
+  return "generic-eye-glass-" .. tostring(math.random(1, 3))
+end
+
 commands.add_command("goo-balls", nil, function(command)
   local player = game.get_player(command.player_index) --[[@as LuaPlayer]]
   local fishes = player.surface.find_entities_filtered{type = "fish"}
   game.print(#fishes)
 
   for _, fish in ipairs(fishes) do
+    -- rendering.draw_sprite{
+    --   surface = fish.surface,
+    --   target = fish,
+    --   sprite = "goo-ball",
+      -- use_target_orientation = true,
+      -- orientation_target = fish,
+    -- }
+
     rendering.draw_sprite{
       surface = fish.surface,
-      target = fish,
-      sprite = "goo-ball",
+      target = {entity = fish},
+      sprite = "common-body",
       use_target_orientation = true,
       orientation_target = fish,
+    }
+
+    -- rendering.draw_sprite{
+    --   surface = fish.surface,
+    --   target = {entity = fish, offset = {0, -0.2}},
+    --   sprite = get_random_eye_name(),
+    --   use_target_orientation = true,
+    --   orientation_target = fish,
+    --   oriented_offset = {-0.1, 0},
+    -- }
+
+    -- rendering.draw_sprite{
+    --   surface = fish.surface,
+    --   target = {entity = fish, offset = {0, -0.2}},
+    --   sprite = get_random_eye_name(),
+    --   use_target_orientation = true,
+    --   orientation_target = fish,
+    --   oriented_offset = { 0.1, 0},
+    -- }
+
+    rendering.draw_sprite{
+      surface = fish.surface,
+      target = {entity = fish},
+      sprite = get_random_eye_name(),
+      use_target_orientation = true,
+      orientation_target = fish,
+      oriented_offset = {-0.15, -0.05},
+    }
+
+    rendering.draw_sprite{
+      surface = fish.surface,
+      target = {entity = fish},
+      sprite = get_random_eye_name(),
+      use_target_orientation = true,
+      orientation_target = fish,
+      oriented_offset = { 0.15, -0.05},
+    }
+
+    rendering.draw_sprite{
+      surface = fish.surface,
+      target = {entity = fish},
+      sprite = "generic-pupil",
+      use_target_orientation = true,
+      orientation_target = fish,
+      oriented_offset = {-0.15, -0.07},
+    }
+
+    rendering.draw_sprite{
+      surface = fish.surface,
+      target = {entity = fish},
+      sprite = "generic-pupil",
+      use_target_orientation = true,
+      orientation_target = fish,
+      oriented_offset = { 0.15, -0.07},
     }
   end
 end)
--- mods_2.0/131_world-of-goo/data.lua
+++ mods_2.0/131_world-of-goo/data.lua
@@ -6,3 +6,48 @@ data:extend{{
   height = 171,
   scale = 0.25,
 }}
+
+data:extend{{
+  type = "sprite",
+  name = "common-body",
+  filename = "__world-of-goo__/graphics/balls/common/body.png",
+  width = 64,
+  height = 64,
+  scale = 0.25,
+}}
+
+data:extend{{
+  type = "sprite",
+  name = "generic-eye-glass-1",
+  filename = "__world-of-goo__/graphics/balls/_generic/eye_glass_1.png",
+  width = 32,
+  height = 32,
+  scale = 0.25,
+}}
+
+data:extend{{
+  type = "sprite",
+  name = "generic-eye-glass-2",
+  filename = "__world-of-goo__/graphics/balls/_generic/eye_glass_2.png",
+  width = 23,
+  height = 23,
+  scale = 0.25,
+}}
+
+data:extend{{
+  type = "sprite",
+  name = "generic-eye-glass-3",
+  filename = "__world-of-goo__/graphics/balls/_generic/eye_glass_3.png",
+  width = 16,
+  height = 16,
+  scale = 0.25,
+}}
+
+data:extend{{
+  type = "sprite",
+  name = "generic-pupil",
+  filename = "__world-of-goo__/graphics/balls/_generic/pupil1.png",
+  width = 8,
+  height = 8,
+  scale = 0.25,
+}}
