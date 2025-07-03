a3261572e4f06fb140cf96d8b3cddc5adc2145e2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-03 22:17:25 +0200

    use our own entity and item

--- mods_2.0/131_world-of-goo/control.lua
+++ mods_2.0/131_world-of-goo/control.lua
@@ -3,80 +3,71 @@ local function get_random_eye_name()
   return "generic-eye-glass-" .. tostring(math.random(1, 3))
 end
 
+local function render_goo_ball(fish)
+  rendering.draw_sprite{
+    surface = fish.surface,
+    target = {entity = fish},
+    sprite = "common-body",
+  }
+
+  rendering.draw_sprite{
+    surface = fish.surface,
+    target = {entity = fish},
+    sprite = get_random_eye_name(),
+    use_target_orientation = true,
+    orientation_target = fish,
+    oriented_offset = {-0.15, -0.05},
+  }
+
+  rendering.draw_sprite{
+    surface = fish.surface,
+    target = {entity = fish},
+    sprite = get_random_eye_name(),
+    use_target_orientation = true,
+    orientation_target = fish,
+    oriented_offset = { 0.15, -0.05},
+  }
+
+  rendering.draw_sprite{
+    surface = fish.surface,
+    target = {entity = fish},
+    sprite = "generic-pupil",
+    use_target_orientation = true,
+    orientation_target = fish,
+    oriented_offset = {-0.15, -0.07},
+  }
+
+  rendering.draw_sprite{
+    surface = fish.surface,
+    target = {entity = fish},
+    sprite = "generic-pupil",
+    use_target_orientation = true,
+    orientation_target = fish,
+    oriented_offset = { 0.15, -0.07},
+  }
+end
+
 commands.add_command("goo-balls", nil, function(command)
   local player = game.get_player(command.player_index) --[[@as LuaPlayer]]
   local fishes = player.surface.find_entities_filtered{type = "fish"}
   game.print(#fishes)
 
   for _, fish in ipairs(fishes) do
-    -- rendering.draw_sprite{
-    --   surface = fish.surface,
-    --   target = fish,
-    --   sprite = "goo-ball",
-      -- use_target_orientation = true,
-      -- orientation_target = fish,
-    -- }
+    render_goo_ball(fish)
+  end
+end)
 
-    rendering.draw_sprite{
-      surface = fish.surface,
-      target = {entity = fish},
-      sprite = "common-body",
-      use_target_orientation = true,
-      orientation_target = fish,
-    }
+-- does not trigger for the spawning pool, caught by on_init instead, weird right?
+script.on_event(defines.events.on_script_trigger_effect, function(event)
+  if event.effect_id ~= "goo-ball-created" then return end
 
-    -- rendering.draw_sprite{
-    --   surface = fish.surface,
-    --   target = {entity = fish, offset = {0, -0.2}},
-    --   sprite = get_random_eye_name(),
-    --   use_target_orientation = true,
-    --   orientation_target = fish,
-    --   oriented_offset = {-0.1, 0},
-    -- }
+  render_goo_ball(event.source_entity)
+end)
 
-    -- rendering.draw_sprite{
-    --   surface = fish.surface,
-    --   target = {entity = fish, offset = {0, -0.2}},
-    --   sprite = get_random_eye_name(),
-    --   use_target_orientation = true,
-    --   orientation_target = fish,
-    --   oriented_offset = { 0.1, 0},
-    -- }
-
-    rendering.draw_sprite{
-      surface = fish.surface,
-      target = {entity = fish},
-      sprite = get_random_eye_name(),
-      use_target_orientation = true,
-      orientation_target = fish,
-      oriented_offset = {-0.15, -0.05},
-    }
-
-    rendering.draw_sprite{
-      surface = fish.surface,
-      target = {entity = fish},
-      sprite = get_random_eye_name(),
-      use_target_orientation = true,
-      orientation_target = fish,
-      oriented_offset = { 0.15, -0.05},
-    }
-
-    rendering.draw_sprite{
-      surface = fish.surface,
-      target = {entity = fish},
-      sprite = "generic-pupil",
-      use_target_orientation = true,
-      orientation_target = fish,
-      oriented_offset = {-0.15, -0.07},
-    }
-
-    rendering.draw_sprite{
-      surface = fish.surface,
-      target = {entity = fish},
-      sprite = "generic-pupil",
-      use_target_orientation = true,
-      orientation_target = fish,
-      oriented_offset = { 0.15, -0.07},
-    }
+script.on_init(function()
+  for _, surface in pairs(game.surfaces) do
+    for _, entity in ipairs(surface.find_entities_filtered{name = "goo-ball"}) do
+      render_goo_ball(entity) -- probably only nauvis
+    end
   end
 end)
--- mods_2.0/131_world-of-goo/data.lua
+++ mods_2.0/131_world-of-goo/data.lua
@@ -1,5 +1,8 @@
 require("namespace")
 
+local sounds = require("__base__.prototypes.entity.sounds")
+local item_sounds = require("__base__.prototypes.item_sounds")
+
 data:extend{{
   type = "sprite",
   name = "goo-ball",
@@ -67,3 +70,60 @@ data.raw["tile"]["deepwater"].variants.main[3].picture = mod_directory .. "/grap
 data.raw["tile"]["deepwater"].effect_color = {0.1, 0.1, 0.1, 1}
 data.raw["tile"]["deepwater"].effect_color_secondary = {0.1, 0.1, 0.1, 1}
 data.raw["tile"]["deepwater"].map_color = {0.1, 0.1, 0.1, 1}
+
+local fish = {
+  type = "fish",
+  name = "goo-ball",
+  icon = mod_directory .. "/graphics/balls/common/body.png",
+  flags = {"placeable-neutral", "not-on-map"},
+  minable = {mining_time = 0.25, result = "goo-ball", count = 1},
+  mined_sound = sounds.mine_fish,
+  max_health = 25,
+  subgroup = "creatures",
+  order = "b-b",
+  -- factoriopedia_simulation = simulations.factoriopedia_fish,
+  collision_box = {{-1.0, -1.0}, {1.0, 1.0}},
+  selection_box = {{-0.4, -0.4}, {0.4, 0.4}},
+  pictures =
+  {
+    {
+      filename = mod_directory .. "/graphics/balls/common/body.png",
+      priority = "extra-high",
+      width = 64,
+      height = 64,
+      scale = 0.2, -- 0.5 smaller so its behind the lua rendering
+    }
+  },
+  autoplace = { probability_expression = 0.1 },
+  protected_from_tile_building = false,
+
+  created_effect = {
+    type = "direct",
+    action_delivery = {
+      type = "instant",
+      source_effects = {
+        {
+          type = "script",
+          effect_id = "goo-ball-created",
+        },
+      }
+    }
+  },
+}
+
+local item = {
+  type = "item",
+  name = "goo-ball",
+  icon = mod_directory .. "/graphics/balls/common/body.png",
+  subgroup = "raw-resource",
+  order = "i[goo-ball]",
+  inventory_move_sound = item_sounds.raw_fish_inventory_move,
+  pick_sound = item_sounds.raw_fish_inventory_pickup,
+  drop_sound = item_sounds.raw_fish_inventory_move,
+  stack_size = 10,
+}
+
+data:extend{fish, item}
+
+data.raw["planet"]["nauvis"].map_gen_settings.autoplace_settings.entity.settings["goo-ball"] = {}
+data.raw["planet"]["nauvis"].map_gen_settings.autoplace_settings.entity.settings["fish"] = nil
