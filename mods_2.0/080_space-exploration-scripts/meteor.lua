e8253d05b275cb40ad6d80c637000d8b8d8322d9
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-04 13:42:21 +0200

    Port meteor.lua

--- mods_2.0/080_space-exploration-scripts/meteor.lua
+++ mods_2.0/080_space-exploration-scripts/meteor.lua
@@ -1,9 +1,9 @@
 -- step 1
-mod_prefix = 'se-'
+mod_prefix = "se-"
 Event = {addListener = function() end}
 
 -- step 2
-local Meteor = require('__space-exploration__.scripts.meteor')
+local Meteor = require("__space-exploration__.scripts.meteor")
 
 -- step 3
 Event = nil
@@ -11,4 +11,5 @@ mod_prefix = nil
 
 -- step 4
 
+--- @since 2.5.0
 return Meteor
