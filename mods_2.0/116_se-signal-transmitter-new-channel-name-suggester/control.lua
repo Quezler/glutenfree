270b3e0e731fbdf063b8a67684c9ced047d7cc0d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-04 14:00:16 +0200

    Fix crash when configuring with remote view

--- mods_2.0/116_se-signal-transmitter-new-channel-name-suggester/control.lua
+++ mods_2.0/116_se-signal-transmitter-new-channel-name-suggester/control.lua
@@ -4,26 +4,28 @@ local default_channel = "Default"
 
 script.on_init(function(event)
   storage.player_keystrokes_since_editing_channel = {}
+  storage.deathrattles = {}
 end)
 
 script.on_configuration_changed(function(event)
   storage.player_keystrokes_since_editing_channel = storage.player_keystrokes_since_editing_channel or {}
+  storage.deathrattles = storage.deathrattles or {}
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
   if event.element and event.element.name == "aai-change_channel" then
     storage.player_keystrokes_since_editing_channel[event.player_index] = 0
     if event.element.parent.children[1].caption == default_channel then
-      local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
-      local coin = {name = "coin", count = 1}
-      if player.get_main_inventory().insert(coin) then
-        player.get_main_inventory().remove(coin)
-      end
+      local inventory = game.create_inventory(1)
+      local stack = inventory[1]
+      stack.set_stack({name = "blueprint"})
+      storage.deathrattles[script.register_on_object_destroyed(stack.item)] = {player_index = event.player_index}
+      inventory.destroy()
     end
   end
 end)
 
-script.on_event(defines.events.on_player_main_inventory_changed, function(event)
+local function on_player_main_inventory_changed(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
   local opened = player.opened
 
@@ -38,7 +40,7 @@ script.on_event(defines.events.on_player_main_inventory_changed, function(event)
     write_channel.text = Zone._get_rich_text_name(zone)
     write_channel.select_all()
   end
-end)
+end
 
 script.on_event(defines.events.on_gui_text_changed, function(event)
   if event.element and event.element.name == "aai-write-channel" then
@@ -53,5 +55,12 @@ script.on_event(defines.events.on_gui_text_changed, function(event)
       event.element.text = Zone._get_rich_text_name(zone)
       event.element.select_all()
     end
+  end
+end)
+
+script.on_event(defines.events.on_object_destroyed, function(event)
+  local deathrattle = storage.deathrattles[event.registration_number]
+  if deathrattle then storage.deathrattles[event.registration_number] = nil
+    on_player_main_inventory_changed({player_index = deathrattle.player_index})
   end
 end)
