fa19fea710320fa2f73ba16197003d8aac5f1b43
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-05 08:04:03 +0200

    Starmap icon

--- mods_2.0/131_world-of-goo/data-final-fixes.lua
+++ mods_2.0/131_world-of-goo/data-final-fixes.lua
@@ -1,23 +1,3 @@
 local planet = data.raw["planet"]["world-of-goo"]
-local distance = planet.distance * 32 * 8
-local orientation = planet.orientation
 
-local function get_position()
-  return {distance * math.sin(orientation * 2 * math.pi), -distance * math.cos(orientation * 2 * math.pi)}
-end
-
-data:extend{{
-  type = "space-location",
-  name = "world-of-goo-base",
-  icon = "__core__/graphics/empty.png",
-  icon_size = 1,
-
-  starmap_icon = "blep",
-  starmap_icons = {
-    {icon = "__core__/graphics/empty.png", icon_size = 1},
-    {icon = mod_directory .. "/graphics/balls/common/body.png", shift = get_position(), floating = true},
-  },
-
-  distance = 0,
-  orientation = 0.5,
-}}
+planet.orientation = 0.5
--- mods_2.0/131_world-of-goo/prototypes/planet.lua
+++ mods_2.0/131_world-of-goo/prototypes/planet.lua
@@ -6,6 +6,15 @@ data:extend{{
   distance = 15,
   orientation = 0.32,
 
-  starmap_icon = "__core__/graphics/empty.png",
-  starmap_icon_size = 1,
+  starmap_icon = "blep",
+  starmap_icons = {
+    {icon = mod_directory .. "/graphics/balls/common/body.png", size = 64, scale = 1},
+
+    {icon = mod_directory .. "/graphics/balls/_generic/eye_glass_2.png", icon_size = 23, shift = {-0.50 * 32, -0.10 * 32}, scale = 1, floating = true},
+    {icon = mod_directory .. "/graphics/balls/_generic/pupil1.png"     , icon_size =  8, shift = {-0.45 * 32, -0.15 * 32}, scale = 1, floating = true},
+    {icon = mod_directory .. "/graphics/balls/_generic/eye_glass_1.png", icon_size = 32, shift = { 0.30 * 32, -0.30 * 32}, scale = 1, floating = true},
+    {icon = mod_directory .. "/graphics/balls/_generic/pupil1.png"     , icon_size =  8, shift = { 0.35 * 32, -0.35 * 32}, scale = 1, floating = true},
+
+    {icon = mod_directory .. "/graphics/telescope-19x19.png", icon_size = 19, shift = { 0.7 * 32, -1.2 * 32}, scale = 1.5, floating = true},
+  }
 }}
