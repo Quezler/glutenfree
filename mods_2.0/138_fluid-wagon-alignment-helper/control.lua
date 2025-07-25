accb6f914414cd8303f77d96150cb1f18a6aaa18
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-25 14:54:01 +0200

    Use config based rendering

--- mods_2.0/138_fluid-wagon-alignment-helper/control.lua
+++ mods_2.0/138_fluid-wagon-alignment-helper/control.lua
@@ -14,32 +14,23 @@ script.on_event(defines.events.on_player_driving_changed_state, function(event)
 
     sprites_for_player = {}
     for i = 1, tank_count * 2 do
-      local left = rendering.draw_sprite{
-        surface = surface,
-        sprite = "utility/indication_arrow",
-        tint = {1, 1, 1, 0.5},
-        orientation = 0.75, -- left
-        target = {entity = entity},
-        players = {player},
-        only_in_alt_mode = true,
-        orientation_target = {entity = entity},
-        use_target_orientation = true,
-        oriented_offset = {-0.7, i - tank_count - 0.5},
-      }
-      local right = rendering.draw_sprite{
-        surface = surface,
-        sprite = "utility/indication_arrow",
-        tint = {1, 1, 1, 0.5},
-        orientation = 0.25, -- right
-        target = {entity = entity},
-        players = {player},
-        only_in_alt_mode = true,
-        orientation_target = {entity = entity},
-        use_target_orientation = true,
-        oriented_offset = { 0.7, i - tank_count - 0.5},
-      }
-      table.insert(sprites_for_player, left)
-      table.insert(sprites_for_player, right)
+      for _, config in ipairs({
+        {name = "left", orientation = 0.75, oriented_offset_x = -0.7},
+        {name = "right", orientation = 0.25, oriented_offset_x = 0.7},
+      }) do
+        table.insert(sprites_for_player, rendering.draw_sprite{
+          surface = surface,
+          sprite = "utility/indication_arrow",
+          tint = {1, 1, 1, 0.5},
+          orientation = config.orientation,
+          target = {entity = entity},
+          players = {player},
+          only_in_alt_mode = true,
+          orientation_target = {entity = entity},
+          use_target_orientation = true,
+          oriented_offset = {config.oriented_offset_x, i - tank_count - 0.5},
+        })
+      end
     end
     storage.sprites_for_player[player.index] = sprites_for_player
   else
