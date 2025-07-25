9659bea2bd1299b074fd244840d94a8f12a4cc1b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-25 14:38:22 +0200

    Clear renderings when the exiting vehicle

--- mods_2.0/138_fluid-wagon-alignment-helper/control.lua
+++ mods_2.0/138_fluid-wagon-alignment-helper/control.lua
@@ -1,15 +1,20 @@
 require("namespace")
 
+script.on_init(function()
+  storage.sprites_for_player = {}
+end)
+
 script.on_event(defines.events.on_player_driving_changed_state, function(event)
   local player = game.get_player(event.player_index) --[[@as LuaPlayer]]
-  local entity = event.entity
+  local entity = player.vehicle
 
   if entity and entity.type == "fluid-wagon" then
     local tank_count = prototypes.mod_data[mod_prefix .. "tank-count"].get(event.entity.name) or 3
     local surface = entity.surface
 
+    sprites_for_player = {}
     for i = 1, tank_count * 2 do
-      rendering.draw_sprite{
+      local left = rendering.draw_sprite{
         surface = surface,
         sprite = "utility/indication_arrow",
         tint = {1, 1, 1, 0.5},
@@ -18,7 +23,7 @@ script.on_event(defines.events.on_player_driving_changed_state, function(event)
         players = {player},
         only_in_alt_mode = true,
       }
-      rendering.draw_sprite{
+      local right = rendering.draw_sprite{
         surface = surface,
         sprite = "utility/indication_arrow",
         tint = {1, 1, 1, 0.5},
@@ -27,6 +32,16 @@ script.on_event(defines.events.on_player_driving_changed_state, function(event)
         players = {player},
         only_in_alt_mode = true,
       }
+      table.insert(sprites_for_player, left)
+      table.insert(sprites_for_player, right)
+    end
+    storage.sprites_for_player[player.index] = sprites_for_player
+  else
+    local sprites_for_player = storage.sprites_for_player[player.index]
+    if sprites_for_player then storage.sprites_for_player[player.index] = nil
+      for _, sprite in ipairs(sprites_for_player) do
+        sprite.destroy()
+      end
     end
   end
 end)
