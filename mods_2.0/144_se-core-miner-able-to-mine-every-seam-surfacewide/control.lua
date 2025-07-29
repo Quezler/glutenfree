dcda2af33f11d25f2446bb82bb07fbe27f116a96
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-07-29 15:24:02 +0200

    Figure out the secret sauce

--- mods_2.0/144_se-core-miner-able-to-mine-every-seam-surfacewide/control.lua
+++ mods_2.0/144_se-core-miner-able-to-mine-every-seam-surfacewide/control.lua
@@ -5,13 +5,6 @@ mod.get_core_seams_for_radius = function(radius)
   return 5 + math.floor(95 * radius / 10000)
 end
 
---- @return string?
-mod.get_core_fragment_name = function(zone)
-  if not (zone.type == "planet" or zone.type == "moon") then return end -- Zone.is_solid(zone)
-  if zone.fragment_name then return zone.fragment_name end
-  return "se-core-fragment-" .. zone.primary_resource
-end
-
 mod.get_core_fragment_mining_time = function(fragment_name)
   return prototypes.entity[fragment_name].mineable_properties.mining_time
 end
@@ -26,8 +19,8 @@ script.on_event(defines.events.on_gui_opened, function(event)
     local zone = remote.call("space-exploration", "get_zone_from_surface_index", {surface_index = entity.surface.index})
     if not zone then return end -- how did you even place this outside a zone?
 
-    local fragment_name = mod.get_core_fragment_name(zone)
-    if not fragment_name then return end -- how did you even place this outside of a planet or moon?
+    if not entity.mining_target then return end
+    local fragment_name = entity.mining_target.name
 
     local mining_productivity = 1 + entity.force.mining_drill_productivity_bonus
     local core_miners = mod.get_core_seams_for_radius(zone.radius)
@@ -38,5 +31,16 @@ script.on_event(defines.events.on_gui_opened, function(event)
       surface_output = mod.get_core_fragments_per_second(fragment_name, zone.radius, mining_productivity, 1) * core_miners
       log(surface_output) -- core miners on every seam without diminishing returns
     end
+  end
+end)
+
+commands.add_command("se-core-miner-set-output", nil, function(command)
+  local player = game.get_player(command.player_index) --[[@as LuaPlayer]]
+  local entity = player.selected
+
+  if entity and entity.name == "se-core-miner-drill" then
+    local fragment_name = entity.mining_target.name
+    local fragment_mining_time = mod.get_core_fragment_mining_time(fragment_name)
+    entity.mining_target.amount = 10000 * fragment_mining_time * (tonumber(command.parameter) or 1) -- 1/s
   end
 end)
