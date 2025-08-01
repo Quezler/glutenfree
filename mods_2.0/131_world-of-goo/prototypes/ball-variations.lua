96305fe1d89c7530ecc658d32c136276cea329b5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-08-01 16:42:53 +0200

    Generate 16 ball variants

--- mods_2.0/131_world-of-goo/prototypes/ball-variations.lua
+++ mods_2.0/131_world-of-goo/prototypes/ball-variations.lua
@@ -1,18 +1,26 @@
 local variations = {}
 
+local function math_random(low, high)
+  return low + math.random() * (high - low)
+end
+
 for i = 1, 16 do
   local scale = 0.5
   local multiplier = 32 * scale
 
-  local body_scale = math.random(7, 10) / 10
+  local body_scale = math_random(0.7, 1.0)
+
+  local left_eye_shift = {-math_random(0.30, 0.50) * multiplier, -math_random(0.10, 0.30) * multiplier}
+  local right_eye_shift = {math_random(0.30, 0.50) * multiplier, -math_random(0.20, 0.40) * multiplier}
+
   variations[i] = {
     {icon = "__core__/graphics/empty.png", icon_size = 64, scale = scale},
     {icon = mod_directory .. "/graphics/balls/common-body.png", icon_size = 64, scale = scale * body_scale},
 
-    {icon = mod_directory .. "/graphics/balls/generic-eye-2.png", icon_size = 23, shift = {-0.50 * multiplier, -0.10 * multiplier}, scale = scale, floating = true},
-    {icon = mod_directory .. "/graphics/balls/generic-pupil.png", icon_size =  8, shift = {-0.45 * multiplier, -0.15 * multiplier}, scale = scale, floating = true},
-    {icon = mod_directory .. "/graphics/balls/generic-eye-1.png", icon_size = 32, shift = { 0.30 * multiplier, -0.30 * multiplier}, scale = scale, floating = true},
-    {icon = mod_directory .. "/graphics/balls/generic-pupil.png", icon_size =  8, shift = { 0.35 * multiplier, -0.35 * multiplier}, scale = scale, floating = true},
+    {icon = mod_directory .. "/graphics/balls/generic-eye-2.png", icon_size = 23, shift = left_eye_shift, scale = scale, floating = true},
+    {icon = mod_directory .. "/graphics/balls/generic-pupil.png", icon_size =  8, shift = left_eye_shift, scale = scale, floating = true},
+    {icon = mod_directory .. "/graphics/balls/generic-eye-1.png", icon_size = 32, shift = right_eye_shift, scale = scale, floating = true},
+    {icon = mod_directory .. "/graphics/balls/generic-pupil.png", icon_size =  8, shift = right_eye_shift, scale = scale, floating = true},
   }
 end
 
