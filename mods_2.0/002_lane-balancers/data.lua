6f9c6182b986adc8354a6d90abe05faa46b4afd8
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-08-04 15:24:59 +0200

    Quote conversion, advanced belts sa override & new thumbnail

--- mods_2.0/002_lane-balancers/data.lua
+++ mods_2.0/002_lane-balancers/data.lua
@@ -1,5 +1,5 @@
-local yellow_item = data.raw['item']['lane-splitter']
-local yellow_entity = data.raw['lane-splitter']['lane-splitter']
+local yellow_item = data.raw["item"]["lane-splitter"]
+local yellow_entity = data.raw["lane-splitter"]["lane-splitter"]
 
 local function update_icon(prototype, prefix)
   prototype.icons =
@@ -13,46 +13,46 @@ end
 yellow_item.hidden = nil
 yellow_entity.hidden = nil
 
-update_icon(yellow_item, '')
-update_icon(yellow_entity, '')
+update_icon(yellow_item, "")
+update_icon(yellow_entity, "")
 
-yellow_item.order = "d[lane-splitter]-a[" .. '' .. "lane-splitter]"
+yellow_item.order = "d[lane-splitter]-a[" .. "" .. "lane-splitter]"
 yellow_item.subgroup = "belt"
 
-yellow_item.weight = data.raw['item']['' .. 'splitter'].weight
+yellow_item.weight = data.raw["item"]["" .. "splitter"].weight
 if yellow_item.weight then yellow_item.weight = yellow_item.weight / 2 end
 
-yellow_item.stack_size = data.raw['item']['' .. 'splitter'].stack_size * 2
+yellow_item.stack_size = data.raw["item"]["" .. "splitter"].stack_size * 2
 
 local function create_recipe(config)
-  local splitter = data.raw['recipe'][config.prefix .. 'splitter']
+  local splitter = data.raw["recipe"][config.prefix .. "splitter"]
   local balancer = table.deepcopy(splitter)
 
-  balancer.name = config.prefix .. 'lane-splitter'
-  balancer.main_product = config.prefix .. 'lane-splitter'
+  balancer.name = config.prefix .. "lane-splitter"
+  balancer.main_product = config.prefix .. "lane-splitter"
 
   for _, ingredient in ipairs(balancer.ingredients) do
-    if ingredient.name == config.previous_prefix .. 'splitter' then
-      ingredient.name = config.previous_prefix .. 'lane-splitter'
+    if ingredient.name == config.previous_prefix .. "splitter" then
+      ingredient.name = config.previous_prefix .. "lane-splitter"
       ingredient.amount = 2
     end
   end
 
-  balancer.results[1].name = config.prefix .. 'lane-splitter'
+  balancer.results[1].name = config.prefix .. "lane-splitter"
   balancer.results[1].amount = 2
 
   data:extend{balancer}
 
-  table.insert(data.raw['technology'][config.tech].effects, {
+  table.insert(data.raw["technology"][config.tech].effects, {
     type = "unlock-recipe",
     recipe = balancer.name,
   })
 end
 
 create_recipe({
-  prefix = '',
-  tech = 'logistics',
-  previous_prefix = 'this value does nothing since the yellow splitter is crafted from belts',
+  prefix = "",
+  tech = "logistics",
+  previous_prefix = "this value does nothing since the yellow splitter is crafted from belts",
 })
 
 local function scale_down_sprite(sprite)
@@ -91,7 +91,7 @@ local function handle(config)
   update_icon(item, config.prefix)
   update_icon(entity, config.prefix)
 
-  local splitter = data.raw['splitter'][config.prefix .. 'splitter']
+  local splitter = data.raw["splitter"][config.prefix .. "splitter"]
 
   entity.belt_animation_set = splitter.belt_animation_set
   entity.minable.result = item.name
@@ -102,17 +102,17 @@ local function handle(config)
   apply_splitter_texture_to_balancer(splitter, entity)
   create_recipe(config)
 
-  item.weight = data.raw['item'][config.prefix .. 'splitter'].weight
+  item.weight = data.raw["item"][config.prefix .. "splitter"].weight
   if item.weight then item.weight = item.weight / 2 end
 
-  item.stack_size = data.raw['item'][config.prefix .. 'splitter'].stack_size * 2
+  item.stack_size = data.raw["item"][config.prefix .. "splitter"].stack_size * 2
 
   data:extend{item, entity}
 
-  -- log(entity.name .. ' is an upgrade for ' .. entity_handled_last.name)
+  -- log(entity.name .. " is an upgrade for " .. entity_handled_last.name)
   entity.next_upgrade = nil -- so turbo doesn't upgrade to red
   if entity_handled_last then
-    -- log(entity_handled_last.name .. ' will upgrade to ' .. entity.name)
+    -- log(entity_handled_last.name .. " will upgrade to " .. entity.name)
     entity_handled_last.next_upgrade = entity.name
   end
   entity_handled_last = entity
@@ -122,71 +122,91 @@ end
 lane_balancers_handle = handle
 
 handle({
-  prefix = 'fast-',
-  tech = 'logistics-2',
-  previous_prefix = '',
-  order = 'b',
+  prefix = "fast-",
+  tech = "logistics-2",
+  previous_prefix = "",
+  order = "b",
 })
 
 handle({
-  prefix = 'express-',
-  tech = 'logistics-3',
-  previous_prefix = 'fast-',
-  order = 'c',
+  prefix = "express-",
+  tech = "logistics-3",
+  previous_prefix = "fast-",
+  order = "c",
 })
 
-if mods['space-age'] then
+if mods["space-age"] then
   handle({
-    prefix = 'turbo-',
-    tech = 'turbo-transport-belt',
-    previous_prefix = 'express-',
-    order = 'd',
+    prefix = "turbo-",
+    tech = "turbo-transport-belt",
+    previous_prefix = "express-",
+    order = "d",
   })
 
-  local previous = 'turbo-'
-  if mods['Krastorio2-spaced-out'] then
+  local previous = "turbo-"
+  if mods["Krastorio2-spaced-out"] then
     handle({
-      prefix = 'kr-superior-',
-      tech = 'kr-logistic-5',
-      previous_prefix = 'turbo-',
-      order = 'e',
+      prefix = "kr-superior-",
+      tech = "kr-logistic-5",
+      previous_prefix = "turbo-",
+      order = "e",
     })
-    previous = 'kr-superior-'
+    previous = "kr-superior-"
   end
 
-  if mods['AdvancedBeltsSA'] then
+  if mods["AdvancedBeltsSA"] then
     handle({
-      prefix = 'extreme-',
-      tech = 'extreme-logistics',
+      prefix = "extreme-",
+      tech = "extreme-logistics",
       previous_prefix = previous,
-      order = 'e',
+      order = "e",
     })
     handle({
-      prefix = 'ultimate-',
-      tech = 'ultimate-logistics',
-      previous_prefix = 'extreme-',
-      order = 'f',
+      prefix = "ultimate-",
+      tech = "ultimate-logistics",
+      previous_prefix = "extreme-",
+      order = "f",
     })
     handle({
-      prefix = 'high-speed-',
-      tech = 'high-speed-logistics',
-      previous_prefix = 'ultimate-',
-      order = 'g',
+      prefix = "high-speed-",
+      tech = "high-speed-logistics",
+      previous_prefix = "ultimate-",
+      order = "g",
     })
+
+    for technology_name, recipe_name in pairs({
+      ["extreme-logistics"] = "extreme-lanesplitter",
+      ["ultimate-logistics"] = "ultimate-lanesplitter",
+      ["high-speed-logistics"] = "high-speed-lanesplitter",
+    }) do
+      for i, effect in ipairs(data.raw["technology"][technology_name].effects) do
+        if effect.type == "unlock-recipe" and effect.recipe == recipe_name then
+          table.remove(data.raw["technology"][technology_name].effects, i)
+        end
+      end
+    end
+
+    -- if helpers.compare_versions(mods["AdvancedBeltsSA"], "2.3.2") ~= -1 then
+    --   data.raw["item"]["extreme-lanesplitter"].hidden = true
+    --   data.raw["lane-splitter"]["extreme-lanesplitter"].next_upgrade = nil
+    --   data.raw["item"]["ultimate-lanesplitter"].hidden = true
+    --   data.raw["lane-splitter"]["ultimate-lanesplitter"].next_upgrade = nil
+    --   data.raw["item"]["high-speed-lanesplitter"].hidden = true
+    -- end
   end
 end
 
-if mods['Krastorio2'] then
+if mods["Krastorio2"] then
   handle({
-    prefix = 'kr-advanced-',
-    tech = 'kr-logistic-4',
-    previous_prefix = 'express-',
-    order = 'd',
+    prefix = "kr-advanced-",
+    tech = "kr-logistic-4",
+    previous_prefix = "express-",
+    order = "d",
   })
   handle({
-    prefix = 'kr-superior-',
-    tech = 'kr-logistic-5',
-    previous_prefix = 'kr-advanced-',
-    order = 'e',
+    prefix = "kr-superior-",
+    tech = "kr-logistic-5",
+    previous_prefix = "kr-advanced-",
+    order = "e",
   })
 end
