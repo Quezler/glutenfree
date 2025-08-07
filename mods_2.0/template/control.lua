51e0548c45975106817af921719cb61cd07f3620
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-08-07 12:08:37 +0200

    Stash template changes

---
+++ mods_2.0/template/control.lua
@@ -0,0 +1,30 @@
+local mod = {}
+
+mod.on_created_entity_filters = {
+  {filter = "name", name = "entity-name"},
+}
+
+mod.on_created_entity = function(event)
+  local entity = event.entity or event.destination
+end
+
+for _, event in ipairs({
+  defines.events.on_built_entity,
+  defines.events.on_robot_built_entity,
+  defines.events.on_space_platform_built_entity,
+  defines.events.script_raised_built,
+  defines.events.script_raised_revive,
+  defines.events.on_entity_cloned,
+}) do
+  script.on_event(event, mod.on_created_entity, mod.on_created_entity_filters)
+end
+
+script.on_event(defines.events.on_object_destroyed, function(event)
+  local deathrattle = storage.deathrattles[event.registration_number]
+  if deathrattle then storage.deathrattles[event.registration_number] = nil
+    local struct = storage.structs[event.useful_id]
+    if struct then storage.structs[event.useful_id] = nil
+      --
+    end
+  end
+end)
---
+++ mods_2.0/template/scripts/surfacedata.lua
@@ -0,0 +1,21 @@
+return function(mod)
+  function mod.refresh_surfacedata()
+    -- deleted old
+    for surface_index, surfacedata in pairs(storage.surfacedata) do
+      if surfacedata.surface.valid == false then
+        storage.surfacedata[surface_index] = nil
+      end
+    end
+
+    -- created new
+    for _, surface in pairs(game.surfaces) do
+      storage.surfacedata[surface.index] = storage.surfacedata[surface.index] or {
+        surface = surface,
+        structs = {},
+      }
+    end
+  end
+
+  script.on_event(defines.events.on_surface_created, mod.refresh_surfacedata)
+  script.on_event(defines.events.on_surface_deleted, mod.refresh_surfacedata)
+end
