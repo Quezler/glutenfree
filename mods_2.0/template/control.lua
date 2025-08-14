a98a25630c186d54f32e9dac79a0b736467d1440
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-08-14 14:23:34 +0200

    Update template

--- mods_2.0/template/control.lua
+++ mods_2.0/template/control.lua
@@ -1,11 +1,34 @@
 local mod = {}
 
+script.on_init(function()
+  storage.structs = {}
+  storage.deathrattles = {}
+end)
+
+script.on_configuration_changed(function()
+  --
+end)
+
+function new_struct(table, struct)
+  assert(struct.id)
+  assert(table[struct.id] == nil)
+  table[struct.id] = struct
+  return struct
+end
+
 mod.on_created_entity_filters = {
   {filter = "name", name = "entity-name"},
 }
 
 mod.on_created_entity = function(event)
   local entity = event.entity or event.destination
+
+  local struct = new_struct(storage.structs, {
+    id = entity.unit_number,
+    entity = entity,
+  })
+
+  storage.deathrattles[script.register_on_object_destroyed(entity)] = {struct_id = struct.id}
 end
 
 for _, event in ipairs({
@@ -22,8 +45,8 @@ end
 script.on_event(defines.events.on_object_destroyed, function(event)
   local deathrattle = storage.deathrattles[event.registration_number]
   if deathrattle then storage.deathrattles[event.registration_number] = nil
-    local struct = storage.structs[event.useful_id]
-    if struct then storage.structs[event.useful_id] = nil
+    local struct = storage.structs[deathrattle.struct_id]
+    if struct then storage.structs[deathrattle.struct_id] = nil
       --
     end
   end
