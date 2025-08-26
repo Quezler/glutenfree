c33ac408a710a907d3a46d5d98254e268716627d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-08-26 16:51:13 +0200

    Allow greater control through the remote interface

--- mods_2.0/109_space-platform-entity-build-animation-lib/control.lua
+++ mods_2.0/109_space-platform-entity-build-animation-lib/control.lua
@@ -17,6 +17,35 @@ local function add_task_at_tick(tick, task)
   end
 end
 
+local function explicit_surface(surface)
+  if type(surface) == "string" then
+    return game.get_surface(surface)
+  elseif type(surface) == "number" then
+    return game.get_surface(surface)
+  else
+    return surface
+  end
+end
+
+local function explicit_position(position)
+  if position[1] then
+    position = {x = position[1], y = position[2]}
+  end
+
+  return position
+end
+
+local function explicit_area(area)
+  if area[1] then
+    area = {
+      left_top = {x = area[1][1], y = area[1][2]},
+      right_bottom = {x = area[2][1], y = area[2][2]},
+    }
+  end
+
+  return area
+end
+
 local function get_tilebox(bounding_box)
   local left_top = {x = math.floor(bounding_box.left_top.x), y = math.floor(bounding_box.left_top.y)}
   local right_bottom = {x = math.ceil(bounding_box.right_bottom.x), y = math.ceil(bounding_box.right_bottom.y)}
@@ -67,23 +96,29 @@ local TICKS_PER_FRAME = 2
 local FRAMES_BETWEEN_BUILDING = 8 * 2
 local FRAMES_BETWEEN_REMOVING = 4
 
-function request_platform_animation_for_entity(entity)
+function request_platform_animation_for_config(config)
   local tick = game.tick
-  local surface = entity.surface
+
+  local surface = explicit_surface(config.surface)
+  local position = explicit_position(config.position)
+  local top_render_layer = config.top_render_layer or "higher-object-above"
+
+  local collision_box = explicit_area(config.area)
+  local selection_box = explicit_area(config.area)
 
   surface.play_sound{
-    path = get_build_sound_path(entity.selection_box),
-    position = entity.position,
+    path = get_build_sound_path(selection_box),
+    position = position,
   }
 
-  local tilebox = get_tilebox(entity.bounding_box)
+  local tilebox = get_tilebox(collision_box)
   local largest_manhattan_distance = 0
-  for _, position in ipairs(tilebox) do
-    position.center = {x = position.x + 0.5, y = position.y + 0.5}
-    position.manhattan_distance = get_manhattan_distance(position.center, entity.position)
+  for _, tile in ipairs(tilebox) do
+    tile.center = {x = tile.x + 0.5, y = tile.y + 0.5}
+    tile.manhattan_distance = math.ceil(get_manhattan_distance(tile.center, position))
 
-    if position.manhattan_distance > largest_manhattan_distance then
-      largest_manhattan_distance = position.manhattan_distance
+    if tile.manhattan_distance > largest_manhattan_distance then
+      largest_manhattan_distance = tile.manhattan_distance
     end
   end
 
@@ -92,32 +127,32 @@ function request_platform_animation_for_entity(entity)
   local all_scaffolding_down_at = tick + 1 + largest_manhattan_distance * FRAMES_BETWEEN_REMOVING + remove_scaffold_delay + 16 * TICKS_PER_FRAME
   local all_scaffolding_up_at = tick + 1 + largest_manhattan_distance * FRAMES_BETWEEN_BUILDING + 15 * TICKS_PER_FRAME
 
-  for _, position in ipairs(tilebox) do
-    local piece = get_piece(position.center, entity.position)
+  for _, tile in ipairs(tilebox) do
+    local piece = get_piece(tile.center, position)
     local animations = {} -- {top, body}
 
-    local up_base = tick + 1 + position.manhattan_distance * FRAMES_BETWEEN_BUILDING
+    local up_base = tick + 1 + tile.manhattan_distance * FRAMES_BETWEEN_BUILDING
     add_task_at_tick(up_base + 00 * TICKS_PER_FRAME, {name = "start", animations = animations})
     add_task_at_tick(up_base + 15 * TICKS_PER_FRAME, {name = "pause", offset = 15, animations = animations})
 
-    local down_base = tick + 1 + position.manhattan_distance * FRAMES_BETWEEN_REMOVING + remove_scaffold_delay
+    local down_base = tick + 1 + tile.manhattan_distance * FRAMES_BETWEEN_REMOVING + remove_scaffold_delay
     add_task_at_tick(down_base + 00 * TICKS_PER_FRAME, {name = "unpause", offset = 16, animations = animations})
 
     local ttl = down_base - tick + 16 * TICKS_PER_FRAME
 
     animations[1] = rendering.draw_animation{
-      target = position.center,
+      target = tile.center,
       surface = surface,
       animation = mod_prefix .. piece .. "-top",
       time_to_live = ttl,
       animation_offset = 0,
       animation_speed = 0,
-      render_layer = get_entity_type(entity) and "above-inserters" or "higher-object-above",
+      render_layer = top_render_layer,
       visible = false,
     }
 
     animations[2] = rendering.draw_animation{
-      target = position.center,
+      target = tile.center,
       surface = surface,
       animation = mod_prefix .. piece .. "-body",
       time_to_live = ttl,
@@ -165,6 +200,24 @@ end
 
 script.on_event(defines.events.on_tick, do_tasks_at_tick)
 
+local function interface_create(config)
+  return request_platform_animation_for_config(config)
+end
+
+local function interface_legacy(entity)
+  return interface_create({
+    surface = entity.surface,
+    position = entity.position, -- must be a position within the area, preferably the center
+    area = entity.bounding_box, -- the area of the map that shound be covered by scaffolding
+
+    top_render_layer = get_entity_type(entity) == "cargo-landing-pad" and "above-inserters" or "higher-object-above",
+  })
+end
+
+-- /c remote.call("space-platform-entity-build-animation-lib", "legacy", game.player.selected)
+-- /c remote.call("space-platform-entity-build-animation-lib", "create", {surface = "nauvis", position = {0, 0}, area = {{-2, -2}, {2, 2}}})
+-- /c remote.call("space-platform-entity-build-animation-lib", "create", {surface = game.player.surface, position = game.player.position, area = {{-10, -20}, {20, 10}}})
 remote.add_interface(mod_name, {
-  legacy = request_platform_animation_for_entity,
+  legacy = interface_legacy,
+  create = interface_create,
 })
