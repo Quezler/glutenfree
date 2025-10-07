7c9fee3e1abd8241c7972876da5aad2ea5c24585
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-10-07 15:05:53 +0200

    Add numbers to the quality modules too

--- mods_2.0/117_se-module-icons-have-their-tier-number-the-corner/data-updates.lua
+++ mods_2.0/117_se-module-icons-have-their-tier-number-the-corner/data-updates.lua
@@ -1,6 +1,12 @@
 -- weird, data-updates.lua is required since SE doesn't define the new module tiers in data.lua?
 
-for _, variant in ipairs({"speed", "productivity", "efficiency"}) do
+local module_types = {"speed", "productivity", "efficiency"}
+
+if settings.startup["se-enable-data-stage-quality-mod-compatibility"].value then
+  table.insert(module_types, "quality")
+end
+
+for _, variant in ipairs(module_types) do
   for i = 1, 9 do
     local name = variant .. "-module"
     if i ~= 1 then name = name .. "-" .. i end
