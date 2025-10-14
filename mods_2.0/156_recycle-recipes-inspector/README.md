578745ca0fcbf924a8c812fa8f11a051bd06e19e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2025-10-14 14:36:42 +0200

    Mod portal release

---
+++ mods_2.0/156_recycle-recipes-inspector/README.md
@@ -0,0 +1,8 @@
+Aimed at modpacks and the people working on compatibility code for them.
+
+With this you can scroll through the list to visibly confirm if the recycle recipes look good or not.
+
+(like good luck checking each recycle result in factoriopedia to see if it picked the right recipe and was not updated later)
+
+Usage: `/recycle-recipes-inspector`
+(this mod assumes recycling recipes are named `<item name>-recycling`)
---
+++ mods_2.0/156_recycle-recipes-inspector/control.lua
@@ -0,0 +1,87 @@
+require("shared")
+
+local gui_frame_name = mod_prefix .. "frame"
+
+local function open_gui(player)
+  local frame = player.gui.screen[gui_frame_name]
+  if frame then frame.destroy() end
+
+  frame = player.gui.screen.add{
+    type = "frame",
+    name = gui_frame_name,
+    direction = "vertical",
+    caption = {"mod-name." .. mod_name},
+  }
+  frame.style.maximal_height = 500
+
+  local inner = frame.add{
+    type = "frame",
+    name = "inner",
+    style = "inside_shallow_frame",
+    direction = "vertical",
+  }
+  frame.style.minimal_width = 1000
+
+  local scroll_pane = inner.add{
+    type = "scroll-pane",
+    name = "scroll-pane",
+    style = "deep_scroll_pane",
+    vertical_scroll_policy = "always",
+  }
+  scroll_pane.style.minimal_width = frame.style.minimal_width - (8 * 3) - 12
+  scroll_pane.style.maximal_width = frame.style.minimal_width - (8 * 3) - 12
+  scroll_pane.style.minimal_height = frame.style.maximal_height - 80
+
+  for _, item in pairs(prototypes.item) do
+    local flow = scroll_pane.add{
+      type = "flow",
+      name = item.name,
+      style = "horizontal_flow",
+    }
+
+    local ingredient = flow.add{
+      type = "sprite-button",
+      sprite = "item/" .. item.name,
+      tooltip = string.format("%s (%s)", item.name, item.type),
+      enabled = not (item.hidden or item.parameter),
+    }
+    flow.add{
+      type = "sprite-button",
+      sprite = "virtual-signal/right-arrow",
+      tooltip = "recycles into",
+    }
+
+    local recycling = prototypes.recipe[item.name .. "-recycling"]
+    if not recycling then
+      flow.add{
+        type = "sprite-button",
+        sprite = "virtual-signal/signal-no-entry",
+        tooltip = "cannot be recycled",
+      }
+    else
+      ingredient.number = recycling.energy
+      for _, product in ipairs(recycling.products) do
+        flow.add{
+          type = "sprite-button",
+          sprite = "item/" .. product.name,
+          tooltip = string.format("%s (%s)", product.name, product.type),
+          number = product.amount + (product.extra_count_fraction or 0)
+        }
+      end
+    end
+  end
+
+  player.opened = frame
+  frame.force_auto_center()
+end
+
+commands.add_command(mod_name, nil, function(command)
+  local player = game.get_player(command.player_index) --[[@as LuaPlayer]]
+  open_gui(player)
+end)
+
+script.on_event(defines.events.on_gui_closed, function(event)
+  if event.element and event.element.name == gui_frame_name then
+    event.element.destroy()
+  end
+end)
