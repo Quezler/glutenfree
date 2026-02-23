3c1976c41e991dfd59f858f5c0ee33e6d7e22324
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-02-23 12:10:23 +0100

    Fix zonelist crash on world load with SE 0.7.45

--- mods_2.0/080_space-exploration-scripts/zonelist.lua
+++ mods_2.0/080_space-exploration-scripts/zonelist.lua
@@ -1,4 +1,6 @@
 -- step 1
+local old_mod_prefix = mod_prefix
+mod_prefix = "se-"
 Event = {addListener = function() end}
 
 -- step 2
@@ -6,6 +8,7 @@ local Zonelist = require("__space-exploration__.scripts.zonelist")
 
 -- step 3
 Event = nil
+mod_prefix = old_mod_prefix
 
 -- step 4
 
