98c55b2cb65e76d2c3e82e6d79110571594067f0
Patrick 'Quezler' Mounier <patrick@queztech.net>
2026-03-08 20:10:37 +0100

    Fix attempt to concatenate global with SE 0.7.51

--- mods_2.0/080_space-exploration-scripts/misc.lua
+++ mods_2.0/080_space-exploration-scripts/misc.lua
@@ -1,9 +1,11 @@
 function require_SE(path)
   local old_mod_prefix = mod_prefix
+  local old_mod_prefix_snake_case = mod_prefix_snake_case
   local old_event = Event
   local old_util = util
 
   mod_prefix = "se-"
+  mod_prefix_snake_case = "se_"
   Event = {
     addListener = function() end,
     addOnEntityCreatedListeners = function() end,
@@ -14,6 +16,7 @@ function require_SE(path)
   local module = require(path)
 
   mod_prefix = old_mod_prefix
+  mod_prefix_snake_case = old_mod_prefix_snake_case
   Event = old_event
   util = old_util
 
